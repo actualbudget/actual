3fee9cbb4260d064328938c4f0da062695f3b0d2
Jared Gharib <jazzag69@gmail.com>
2022-05-21 21:17:20 +1000

    fix: error handling middleware signature
    Error handling middleware functions must have four arguments to
    identify it as such.
    
    Fixes #40

--- util/error-middleware.js
+++ util/error-middleware.js
@@ -1,4 +1,4 @@
-async function middleware(err, req, res) {
+async function middleware(err, req, res, next) {
   console.log('ERROR', err);
   res.status(500).send({ status: 'error', reason: 'internal-error' });
 }
