bafa4866687772ed15b17c7879a98e7e7d07f0f0
Tom French <tom@tomfren.ch>
2022-07-16 22:47:33 +0100

    style: ignore unused variables which begin with an underscore

--- .eslintrc.js
+++ .eslintrc.js
@@ -10,6 +10,13 @@ module.exports = {
   extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended'],
   rules: {
     'prettier/prettier': 'error',
+    '@typescript-eslint/no-unused-vars': [
+      'warn',
+      {
+        argsIgnorePattern: '^_'
+      }
+    ],
+
     '@typescript-eslint/no-var-requires': 'off'
   }
 };
