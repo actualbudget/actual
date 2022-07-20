ac86c4a5a595441b1ff726fcaf70b691cdf14ea4
Arthur E. Jones <PartyLich@gmail.com>
2022-07-19 20:30:01 -0500

    refactor: extract useServerVersion hook

---
+++ packages/desktop-client/src/hooks/useServerVersion.js
@@ -0,0 +1,22 @@
+import React, { useState, useEffect } from 'react';
+import { send } from 'loot-core/src/platform/client/fetch';
+
+function useServerVersion() {
+  let [version, setVersion] = useState('');
+
+  useEffect(() => {
+    (async () => {
+      const { error, version } = await send('get-server-version');
+
+      if (error) {
+        setVersion('');
+      } else {
+        setVersion(version);
+      }
+    })();
+  }, []);
+
+  return version ? `v${version}` : 'N/A';
+}
+
+export default useServerVersion;
