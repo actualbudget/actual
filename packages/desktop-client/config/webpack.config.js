c364f3f91cdf849df88653c0dd27af55e0a9ad1e
Tom French <tom@tomfren.ch>
2022-07-20 01:58:06 +0100

    build: always pass paths.appBuild as output path

--- packages/desktop-client/config/webpack.config.js
+++ packages/desktop-client/config/webpack.config.js
@@ -129,7 +129,7 @@ module.exports = function(webpackEnv) {
     entry: paths.appIndexJs,
     output: {
       // The build folder.
-      path: isEnvProduction ? paths.appBuild : undefined,
+      path: paths.appBuild,
       // Add /* filename */ comments to generated require()s in the output.
       pathinfo: isEnvDevelopment,
       // There will be one main bundle, and one file per asynchronous chunk.
