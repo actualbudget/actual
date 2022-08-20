71eff903f33f8816d96ed4051a74306a901c9949
Tom French <tom@tomfren.ch>
2022-08-20 17:24:24 +0100

    build: update file loader to new style

--- packages/desktop-client/config/webpack.config.js
+++ packages/desktop-client/config/webpack.config.js
@@ -391,15 +391,12 @@ module.exports = function(webpackEnv) {
             // This loader doesn't use a "test" so it will catch all modules
             // that fall through the other loaders.
             {
-              loader: require.resolve('file-loader'),
               // Exclude `js` files to keep "css" loader working as it injects
               // its runtime that would otherwise be processed through "file" loader.
               // Also exclude `html` and `json` extensions so they get processed
               // by webpacks internal loaders.
               exclude: [/\.(js|mjs|jsx|ts|tsx)$/, /\.html$/, /\.json$/],
-              options: {
-                name: 'static/media/[name].[hash:8].[ext]'
-              }
+              type: 'asset/resource'
             }
             // ** STOP ** Are you adding a new loader?
             // Make sure to add the new loader(s) before the "file" loader.
