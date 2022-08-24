0e0b46ed5ede5639db257fde1af6e4abcb0200ad
Tom French <tom@tomfren.ch>
2022-08-24 21:13:37 +0100

    fix: stop the web tests from running in node environment

--- packages/loot-core/jest.config.js
+++ packages/loot-core/jest.config.js
@@ -7,7 +7,7 @@ module.exports = {
     .concat(['mjs', 'js', 'ts', 'json']),
   setupFilesAfterEnv: ['<rootDir>/src/mocks/setup.js'],
   testEnvironment: 'node',
-  testPathIgnorePatterns: ['/node_modules/', '/lib/', 'index.web.test.js'],
+  testPathIgnorePatterns: ['/node_modules/', '/lib/', '.+/index.web.test.js'],
   transformIgnorePatterns: ['__mocks__'],
   globals: {
     __TESTING__: true
