1d0b2721dbaa8c4050040e85b222c087e45ef437
Tom French <tom@tomfren.ch>
2022-08-25 16:36:02 +0100

    style: prettify .eslintrc.js

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,10 +1,10 @@
 module.exports = {
-  plugins: ["prettier"],
-  extends: ["react-app"],
+  plugins: ['prettier'],
+  extends: ['react-app'],
   rules: {
-    "prettier/prettier": "error",
-    "no-unused-vars": "off",
-    "no-loop-func": "off",
-    "no-restricted-globals": "off"
+    'prettier/prettier': 'error',
+    'no-unused-vars': 'off',
+    'no-loop-func': 'off',
+    'no-restricted-globals': 'off'
   }
 };
