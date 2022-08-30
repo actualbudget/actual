953846732c10cb9b673ee82d6d78bc568fa904c8
Tom French <tom@tomfren.ch>
2022-08-31 00:21:17 +0100

    style: remove setting of indent size to 4 in i18n-parser config

--- packages/desktop-client/i18next-parser.config.js
+++ packages/desktop-client/i18next-parser.config.js
@@ -3,7 +3,6 @@ module.exports = {
   output: 'src/locales/$LOCALE.json',
   locales: ['en-GB', 'es-ES'],
   sort: true,
-  indentation: 4,
   // Force usage of JsxLexer for .js files as otherwise we can't pick up <Trans> components.
   lexers: {
     js: ['JsxLexer'],
