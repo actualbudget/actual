7bafaa3d379f70424c5343f171dfd7e06c5e17fc
Manuel Canepa <manuelcanepa@gmail.com>
2022-08-30 12:17:48 -0300

    #199 Translation for status badge

--- packages/desktop-client/src/components/schedules/StatusBadge.js
+++ packages/desktop-client/src/components/schedules/StatusBadge.js
@@ -9,45 +9,55 @@ import CalendarIcon from 'loot-design/src/svg/v2/Calendar';
 import ValidationCheck from 'loot-design/src/svg/v2/ValidationCheck';
 import FavoriteStar from 'loot-design/src/svg/v2/FavoriteStar';
 import CheckCircle1 from 'loot-design/src/svg/v2/CheckCircle1';
+import { useTranslation } from 'react-i18next';
 
 export function getStatusProps(status) {
-  let color, backgroundColor, Icon;
+  let color, backgroundColor, Icon, Title;
+
+  const { t } = useTranslation();
 
   switch (status) {
     case 'missed':
       color = colors.r1;
       backgroundColor = colors.r10;
       Icon = EditSkull1;
+      Title = t('status.missed');
       break;
     case 'due':
       color = colors.y1;
       backgroundColor = colors.y9;
       Icon = AlertTriangle;
+      Title = t('status.due');
       break;
     case 'upcoming':
       color = colors.p1;
       backgroundColor = colors.p10;
       Icon = CalendarIcon;
+      Title = t('status.upcoming');
       break;
     case 'paid':
       color = colors.g2;
       backgroundColor = colors.g10;
       Icon = ValidationCheck;
+      Title = t('status.paid');
       break;
     case 'completed':
       color = colors.n4;
       backgroundColor = colors.n11;
       Icon = FavoriteStar;
+      Title = t('status.completed');
       break;
     case 'pending':
       color = colors.g4;
       backgroundColor = colors.g11;
       Icon = CalendarIcon;
+      Title = t('status.pending');
       break;
     case 'scheduled':
       color = colors.n1;
       backgroundColor = colors.n11;
       Icon = CalendarIcon;
+      Title = t('status.scheduled');
       break;
     default:
       color = colors.n1;
@@ -56,7 +66,7 @@ export function getStatusProps(status) {
       break;
   }
 
-  return { color, backgroundColor, Icon };
+  return { color, backgroundColor, Icon, Title };
 }
 
 export function StatusIcon({ status }) {
@@ -66,7 +76,7 @@ export function StatusIcon({ status }) {
 }
 
 export function StatusBadge({ status, style }) {
-  let { color, backgroundColor, Icon } = getStatusProps(status);
+  let { color, backgroundColor, Icon, Title } = getStatusProps(status);
   return (
     <View
       style={[
@@ -90,7 +100,7 @@ export function StatusBadge({ status, style }) {
           marginRight: 7
         }}
       />
-      <Text style={{ lineHeight: '1em' }}>{titleFirst(status)}</Text>
+      <Text style={{ lineHeight: '1em' }}>{titleFirst(Title)}</Text>
     </View>
   );
 }
