ecd91b8f8e1eea5c419c9206bfb834c718c094b5
Ezekiel Elin <ezekielelin@me.com>
2022-09-01 09:11:31 -0400

    Allow `enter` to create new transaction when focused on `cleared` column (#234)

--- packages/desktop-client/src/components/accounts/TransactionsTable.js
+++ packages/desktop-client/src/components/accounts/TransactionsTable.js
@@ -1634,7 +1634,8 @@ export let TransactionTable = React.forwardRef((props, ref) => {
             onAddSplit(lastTransaction.id);
           } else if (
             (newNavigator.focusedField === 'debit' ||
-              newNavigator.focusedField === 'credit') &&
+              newNavigator.focusedField === 'credit' ||
+              newNavigator.focusedField === 'cleared') &&
             newNavigator.editingId === lastTransaction.id &&
             (!isSplit || !lastTransaction.error)
           ) {
