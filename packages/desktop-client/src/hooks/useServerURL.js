12d7eef0599f0976dd6805a67aa6b706c1fe9bad
Jed Fox <git@jedfox.com>
2022-11-08 15:15:57 -0500

    url → serverUrl
    Co-Authored-By: Matiss Janis Aboltins <886567+matissjanis@users.noreply.github.com>

--- packages/desktop-client/src/hooks/useServerURL.js
+++ packages/desktop-client/src/hooks/useServerURL.js
@@ -3,16 +3,16 @@ import { useState, useEffect } from 'react';
 import { send } from 'loot-core/src/platform/client/fetch';
 
 export function useServerURL() {
-  let [url, setUrl] = useState('');
+  let [serverUrl, setServerUrl] = useState('');
   useEffect(() => {
     async function run() {
       let url = (await send('get-server-url')) || '';
       if (url === 'https://not-configured/') {
         url = '';
       }
-      setUrl(url);
+      setServerUrl(url);
     }
     run();
   }, []);
-  return url;
+  return serverUrl;
 }
