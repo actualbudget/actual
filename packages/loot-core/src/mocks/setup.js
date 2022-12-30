6310d9230b572e649ecc7328b84963c3185898b8
Jed Fox <git@jedfox.com>
2022-12-30 13:05:58 -0500

    Simplify the settings layout and merge everything onto one page (#267)
    Co-authored-by: Matiss Janis Aboltins <matiss@mja.lv>

--- packages/loot-core/src/mocks/setup.js
+++ packages/loot-core/src/mocks/setup.js
@@ -9,14 +9,10 @@ import {
 import { setServer } from '../server/server-config';
 import * as sheet from '../server/sheet';
 import { setSyncingMode } from '../server/sync';
-import * as tracking from '../server/tracking/events';
 import { updateVersion } from '../server/update';
 import { resetTracer, tracer } from '../shared/test-helpers';
 
 jest.mock('../server/post');
-
-// No need to run any of the tracking code in tests
-tracking.toggle(false);
 
 const nativeFs = require('fs');
 
