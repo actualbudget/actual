632114b4d181e840b1102b0c9e207f07fea76cc0
Jed Fox <git@jedfox.com>
2023-01-02 10:27:50 -0500

    move “Find schedules” to a button on the Schedules page

--- packages/loot-design/src/components/sidebar.js
+++ packages/loot-design/src/components/sidebar.js
@@ -439,7 +439,6 @@ function Tools() {
   let items = [
     { name: 'payees', text: 'Payees' },
     { name: 'rules', text: 'Rules' },
-    { name: 'find-schedules', text: 'Find schedules' },
     { name: 'repair-splits', text: 'Repair split transactions' }
   ];
 
@@ -451,9 +450,6 @@ function Tools() {
           break;
         case 'rules':
           history.push('/rules');
-          break;
-        case 'find-schedules':
-          history.push('/schedule/discover');
           break;
         case 'repair-splits':
           history.push('/tools/fix-splits', { locationPtr: history.location });
@@ -474,12 +470,9 @@ function Tools() {
         onClick={onToggle}
         style={{ pointerEvents: isOpen ? 'none' : 'auto' }}
         forceHover={isOpen}
-        forceActive={[
-          '/payees',
-          '/rules',
-          '/tools',
-          '/schedule/discover'
-        ].some(route => location.pathname.startsWith(route))}
+        forceActive={['/payees', '/rules', '/tools'].some(route =>
+          location.pathname.startsWith(route)
+        )}
         button={
           <ChevronRight
             width={12}
