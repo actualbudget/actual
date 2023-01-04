dd14ec66083bc512bbeabfeb03b40b8918bb6b90
Matiss Janis Aboltins <matiss@mja.lv>
2023-01-04 18:16:33 +0000

    chore: run prettier against sample-main.js and fix crlf

--- packages/mobile/nodejs-assets/nodejs-project/sample-main.js
+++ packages/mobile/nodejs-assets/nodejs-project/sample-main.js
@@ -4,9 +4,9 @@
 var rn_bridge = require('rn-bridge');
 
 // Echo every message received from react-native.
-rn_bridge.channel.on('message', (msg) => {
+rn_bridge.channel.on('message', msg => {
   rn_bridge.channel.send(msg);
-} );
+});
 
 // Inform react-native node is initialized.
-rn_bridge.channel.send("Node was initialized.");
+rn_bridge.channel.send('Node was initialized.');
