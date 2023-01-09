8dbd397adf98a0000e76b34eb65d302a9c591b35
Erick Shaffer <erick.shaffer321@gmail.com>
2023-01-09 07:33:01 -0700

    removed lively from sidebar usage

--- packages/loot-design/src/components/sidebar.usage.js
+++ packages/loot-design/src/components/sidebar.usage.js
@@ -1,9 +1,7 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { DndProvider } from 'react-dnd';
 import Backend from 'react-dnd-html5-backend';
 import { MemoryRouter as Router } from 'react-router-dom';
-
-import lively from '@jlongster/lively';
 
 import { generateAccount } from 'loot-core/src/mocks';
 import makeSpreadsheet from 'loot-core/src/mocks/spreadsheet';
@@ -11,11 +9,6 @@ import makeSpreadsheet from 'loot-core/src/mocks/spreadsheet';
 import { Section } from '../guide/components';
 import { Sidebar } from './sidebar';
 import SpreadsheetContext from './spreadsheet/SpreadsheetContext';
-
-function withState(state, render) {
-  const Component = lively(render, { getInitialState: () => state });
-  return <Component />;
-}
 
 const accounts = [
   generateAccount('Bank of America', true),
@@ -26,23 +19,24 @@ const accounts = [
 ];
 
 function makeSidebar(selected) {
-  return withState({ selected }, ({ state: { selected }, setState }) => {
-    return (
-      <Router initialEntries={[selected]} initialIndex={0}>
-        <DndProvider backend={Backend}>
-          <Sidebar
-            budgetName="Personal"
-            accounts={accounts}
-            getBalanceQuery={account => ({ expr: 10000 })}
-            getOnBudgetBalance={() => ({ expr: 30000 })}
-            getOffBudgetBalance={() => ({ expr: 10000 })}
-            showClosedAccounts={true}
-            style={{ paddingBottom: 100 }}
-          />
-        </DndProvider>
-      </Router>
-    );
-  });
+  const [state, setState] = useState(
+    typeof selected === 'string' ? selected : ''
+  );
+  return (
+    <Router initialEntries={[state]} initialIndex={0}>
+      <DndProvider backend={Backend}>
+        <Sidebar
+          budgetName="Personal"
+          accounts={accounts}
+          getBalanceQuery={account => ({ expr: 10000 })}
+          getOnBudgetBalance={() => ({ expr: 30000 })}
+          getOffBudgetBalance={() => ({ expr: 10000 })}
+          showClosedAccounts={true}
+          style={{ paddingBottom: 100 }}
+        />
+      </DndProvider>
+    </Router>
+  );
 }
 
 export default () => (
