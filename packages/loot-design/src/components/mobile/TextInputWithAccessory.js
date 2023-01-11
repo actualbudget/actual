e5109e2227b395144bd0457c82f46d3ec34c3523
Matiss Janis Aboltins <matiss@mja.lv>
2023-01-11 22:19:17 +0000

    Even more remvoed code

--- packages/loot-design/src/components/mobile/TextInputWithAccessory.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { TextInput } from 'react-native';
-
-export default React.forwardRef(function TextInputWithAccessory(
-  { accessoryId, ...props },
-  ref
-) {
-  return <TextInput ref={ref} inputAccessoryViewID={accessoryId} {...props} />;
-});
