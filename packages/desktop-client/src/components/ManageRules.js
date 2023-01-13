f15435cf9539d4e7079b4f4031dd04a95c6a504a
winklevos <winklevos@outlook.com>
2023-01-13 19:04:11 +1100

    let rule

--- packages/desktop-client/src/components/ManageRules.js
+++ packages/desktop-client/src/components/ManageRules.js
@@ -588,22 +588,30 @@ export default function ManageRules({
   }, []);
 
   function onCreateRule() {
+    let rule = {
+      id: null,
+      stage: null,
+      conditions: [
+        {
+          field: 'payee',
+          op: 'is',
+          value: payeeId,
+          type: 'id'
+        }
+      ],
+      actions: [
+        {
+          op: 'set',
+          field: 'category',
+          value: null,
+          type: 'id'
+        }
+      ]
+    };
+
     dispatch(
       pushModal('edit-rule', {
-        rule: {
-          stage: null,
-          conditions: [
-            { op: 'is', field: 'payee', value: payeeId, type: 'id' }
-          ],
-          actions: [
-            {
-              op: 'set',
-              field: 'category',
-              value: null,
-              type: 'id'
-            }
-          ]
-        },
+        rule,
         onSave: async newRule => {
           let newRules = await loadRules();
 
