1b714bafecf2f36a8d261a47b9451fbe4eba55b6
Jed Fox <git@jedfox.com>
2023-01-14 08:52:43 -0500

    Make it easier to build the bundle.api.js for the API

--- packages/loot-core/webpack/webpack.api.config.js
+++ packages/loot-core/webpack/webpack.api.config.js
@@ -1,9 +1,17 @@
+let path = require('path');
+
 let webpack = require('webpack');
+
 let config = require('./webpack.desktop.config');
 
 config.resolve.extensions = ['.api.js', '.electron.js', '.js', '.json'];
 config.output.filename = 'bundle.api.js';
 config.output.sourceMapFilename = 'bundle.api.js.map';
+config.output.path = path.join(
+  path.dirname(path.dirname(__dirname)),
+  'api',
+  'app'
+);
 
 config.plugins.push(
   new webpack.DefinePlugin({
