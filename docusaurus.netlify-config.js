5196d068ad1a1eaab66ecfe753629b203bc6aa99
Matiss Janis Aboltins <matiss@mja.lv>
2023-01-14 18:28:58 +0000

    :construction_worker:  netlify preview deployments

---
+++ docusaurus.netlify-config.js
@@ -0,0 +1,10 @@
+// @ts-check
+// Note: type annotations allow type checking and IDEs autocompletion
+
+const baseConfig = require('./docusaurus.config');
+
+/** @type {import('@docusaurus/types').Config} */
+module.exports = {
+  ...baseConfig,
+  baseUrl: '/',
+};
