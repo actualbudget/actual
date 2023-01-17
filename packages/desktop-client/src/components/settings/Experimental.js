cf15081ca3b01ec5e58a4d45fc8d16f1b61df1b3
Jed Fox <git@jedfox.com>
2023-01-17 16:24:48 -0500

    Put experimental features behind another link

--- packages/desktop-client/src/components/settings/Experimental.js
+++ packages/desktop-client/src/components/settings/Experimental.js
@@ -1,12 +1,18 @@
 import React from 'react';
 
-import { Text, View } from 'loot-design/src/components/common';
+import {
+  ButtonLink,
+  Link,
+  Text,
+  View
+} from 'loot-design/src/components/common';
 import { Checkbox } from 'loot-design/src/components/forms';
 import { colors } from 'loot-design/src/style';
 
 import { Setting } from './UI';
 
 export default function ExperimentalFeatures({ prefs, savePrefs }) {
+  let [expanded, setExpanded] = React.useState(false);
   let flags = Object.fromEntries(
     Object.entries(prefs)
       .filter(([key]) => key.startsWith('flags.'))
@@ -16,39 +22,55 @@ export default function ExperimentalFeatures({ prefs, savePrefs }) {
   return (
     <Setting
       primaryAction={
-        <View style={{ gap: '1em' }}>
-          <label
-            style={{ display: 'flex', color: disabled ? colors.n5 : 'inherit' }}
-          >
-            <Checkbox
-              id="report-budget-flag"
-              checked={flags.reportBudget}
-              onChange={() => {
-                savePrefs({ 'flags.reportBudget': !flags.reportBudget });
+        expanded ? (
+          <View style={{ gap: '1em' }}>
+            <label
+              style={{
+                display: 'flex',
+                color: disabled ? colors.n5 : 'inherit'
               }}
-              disabled={disabled}
-            />{' '}
-            <View>
-              Enable budget mode toggle
-              {disabled && (
-                <Text style={{ color: colors.r3, fontWeight: 500 }}>
-                  Switch to a rollover budget before turning off this feature
-                </Text>
-              )}
-            </View>
-          </label>
+            >
+              <Checkbox
+                id="report-budget-flag"
+                checked={flags.reportBudget}
+                onChange={() => {
+                  savePrefs({ 'flags.reportBudget': !flags.reportBudget });
+                }}
+                disabled={disabled}
+              />{' '}
+              <View>
+                Enable budget mode toggle
+                {disabled && (
+                  <Text style={{ color: colors.r3, fontWeight: 500 }}>
+                    Switch to a rollover budget before turning off this feature
+                  </Text>
+                )}
+              </View>
+            </label>
 
-          <label style={{ display: 'flex' }}>
-            <Checkbox
-              id="report-budget-flag"
-              checked={flags.syncAccount}
-              onChange={() => {
-                savePrefs({ 'flags.syncAccount': !flags.syncAccount });
-              }}
-            />{' '}
-            <View>Enable account syncing</View>
-          </label>
-        </View>
+            <label style={{ display: 'flex' }}>
+              <Checkbox
+                id="report-budget-flag"
+                checked={flags.syncAccount}
+                onChange={() => {
+                  savePrefs({ 'flags.syncAccount': !flags.syncAccount });
+                }}
+              />{' '}
+              <View>Enable account syncing</View>
+            </label>
+          </View>
+        ) : (
+          <Link
+            onClick={() => setExpanded(true)}
+            style={{
+              flexShrink: 0,
+              alignSelf: 'flex-start',
+              color: colors.p4
+            }}
+          >
+            I understand the risks, show experimental features
+          </Link>
+        )
       }
     >
       <Text>
