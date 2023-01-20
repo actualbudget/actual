71ee11bf5bc798a4b8728019b42e4f562a72f331
shall0pass <20625555+shall0pass@users.noreply.github.com>
2023-01-20 16:06:31 -0600

    Revert "allows xfer trans. to be marked "clear" by rule"
    This reverts commit 0c5f90e38cec59868156053e280dafe2e3b31da8.

--- packages/loot-core/src/server/accounts/transfer.js
+++ packages/loot-core/src/server/accounts/transfer.js
@@ -75,7 +75,7 @@ export async function addTransfer(transaction, transferredAccount) {
     date: transaction.date,
     transfer_id: transaction.id,
     notes: transaction.notes || null,
-    cleared: null
+    cleared: false
   });
 
   await db.updateTransaction({ id: transaction.id, transfer_id: id });
