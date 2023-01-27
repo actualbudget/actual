ede1e8998e71a9a11dfb4646a7fdfd5f7e2e77df
Rich Howell <github@codenameowl.com>
2023-01-27 21:03:51 +0000

    Update Export.js
    Added the date to the file name of the zip file.

--- packages/desktop-client/src/components/settings/Export.js
+++ packages/desktop-client/src/components/settings/Export.js
@@ -2,13 +2,14 @@ import React from 'react';
 
 import { send } from 'loot-core/src/platform/client/fetch';
 import { Text, Button } from 'loot-design/src/components/common';
+import { format } from 'date-fns'
 
 import { Setting } from './UI';
 
 export default function ExportBudget({ prefs }) {
   async function onExport() {
     let data = await send('export-budget');
-    window.Actual.saveFile(data, `${prefs.id}.zip`, 'Export budget');
+    window.Actual.saveFile(data, `${format(new Date(), 'yyyymmdd')}-${prefs.id}.zip`, 'Export budget');
   }
 
   return (
