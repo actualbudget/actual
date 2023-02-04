b785cbbfdadaf2a57d5dddb7db2d71d3f0f1086f
Charles Taylor <taylorc822@gmail.com>
2023-02-04 22:48:14 +1100

    fix: remove incorrect autoformatting

--- packages/loot-core/src/client/actions/queries.js
+++ packages/loot-core/src/client/actions/queries.js
@@ -11,7 +11,7 @@ import {
 } from './notifications';
 
 export function applyBudgetAction(month, type, args) {
-  return async function() {
+  return async function () {
     switch (type) {
       case 'budget-amount':
         await send('budget/budget-amount', {
@@ -80,7 +80,7 @@ export function applyBudgetAction(month, type, args) {
 }
 
 export function getCategories() {
-  return async function(dispatch) {
+  return async function (dispatch) {
     const categories = await send('get-categories');
     dispatch({
       type: constants.LOAD_CATEGORIES,
@@ -91,7 +91,7 @@ export function getCategories() {
 }
 
 export function createCategory(name, groupId, isIncome) {
-  return async function(dispatch) {
+  return async function (dispatch) {
     let id = await send('category-create', {
       name,
       groupId,
@@ -103,7 +103,7 @@ export function createCategory(name, groupId, isIncome) {
 }
 
 export function deleteCategory(id, transferId) {
-  return async function(dispatch, getState) {
+  return async function (dispatch, getState) {
     let { error } = await send('category-delete', { id, transferId });
 
     if (error) {
@@ -173,7 +173,7 @@ export function updateGroup(group) {
 }
 
 export function deleteGroup(id, transferId) {
-  return async function(dispatch, getState) {
+  return async function (dispatch, getState) {
     await send('category-group-delete', { id, transferId });
     await dispatch(getCategories());
     // See `deleteCategory` for why we need this
@@ -182,7 +182,7 @@ export function deleteGroup(id, transferId) {
 }
 
 export function getPayees() {
-  return async function(dispatch) {
+  return async function (dispatch) {
     let payees = await send('payees-get');
     dispatch({
       type: constants.LOAD_PAYEES,
@@ -193,7 +193,7 @@ export function getPayees() {
 }
 
 export function initiallyLoadPayees() {
-  return async function(dispatch, getState) {
+  return async function (dispatch, getState) {
     if (getState().queries.payees.length === 0) {
       return dispatch(getPayees());
     }
@@ -207,7 +207,7 @@ export function createPayee(name) {
 }
 
 export function getAccounts() {
-  return async function(dispatch) {
+  return async function (dispatch) {
     const accounts = await send('accounts-get');
     dispatch({ type: constants.LOAD_ACCOUNTS, accounts });
     return accounts;
@@ -215,7 +215,7 @@ export function getAccounts() {
 }
 
 export function updateAccount(account) {
-  return async function(dispatch) {
+  return async function (dispatch) {
     const error = await send('account-update', account);
 
     switch (error) {
@@ -231,7 +231,7 @@ export function updateAccount(account) {
 }
 
 export function createAccount(name, type, balance, offBudget) {
-  return async function(dispatch) {
+  return async function (dispatch) {
     let id = await send('account-create', { name, type, balance, offBudget });
 
     switch (id) {
@@ -248,7 +248,7 @@ export function createAccount(name, type, balance, offBudget) {
 }
 
 export function openAccountCloseModal(accountId) {
-  return async function(dispatch, getState) {
+  return async function (dispatch, getState) {
     const { balance, numTransactions } = await send('account-properties', {
       id: accountId
     });
@@ -267,7 +267,7 @@ export function openAccountCloseModal(accountId) {
 }
 
 export function closeAccount(accountId, transferAccountId, categoryId, forced) {
-  return async function(dispatch) {
+  return async function (dispatch) {
     await send('account-close', {
       id: accountId,
       transferAccountId,
@@ -279,7 +279,7 @@ export function closeAccount(accountId, transferAccountId, categoryId, forced) {
 }
 
 export function reopenAccount(accountId) {
-  return async function(dispatch) {
+  return async function (dispatch) {
     await send('account-reopen', { id: accountId });
     dispatch(getAccounts());
   };
