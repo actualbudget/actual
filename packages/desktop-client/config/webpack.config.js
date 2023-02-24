ec90f9a539b3ac8042c84d784f10e5028cb469f5
Jed Fox <git@jedfox.com>
2023-02-24 13:59:40 -0500

    Don’t run linting while building in CI (#689)
    * Don’t run linting while building in CI
    
    * Update webpack.config.js

--- packages/desktop-client/config/webpack.config.js
+++ packages/desktop-client/config/webpack.config.js
@@ -282,7 +282,7 @@ module.exports = function(webpackEnv) {
 
         // First, run the linter.
         // It's important to do this before Babel processes the JS.
-        {
+        !process.env.CI && {
           test: /\.(js|mjs|jsx)$/,
           enforce: 'pre',
           use: [
@@ -428,7 +428,7 @@ module.exports = function(webpackEnv) {
             // Make sure to add the new loader(s) before the "file" loader.
           ]
         }
-      ]
+      ].filter(Boolean)
     },
     plugins: [
       // Generates an `index.html` file with the <script> injected.
