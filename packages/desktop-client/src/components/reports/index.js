af62d441a65d677929b33f5818c1a68f1ccd6a6e
carkom <neilmccreery@yahoo.com>
2023-02-26 18:42:11 +0000

    Fixing build errors

--- packages/desktop-client/src/components/reports/index.js
+++ packages/desktop-client/src/components/reports/index.js
@@ -6,7 +6,6 @@ import { View } from 'loot-design/src/components/common';
 import CashFlow from './CashFlow';
 import NetWorth from './NetWorth';
 import Overview from './Overview';
-import Spending from './Spending';
 
 class Reports extends React.Component {
   render() {
@@ -15,7 +14,6 @@ class Reports extends React.Component {
         <Route path="/reports" exact component={Overview} />
         <Route path="/reports/net-worth" exact component={NetWorth} />
         <Route path="/reports/cash-flow" exact component={CashFlow} />
-        <Route path="/reports/spending" exact component={Spending} />
       </View>
     );
   }
