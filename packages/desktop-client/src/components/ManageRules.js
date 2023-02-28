b57371adf89ef45637e5341b4115ca7d696c73da
Jed Fox <git@jedfox.com>
2023-02-27 20:59:01 -0500

    Allow rendering a schedule rule in <Value />

--- packages/desktop-client/src/components/ManageRules.js
+++ packages/desktop-client/src/components/ManageRules.js
@@ -117,6 +117,7 @@ export function Value({
         case 'payee':
         case 'category':
         case 'account':
+        case 'rule':
           if (valueIsRaw) {
             return value;
           }
