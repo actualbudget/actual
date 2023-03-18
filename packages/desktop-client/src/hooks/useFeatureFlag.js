4bdfcaec5a1d0447e29632883ced1d16fa7a0462
Matiss Janis Aboltins <matiss@mja.lv>
2023-03-18 18:03:12 +0000

    Fix the default logic

--- packages/desktop-client/src/hooks/useFeatureFlag.js
+++ packages/desktop-client/src/hooks/useFeatureFlag.js
@@ -7,36 +7,29 @@ import {
 
 const DEFAULT_FEATURE_FLAG_STATE = {
   newAutocomplete: isDevelopmentEnvironment() || isPreviewEnvironment(),
+  syncAccount: false,
   goalTemplatesEnabled: false,
 };
 
-/**
- * If the feature flag is set: use its value.
- * If the flag is not set (default case): use the
- * pre-defined list of feature flags. If it's not
- * there: fall-back to 'false'.
- */
-function getFatureFlagNormalizedValue(name, value) {
-  return value === undefined
-    ? DEFAULT_FEATURE_FLAG_STATE[name] || false
-    : value;
-}
-
 export default function useFeatureFlag(name) {
-  return useSelector(state =>
-    getFatureFlagNormalizedValue(name, state.prefs.local[`flags.${name}`]),
-  );
+  return useSelector(state => {
+    const value = state.prefs.local[`flags.${name}`];
+
+    return value === undefined
+      ? DEFAULT_FEATURE_FLAG_STATE[name] || false
+      : value;
+  });
 }
 
 export function useAllFeatureFlags() {
   return useSelector(state => {
-    return Object.fromEntries(
-      Object.entries(state.prefs.local)
-        .filter(([key]) => key.startsWith('flags.'))
-        .map(([key, value]) => {
-          const name = key.replace('flags.', '');
-          return [name, getFatureFlagNormalizedValue(name, value)];
-        }),
-    );
+    return {
+      ...DEFAULT_FEATURE_FLAG_STATE,
+      ...Object.fromEntries(
+        Object.entries(state.prefs.local)
+          .filter(([key]) => key.startsWith('flags.'))
+          .map(([key, value]) => [key.replace('flags.', ''), value]),
+      ),
+    };
   });
 }
