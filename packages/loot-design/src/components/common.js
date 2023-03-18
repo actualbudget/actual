3c5f9100cd89711c4cf5c3c2e78f72727aaa4f3f
Jed Fox <git@jedfox.com>
2023-03-18 09:52:29 -0400

    HoverTarget: port to function component

--- packages/loot-design/src/components/common.js
+++ packages/loot-design/src/components/common.js
@@ -1104,43 +1104,45 @@ export function InitialFocus({ children }) {
   return React.cloneElement(children, { inputRef: node });
 }
 
-export class HoverTarget extends React.Component {
-  state = { hovered: false };
+export function HoverTarget({
+  style,
+  contentStyle,
+  children,
+  renderContent,
+  disabled,
+}) {
+  let [hovered, setHovered] = useState(false);
 
-  onMouseEnter = () => {
-    if (!this.props.disabled) {
-      this.setState({ hovered: true });
+  const onMouseEnter = useCallback(() => {
+    if (!disabled) {
+      setHovered(true);
     }
-  };
+  }, [disabled]);
 
-  onMouseLeave = () => {
-    if (!this.props.disabled) {
-      this.setState({ hovered: false });
+  const onMouseLeave = useCallback(() => {
+    if (!disabled) {
+      setHovered(false);
     }
-  };
+  }, [disabled]);
 
-  componentDidUpdate(prevProps) {
-    let { disabled } = this.props;
-    if (disabled && this.state.hovered) {
-      this.setState({ hovered: false });
+  useEffect(() => {
+    if (disabled && hovered) {
+      setHovered(false);
     }
-  }
+  }, [disabled, hovered]);
 
-  render() {
-    let { style, contentStyle, children, renderContent } = this.props;
-    return (
-      <View style={style}>
-        <View
-          onMouseEnter={this.onMouseEnter}
-          onMouseLeave={this.onMouseLeave}
-          style={contentStyle}
-        >
-          {children}
-        </View>
-        {this.state.hovered && renderContent()}
+  return (
+    <View style={style}>
+      <View
+        onMouseEnter={onMouseEnter}
+        onMouseLeave={onMouseLeave}
+        style={contentStyle}
+      >
+        {children}
       </View>
-    );
-  }
+      {hovered && renderContent()}
+    </View>
+  );
 }
 
 export function Label({ title, style }) {
