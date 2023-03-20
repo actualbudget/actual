5c4129a42f309be67924b503a64869bf6aae71ff
Jed Fox <git@jedfox.com>
2023-03-19 20:59:28 -0400

    Fix rule tests

--- packages/eslint-plugin-actual/tests/lib/rules/typography.js
+++ packages/eslint-plugin-actual/tests/lib/rules/typography.js
@@ -13,35 +13,56 @@ const rule = require('../../../lib/rules/typography');
 // Tests
 //------------------------------------------------------------------------------
 
-const ruleTester = new RuleTester();
+const ruleTester = new RuleTester({
+  parserOptions: {
+    ecmaVersion: 2015,
+    ecmaFeatures: { jsx: true },
+  },
+});
 ruleTester.run('typography', rule, {
   valid: [
     `let test = 'I said “Hello, world!”';`,
     `let test = "I said ‘Hello, world!’";`,
+    `let test = \`I said ‘Hello, world!’\`;`,
     `let selected = document.querySelector('[data-test="foo"]');`,
-    `let selected = document.querySelectorALl('[data-test="foo"]');`,
+    `let selected = document.querySelectorAll('[data-test="foo"]');`,
   ],
 
   invalid: [
     {
-      code: 'let test = `I said "Hello, world!"`;',
-      errors: [{ messageId: 'quote', type: 'TemplateElement' }],
+      code: 'var test = `I said "Hello, world!"`;',
+      errors: [
+        { messageId: 'quote', type: 'TemplateElement', column: 20 },
+        { messageId: 'quote', type: 'TemplateElement', column: 34 },
+      ],
     },
     {
       code: `test2("I said 'Hello, world!'");`,
-      errors: [{ messageId: 'quote', type: 'Literal' }],
+      errors: [
+        { messageId: 'quote', type: 'Literal', column: 15 },
+        { messageId: 'quote', type: 'Literal', column: 29 },
+      ],
     },
     {
       code: `test3('I said "Hello, world!"');`,
-      errors: [{ messageId: 'quote', type: 'Literal' }],
+      errors: [
+        { messageId: 'quote', type: 'Literal', column: 15 },
+        { messageId: 'quote', type: 'Literal', column: 29 },
+      ],
     },
     {
       code: `<Test4>I said "Hello, world!"</Test4>`,
-      errors: [{ messageId: 'quote', type: 'JSXText' }],
+      errors: [
+        { messageId: 'quote', type: 'JSXText', column: 15 },
+        { messageId: 'quote', type: 'JSXText', column: 29 },
+      ],
     },
     {
       code: `<Test5>I said 'Hello, world!'</Test5>`,
-      errors: [{ messageId: 'quote', type: 'JSXText' }],
+      errors: [
+        { messageId: 'quote', type: 'JSXText', column: 15 },
+        { messageId: 'quote', type: 'JSXText', column: 29 },
+      ],
     },
   ],
 });
