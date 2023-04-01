3a1ca9013622f1df55c36296fe50f28afb19051b
Matiss Janis Aboltins <matiss@mja.lv>
2023-04-01 08:10:40 +0100

    :bug: (autocomplete) set min-height used to calculate the flip (top-bottom) (#837)

--- packages/desktop-client/src/components/autocomplete/NewPayeeAutocomplete.js
+++ packages/desktop-client/src/components/autocomplete/NewPayeeAutocomplete.js
@@ -133,6 +133,7 @@ export default function PayeeAutocomplete({
       components={{
         MenuList: MenuListWithFooter,
       }}
+      minMenuHeight={300}
       footer={
         <AutocompleteFooter show={showMakeTransfer || showManagePayees}>
           {showMakeTransfer && (
