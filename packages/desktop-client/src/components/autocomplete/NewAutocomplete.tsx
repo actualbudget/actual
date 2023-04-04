596764c9fedafe67c1e85b15f775d46d695308be
Matiss Janis Aboltins <matiss@mja.lv>
2023-04-04 22:38:08 +0100

    :bug: (autocomplete) allow editing selected payee name

--- packages/desktop-client/src/components/autocomplete/NewAutocomplete.tsx
+++ packages/desktop-client/src/components/autocomplete/NewAutocomplete.tsx
@@ -1,26 +1,46 @@
 import React, { useState } from 'react';
 import Select, {
+  GroupBase,
   Props as SelectProps,
   PropsValue,
   SingleValue,
   SelectInstance,
 } from 'react-select';
 
+import type { CreatableProps } from 'react-select/creatable';
 import CreatableSelect from 'react-select/creatable';
 
 import { NullComponent } from '../common';
 
 import styles from './autocomplete-styles';
 
-type OptionValue = string;
+type OptionValue = {
+  __isNew__?: boolean;
+  label: string;
+  value: true;
+};
 
-interface AutocompleteProps extends SelectProps<OptionValue> {
-  focused: boolean;
-  embedded: boolean;
+interface BaseAutocompleteProps {
+  focused?: boolean;
+  embedded?: boolean;
   onSelect: (value: PropsValue<OptionValue>) => void;
-  onCreateOption: (value: SingleValue<OptionValue>) => void;
-  isCreatable: boolean;
+  onCreateOption?: (value: string) => void;
+  isCreatable?: boolean;
 }
+
+type SimpleAutocompleteProps = BaseAutocompleteProps & SelectProps<OptionValue>;
+type CreatableAutocompleteProps = BaseAutocompleteProps &
+  CreatableProps<OptionValue, true, GroupBase<OptionValue>> & {
+    isCreatable: true;
+  };
+
+type AutocompleteProps = SimpleAutocompleteProps | CreatableAutocompleteProps;
+
+const isSingleValue = (
+  value: PropsValue<OptionValue>,
+): value is SingleValue<OptionValue> => {
+  return !Array.isArray(value);
+};
 
 const Autocomplete = React.forwardRef<SelectInstance, AutocompleteProps>(
   (
@@ -40,14 +60,28 @@ const Autocomplete = React.forwardRef<SelectInstance, AutocompleteProps>(
     const [initialValue] = useState(value);
     const [isOpen, setIsOpen] = useState(focused);
 
-    const filterOption = (option, input) => {
+    const [inputValue, setInputValue] = useState<
+      AutocompleteProps['inputValue']
+    >(() => (isSingleValue(value) ? value?.label : undefined));
+    const [isInitialInputValue, setInitialInputValue] = useState(true);
+
+    const onInputChange: AutocompleteProps['onInputChange'] = value => {
+      setInputValue(value);
+      setInitialInputValue(false);
+    };
+
+    const filterOption: AutocompleteProps['filterOption'] = (option, input) => {
+      if (isInitialInputValue) {
+        return true;
+      }
+
       return (
         option.data?.__isNew__ ||
         option.label.toLowerCase().includes(input?.toLowerCase())
       );
     };
 
-    const onChange = async selected => {
+    const onChange: AutocompleteProps['onChange'] = selected => {
       // Clear button clicked
       if (!selected) {
         onSelect(null);
@@ -74,7 +108,7 @@ const Autocomplete = React.forwardRef<SelectInstance, AutocompleteProps>(
       onSelect(selected.value);
     };
 
-    const onKeyDown = event => {
+    const onKeyDown: AutocompleteProps['onKeyDown'] = event => {
       if (event.code === 'Escape') {
         onSelect(initialValue);
         setIsOpen(false);
@@ -114,6 +148,8 @@ const Autocomplete = React.forwardRef<SelectInstance, AutocompleteProps>(
         data-embedded={embedded}
         menuPlacement="auto"
         menuPortalTarget={embedded ? undefined : document.body}
+        inputValue={inputValue}
+        onInputChange={onInputChange}
         {...props}
       />
     );
