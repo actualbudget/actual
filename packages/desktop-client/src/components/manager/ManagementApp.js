17e49403943a9b2eddf32b0767f6240e44eeb786
Matiss Janis Aboltins <matiss@mja.lv>
2023-04-04 18:30:49 +0100

    Update ManagementApp.js

--- packages/desktop-client/src/components/manager/ManagementApp.js
+++ packages/desktop-client/src/components/manager/ManagementApp.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useMemo } from 'react';
+import React, { useEffect, useState } from 'react';
 import { connect } from 'react-redux';
 import { Switch, Redirect, Router, Route } from 'react-router-dom';
 
