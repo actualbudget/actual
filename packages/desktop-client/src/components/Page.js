c4b264bd3c61e787143942c8116310f1d31036a4
Matiss Janis Aboltins <matiss@mja.lv>
2023-04-11 19:58:14 +0100

    :art: (mobile) settings page header

--- packages/desktop-client/src/components/Page.js
+++ packages/desktop-client/src/components/Page.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import { useHistory } from 'react-router-dom';
 
-import { styles } from '../style';
+import { colors, styles } from '../style';
+import { isMobile } from '../util';
 
 import { Modal, View, Text } from './common';
 
 let PageTypeContext = React.createContext({ type: 'page' });
+
+const HORIZONTAL_PADDING = isMobile() ? 10 : 20;
 
 export function PageTypeProvider({ type, current, children }) {
   return (
@@ -20,8 +23,37 @@ export function usePageType() {
 }
 
 function PageTitle({ name }) {
+  if (isMobile()) {
+    return (
+      <View
+        style={{
+          alignItems: 'center',
+          backgroundColor: colors.b2,
+          color: 'white',
+          flexDirection: 'row',
+          flex: '1 0 auto',
+          fontSize: 18,
+          fontWeight: 500,
+          height: 50,
+          justifyContent: 'center',
+          overflowY: 'auto',
+        }}
+      >
+        {name}
+      </View>
+    );
+  }
+
   return (
-    <Text style={{ fontSize: 25, fontWeight: 500, marginBottom: 15 }}>
+    <Text
+      style={{
+        fontSize: 25,
+        fontWeight: 500,
+        paddingLeft: HORIZONTAL_PADDING,
+        paddingRight: HORIZONTAL_PADDING,
+        marginBottom: 15,
+      }}
+    >
       {name}
     </Text>
   );
@@ -51,9 +83,21 @@ export function Page({ title, modalSize, children }) {
   }
 
   return (
-    <View style={[styles.page, { paddingLeft: 20, paddingRight: 20, flex: 1 }]}>
+    <View style={isMobile() ? undefined : styles.page}>
       <PageTitle name={title} />
-      {children}
+      <View
+        style={
+          isMobile()
+            ? { overflowY: 'auto', padding: HORIZONTAL_PADDING }
+            : {
+                paddingLeft: HORIZONTAL_PADDING,
+                paddingRight: HORIZONTAL_PADDING,
+                flex: 1,
+              }
+        }
+      >
+        {children}
+      </View>
     </View>
   );
 }
