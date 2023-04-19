ec9bf0d1d180af13b5e2ab021282e4af67000594
Matiss Janis Aboltins <matiss@mja.lv>
2023-04-19 19:04:11 +0100

    oops

--- packages/desktop-client/src/components/modals/SelectLinkedAccounts.js
+++ packages/desktop-client/src/components/modals/SelectLinkedAccounts.js
@@ -172,7 +172,6 @@ function TableRow({
             highlightFirst
             suggestions={availableAccountOptions}
             onSelect={value => {
-              console.log('select');
               onSetLinkedAccount(externalAccount, value);
             }}
             inputProps={{
