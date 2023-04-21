944c7ff30f6f69fd2142126a1a02b5c9d2855ef4
Jed Fox <git@jedfox.com>
2023-04-21 17:48:47 -0400

    Improve sidebar auto-floating behavior (#868)
    I got some feedback in the discord that this behavior was disruptive
    when zooming in.
    
    - I’ve reduced the breakpoint so hopefully the disruption of the
    transition is matched by a significant improvement in available space
    now.
    - Also the 2 places in the app that use window width (including here)
    now check for the width of the `<html>` tag, not the width of the
    viewport (those 2 values can be different when doing a pinch-zoom,
    causing undesirable layout shifts.)
    - Most of the logic has been rewritten to improve the transitions
    
    Mobile & desktop experience
    
    https://user-images.githubusercontent.com/25517624/233653721-b13c5e22-ae11-4bdf-a494-a6916556d05e.mov
    
    https://user-images.githubusercontent.com/25517624/233654784-b6cc1006-44ea-4066-be7a-8d0dd786fb5b.mov
    
    (I’d like tapping on something to close the sidebar on mobile, but that
    can be approached in a future PR)

--- packages/desktop-client/src/components/accounts/MobileTransaction.js
+++ packages/desktop-client/src/components/accounts/MobileTransaction.js
@@ -394,48 +394,46 @@ function ListBoxSection({ section, state }) {
   // The heading is rendered inside an <li> element, which contains
   // a <ul> with the child items.
   return (
-    <>
-      <li {...itemProps} style={{ width: '100%' }}>
-        {section.rendered && (
-          <div
-            {...headingProps}
-            style={{
-              ...styles.smallText,
-              backgroundColor: colors.n10,
-              borderBottom: `1px solid ${colors.n9}`,
-              borderTop: `1px solid ${colors.n9}`,
-              color: colors.n4,
-              display: 'flex',
-              justifyContent: 'center',
-              paddingBottom: 4,
-              paddingTop: 4,
-              position: 'sticky',
-              top: '0',
-              width: '100%',
-              zIndex: zIndices.SECTION_HEADING,
-            }}
-          >
-            {section.rendered}
-          </div>
-        )}
-        <ul
-          {...groupProps}
+    <li {...itemProps} style={{ width: '100%' }}>
+      {section.rendered && (
+        <div
+          {...headingProps}
           style={{
-            padding: 0,
-            listStyle: 'none',
+            ...styles.smallText,
+            backgroundColor: colors.n10,
+            borderBottom: `1px solid ${colors.n9}`,
+            borderTop: `1px solid ${colors.n9}`,
+            color: colors.n4,
+            display: 'flex',
+            justifyContent: 'center',
+            paddingBottom: 4,
+            paddingTop: 4,
+            position: 'sticky',
+            top: '0',
+            width: '100%',
+            zIndex: zIndices.SECTION_HEADING,
           }}
         >
-          {[...section.childNodes].map((node, index, nodes) => (
-            <Option
-              key={node.key}
-              item={node}
-              state={state}
-              isLast={index === nodes.length - 1}
-            />
-          ))}
-        </ul>
-      </li>
-    </>
+          {section.rendered}
+        </div>
+      )}
+      <ul
+        {...groupProps}
+        style={{
+          padding: 0,
+          listStyle: 'none',
+        }}
+      >
+        {[...section.childNodes].map((node, index, nodes) => (
+          <Option
+            key={node.key}
+            item={node}
+            state={state}
+            isLast={index === nodes.length - 1}
+          />
+        ))}
+      </ul>
+    </li>
   );
 }
 
