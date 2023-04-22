32e7ed3d2ecd0d5409b5fcc6e3a8d748d937a448
Aaron Eiche <aeiche@aaroneiche.com>
2023-04-22 14:35:31 -0700

    Adds CSV import option to skip empty lines.

--- packages/loot-core/src/server/accounts/parse-file.js
+++ packages/loot-core/src/server/accounts/parse-file.js
@@ -47,6 +47,7 @@ async function parseCSV(filepath, options = {}) {
       quote: '"',
       trim: true,
       relax_column_count: true,
+      skip_empty_lines: true,
     });
   } catch (err) {
     errors.push({
