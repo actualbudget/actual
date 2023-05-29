72c0b1244c5516460db03b9543577db781ccb85e
Jed Fox <git@jedfox.com>
2023-05-29 12:52:42 -0400

    Replace window.__history with history in upgrade-notifications.ts

--- packages/loot-core/src/client/upgrade-notifications.ts
+++ packages/loot-core/src/client/upgrade-notifications.ts
@@ -5,7 +5,6 @@ import * as Platform from './platform';
 export default function checkForUpgradeNotifications(
   addNotification,
   resetSync,
-  // Note: history is only available on desktop
   history,
 ) {
   // TODO: Probably should only show one of these at at time?
@@ -33,10 +32,9 @@ export default function checkForUpgradeNotifications(
             button: Platform.env !== 'mobile' && {
               title: 'Find schedules',
               action: async () => {
-                window.__history &&
-                  window.__history.push('/schedule/discover', {
-                    locationPtr: window.__history.location,
-                  });
+                history.push('/schedule/discover', {
+                  locationPtr: history.location,
+                });
               },
             },
             onClose: () => {
@@ -47,26 +45,24 @@ export default function checkForUpgradeNotifications(
         }
 
         case 'repair-splits':
-          if (history) {
-            addNotification({
-              type: 'message',
-              title: 'Split transactions now support transfers & payees',
-              message:
-                'The payee field is now available on split transactions, allowing you to perform transfers on individual split transactions.\n\nAll existing split transactions have a blank payee and we recommend using the tool below to set the payee from the parent. [View a video walkthrough](https://www.youtube.com/watch?v=5kTtAsB0Oqk)',
-              sticky: true,
-              id: 'repair-splits',
-              button: {
-                title: 'Repair splits...',
-                action: () =>
-                  history.push('/tools/fix-splits', {
-                    locationPtr: history.location,
-                  }),
-              },
-              onClose: () => {
-                send('seen-upgrade-notification', { type: 'repair-splits' });
-              },
-            });
-          }
+          addNotification({
+            type: 'message',
+            title: 'Split transactions now support transfers & payees',
+            message:
+              'The payee field is now available on split transactions, allowing you to perform transfers on individual split transactions.\n\nAll existing split transactions have a blank payee and we recommend using the tool below to set the payee from the parent. [View a video walkthrough](https://www.youtube.com/watch?v=5kTtAsB0Oqk)',
+            sticky: true,
+            id: 'repair-splits',
+            button: {
+              title: 'Repair splits...',
+              action: () =>
+                history.push('/tools/fix-splits', {
+                  locationPtr: history.location,
+                }),
+            },
+            onClose: () => {
+              send('seen-upgrade-notification', { type: 'repair-splits' });
+            },
+          });
           break;
 
         default:
