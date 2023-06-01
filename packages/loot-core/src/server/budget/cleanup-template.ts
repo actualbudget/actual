1d301ac78d7788db417aa2c78c12d0b639918833
shall0pass <20625555+shall0pass@users.noreply.github.com>
2023-06-01 09:25:00 -0500

    Bug: Cleanup script (#1084)

--- packages/loot-core/src/server/budget/cleanup-template.ts
+++ packages/loot-core/src/server/budget/cleanup-template.ts
@@ -82,11 +82,9 @@ async function processCleanup(month) {
       `budget-${sinkCategory[c].cat.id}`,
     );
     let categoryId = sinkCategory[c].cat.id;
+    let weight = sinkCategory[c].temp.filter(w => w.type === 'sink')[0].weight;
     let to_budget =
-      budgeted +
-      Math.round(
-        (sinkCategory[c].temp[0].weight / total_weight) * budgetAvailable,
-      );
+      budgeted + Math.round((weight / total_weight) * budgetAvailable);
     if (c === sinkCategory.length - 1) {
       let currentBudgetAvailable = await getSheetValue(sheetName, `to-budget`);
       if (to_budget > currentBudgetAvailable) {
