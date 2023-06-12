2f1347aa26ce317a0f55aaafc598f384b62222db
Shazib Hussain <me@shazib.com>
2023-06-12 20:09:38 +0100

    Delete unused functions

--- packages/desktop-client/src/hooks/index.js
+++ packages/desktop-client/src/hooks/index.js
@@ -2,20 +2,6 @@ import { useEffect, useRef } from 'react';
 
 import { setThemeColor } from '../util/withThemeColor';
 
-export function useScrollFlasher() {
-  let scrollRef = useRef(null);
-
-  useEffect(() => {
-    setTimeout(() => {
-      if (scrollRef.current) {
-        scrollRef.current.flashScrollIndicators();
-      }
-    }, 1000);
-  }, []);
-
-  return scrollRef;
-}
-
 export function useSetThemeColor(color) {
   useEffect(() => {
     setThemeColor(color);
