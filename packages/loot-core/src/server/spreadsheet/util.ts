26a5338c1717f9d816c7550465200113ea61fd51
Shazib Hussain <me@shazib.com>
2023-06-20 21:33:20 +0100

    Revert "Revert "Delete unused files, delete unused functions from loot-core""
    This reverts commit b3ec295203bbd607723b243f847e7b11e3015f37.

--- packages/loot-core/src/server/spreadsheet/util.ts
+++ packages/loot-core/src/server/spreadsheet/util.ts
@@ -12,28 +12,3 @@ export function unresolveName(name) {
 export function resolveName(sheet, name) {
   return sheet + '!' + name;
 }
-
-export function resolveNamesAsObjects(sheets) {
-  const cells = {};
-  Object.keys(sheets).forEach(sheetName => {
-    const sheet = sheets[sheetName];
-
-    Object.keys(sheet).forEach(name => {
-      const expr = sheet[name];
-      cells[resolveName(sheetName, name)] = expr;
-    });
-  });
-  return cells;
-}
-
-export function resolveNamesAsArrays(sheets) {
-  const cells = [];
-  Object.keys(sheets).forEach(sheetName => {
-    const sheet = sheets[sheetName];
-
-    sheet.forEach(name => {
-      cells.push(resolveName(sheetName, name));
-    });
-  });
-  return cells;
-}
