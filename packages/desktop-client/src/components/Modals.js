e3da6fe149a7035ccb4b92f85e2557771980251a
Jed Fox <git@jedfox.com>
2023-06-21 16:18:14 -0400

    Add keys to <Modals />

--- packages/desktop-client/src/components/Modals.js
+++ packages/desktop-client/src/components/Modals.js
@@ -42,209 +42,215 @@ function Modals({
 
   const syncServerStatus = useSyncServerStatus();
 
-  return modalStack.map(({ name, options = {} }, idx) => {
-    const modalProps = {
-      onClose: actions.popModal,
-      onBack: actions.popModal,
-      showBack: idx > 0,
-      isCurrent: idx === modalStack.length - 1,
-      isHidden,
-      stackIndex: idx,
-    };
+  return modalStack
+    .map(({ name, options = {} }, idx) => {
+      const modalProps = {
+        onClose: actions.popModal,
+        onBack: actions.popModal,
+        showBack: idx > 0,
+        isCurrent: idx === modalStack.length - 1,
+        isHidden,
+        stackIndex: idx,
+      };
 
-    switch (name) {
-      case 'import-transactions':
-        return <ImportTransactions modalProps={modalProps} options={options} />;
+      switch (name) {
+        case 'import-transactions':
+          return (
+            <ImportTransactions modalProps={modalProps} options={options} />
+          );
 
-      case 'add-account':
-        return (
-          <CreateAccount
-            modalProps={modalProps}
-            actions={actions}
-            syncServerStatus={syncServerStatus}
-          />
-        );
+        case 'add-account':
+          return (
+            <CreateAccount
+              modalProps={modalProps}
+              actions={actions}
+              syncServerStatus={syncServerStatus}
+            />
+          );
 
-      case 'add-local-account':
-        return (
-          <CreateLocalAccount
-            modalProps={modalProps}
-            actions={actions}
-            history={history}
-          />
-        );
+        case 'add-local-account':
+          return (
+            <CreateLocalAccount
+              modalProps={modalProps}
+              actions={actions}
+              history={history}
+            />
+          );
 
-      case 'close-account':
-        return (
-          <CloseAccount
-            modalProps={modalProps}
-            account={options.account}
-            balance={options.balance}
-            canDelete={options.canDelete}
-            accounts={accounts.filter(acct => acct.closed === 0)}
-            categoryGroups={categoryGroups}
-            actions={actions}
-          />
-        );
+        case 'close-account':
+          return (
+            <CloseAccount
+              modalProps={modalProps}
+              account={options.account}
+              balance={options.balance}
+              canDelete={options.canDelete}
+              accounts={accounts.filter(acct => acct.closed === 0)}
+              categoryGroups={categoryGroups}
+              actions={actions}
+            />
+          );
 
-      case 'select-linked-accounts':
-        return (
-          <SelectLinkedAccounts
-            modalProps={modalProps}
-            externalAccounts={options.accounts}
-            requisitionId={options.requisitionId}
-            localAccounts={accounts.filter(acct => acct.closed === 0)}
-            upgradingAccountId={options.upgradingAccountId}
-            actions={actions}
-          />
-        );
+        case 'select-linked-accounts':
+          return (
+            <SelectLinkedAccounts
+              modalProps={modalProps}
+              externalAccounts={options.accounts}
+              requisitionId={options.requisitionId}
+              localAccounts={accounts.filter(acct => acct.closed === 0)}
+              upgradingAccountId={options.upgradingAccountId}
+              actions={actions}
+            />
+          );
 
-      case 'configure-linked-accounts':
-        return (
-          <ConfigureLinkedAccounts
-            modalProps={modalProps}
-            institution={options.institution}
-            publicToken={options.publicToken}
-            accounts={options.accounts}
-            upgradingId={options.upgradingId}
-            actions={actions}
-          />
-        );
+        case 'configure-linked-accounts':
+          return (
+            <ConfigureLinkedAccounts
+              modalProps={modalProps}
+              institution={options.institution}
+              publicToken={options.publicToken}
+              accounts={options.accounts}
+              upgradingId={options.upgradingId}
+              actions={actions}
+            />
+          );
 
-      case 'confirm-category-delete':
-        return (
-          <ConfirmCategoryDelete
-            modalProps={modalProps}
-            actions={actions}
-            category={categories.find(c => c.id === options.category)}
-            group={categoryGroups.find(g => g.id === options.group)}
-            categoryGroups={categoryGroups}
-            onDelete={options.onDelete}
-          />
-        );
+        case 'confirm-category-delete':
+          return (
+            <ConfirmCategoryDelete
+              modalProps={modalProps}
+              actions={actions}
+              category={categories.find(c => c.id === options.category)}
+              group={categoryGroups.find(g => g.id === options.group)}
+              categoryGroups={categoryGroups}
+              onDelete={options.onDelete}
+            />
+          );
 
-      case 'load-backup':
-        return (
-          <LoadBackup
-            watchUpdates
-            budgetId={budgetId}
-            modalProps={modalProps}
-            actions={actions}
-          />
-        );
+        case 'load-backup':
+          return (
+            <LoadBackup
+              watchUpdates
+              budgetId={budgetId}
+              modalProps={modalProps}
+              actions={actions}
+            />
+          );
 
-      case 'manage-rules':
-        return (
-          <ManageRulesModal
-            history={history}
-            modalProps={modalProps}
-            payeeId={options.payeeId}
-          />
-        );
+        case 'manage-rules':
+          return (
+            <ManageRulesModal
+              history={history}
+              modalProps={modalProps}
+              payeeId={options.payeeId}
+            />
+          );
 
-      case 'edit-rule':
-        return (
-          <EditRule
-            history={history}
-            modalProps={modalProps}
-            defaultRule={options.rule}
-            onSave={options.onSave}
-          />
-        );
+        case 'edit-rule':
+          return (
+            <EditRule
+              history={history}
+              modalProps={modalProps}
+              defaultRule={options.rule}
+              onSave={options.onSave}
+            />
+          );
 
-      case 'merge-unused-payees':
-        return (
-          <MergeUnusedPayees
-            history={history}
-            modalProps={modalProps}
-            payeeIds={options.payeeIds}
-            targetPayeeId={options.targetPayeeId}
-          />
-        );
+        case 'merge-unused-payees':
+          return (
+            <MergeUnusedPayees
+              history={history}
+              modalProps={modalProps}
+              payeeIds={options.payeeIds}
+              targetPayeeId={options.targetPayeeId}
+            />
+          );
 
-      case 'plaid-external-msg':
-        return (
-          <PlaidExternalMsg
-            modalProps={modalProps}
-            actions={actions}
-            onMoveExternal={options.onMoveExternal}
-            onClose={() => {
-              options.onClose && options.onClose();
-              send('poll-web-token-stop');
-            }}
-            onSuccess={options.onSuccess}
-          />
-        );
+        case 'plaid-external-msg':
+          return (
+            <PlaidExternalMsg
+              modalProps={modalProps}
+              actions={actions}
+              onMoveExternal={options.onMoveExternal}
+              onClose={() => {
+                options.onClose && options.onClose();
+                send('poll-web-token-stop');
+              }}
+              onSuccess={options.onSuccess}
+            />
+          );
 
-      case 'nordigen-init':
-        return (
-          <NordigenInitialise
-            modalProps={modalProps}
-            onSuccess={options.onSuccess}
-          />
-        );
+        case 'nordigen-init':
+          return (
+            <NordigenInitialise
+              modalProps={modalProps}
+              onSuccess={options.onSuccess}
+            />
+          );
 
-      case 'nordigen-external-msg':
-        return (
-          <NordigenExternalMsg
-            modalProps={modalProps}
-            actions={actions}
-            onMoveExternal={options.onMoveExternal}
-            onClose={() => {
-              options.onClose && options.onClose();
-              send('nordigen-poll-web-token-stop');
-            }}
-            onSuccess={options.onSuccess}
-          />
-        );
+        case 'nordigen-external-msg':
+          return (
+            <NordigenExternalMsg
+              modalProps={modalProps}
+              actions={actions}
+              onMoveExternal={options.onMoveExternal}
+              onClose={() => {
+                options.onClose && options.onClose();
+                send('nordigen-poll-web-token-stop');
+              }}
+              onSuccess={options.onSuccess}
+            />
+          );
 
-      case 'create-encryption-key':
-        return (
-          <CreateEncryptionKey
-            key={name}
-            modalProps={modalProps}
-            actions={actions}
-            options={options}
-          />
-        );
+        case 'create-encryption-key':
+          return (
+            <CreateEncryptionKey
+              key={name}
+              modalProps={modalProps}
+              actions={actions}
+              options={options}
+            />
+          );
 
-      case 'fix-encryption-key':
-        return (
-          <FixEncryptionKey
-            key={name}
-            modalProps={modalProps}
-            actions={actions}
-            options={options}
-          />
-        );
+        case 'fix-encryption-key':
+          return (
+            <FixEncryptionKey
+              key={name}
+              modalProps={modalProps}
+              actions={actions}
+              options={options}
+            />
+          );
 
-      case 'edit-field':
-        return (
-          <EditField
-            key={name}
-            modalProps={modalProps}
-            actions={actions}
-            name={options.name}
-            onSubmit={options.onSubmit}
-          />
-        );
+        case 'edit-field':
+          return (
+            <EditField
+              key={name}
+              modalProps={modalProps}
+              actions={actions}
+              name={options.name}
+              onSubmit={options.onSubmit}
+            />
+          );
 
-      case 'budget-summary':
-        return (
-          <BudgetSummary
-            key={name}
-            modalProps={modalProps}
-            month={options.month}
-            actions={actions}
-            isGoalTemplatesEnabled={isGoalTemplatesEnabled}
-          />
-        );
+        case 'budget-summary':
+          return (
+            <BudgetSummary
+              key={name}
+              modalProps={modalProps}
+              month={options.month}
+              actions={actions}
+              isGoalTemplatesEnabled={isGoalTemplatesEnabled}
+            />
+          );
 
-      default:
-        console.error('Unknown modal:', name);
-        return null;
-    }
-  });
+        default:
+          console.error('Unknown modal:', name);
+          return null;
+      }
+    })
+    .map((modal, idx) => (
+      <React.Fragment key={modalStack[idx].name}>{modal}</React.Fragment>
+    ));
 }
 
 export default connect(
