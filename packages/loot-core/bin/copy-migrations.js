0483c4cb882f2b67d52894a0c3dbd9ecce62e640
Shazib Hussain <me@shazib.com>
2023-06-24 00:59:24 +0100

    Updates for building new 'api' package

---
+++ packages/loot-core/bin/copy-migrations.js
@@ -0,0 +1,12 @@
+#!usr/bin/env node
+const Path = require('path')
+const migrationUtil = require('./migration-util')
+
+const ROOT = process.cwd(); //Make path consistent with bash
+const DATA_DIR = Path.join(ROOT, process.argv[2]);
+
+async function main() {
+  await migrationUtil.copyMigrations(ROOT, DATA_DIR);
+}
+
+main();
