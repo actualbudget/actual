da0b6635990a5f90637db8d22ab9d1251238f857
Shazib Hussain <me@shazib.com>
2023-06-24 02:22:02 +0100

    electron bash to js conversion

---
+++ packages/desktop-electron/bin/update-client.js
@@ -0,0 +1,18 @@
+#!usr/bin/env node
+const { fsUtil, Path } = require('../../../bin/utils');
+
+const ROOT = process.cwd();
+const BUILD_DIR = Path.join(ROOT, '/client-build');
+
+async function main() {
+  fsUtil.rmdir(BUILD_DIR);
+  fsUtil.copyFiles(Path.join(ROOT, '../desktop-client/build'), BUILD_DIR);
+
+  // Remove the embedded backend for the browser version. Will improve this process
+  fsUtil.rmdir(Path.join(BUILD_DIR,'/data'));
+  fsUtil.emptyDir(BUILD_DIR, /.*kcab.*/);
+  fsUtil.emptyDir(BUILD_DIR, /.*wasm/);
+  fsUtil.emptyDir(BUILD_DIR, /.*map/);
+}
+
+main();
