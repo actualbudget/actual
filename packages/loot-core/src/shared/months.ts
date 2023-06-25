a96705efd28c01ef61bfe1a33f26521eca683032
Tom French <tom@tomfren.ch>
2023-06-25 15:43:44 +0100

    chore: add a shared `DateLike` type alias for `months.ts`

--- packages/loot-core/src/shared/months.ts
+++ packages/loot-core/src/shared/months.ts
@@ -1,7 +1,9 @@
 import * as d from 'date-fns';
 import memoizeOne from 'memoize-one';
 
-function _parse(value: string | Date): Date {
+type DateLike = string | Date;
+
+function _parse(value: DateLike): Date {
   if (typeof value === 'string') {
     // Dates are hard. We just want to deal with months in the format
     // 2020-01 and days in the format 2020-01-01, but life is never
@@ -71,15 +73,15 @@ function _parse(value: string | Date): Date {
 
 export const parseDate = _parse;
 
-export function yearFromDate(date: string | Date): string {
+export function yearFromDate(date: DateLike): string {
   return d.format(_parse(date), 'yyyy');
 }
 
-export function monthFromDate(date: string | Date): string {
+export function monthFromDate(date: DateLike): string {
   return d.format(_parse(date), 'yyyy-MM');
 }
 
-export function dayFromDate(date: string | Date): string {
+export function dayFromDate(date: DateLike): string {
   return d.format(_parse(date), 'yyyy-MM-dd');
 }
 
@@ -99,44 +101,41 @@ export function currentDay(): string {
   }
 }
 
-export function nextMonth(month: string | Date): string {
+export function nextMonth(month: DateLike): string {
   return d.format(d.addMonths(_parse(month), 1), 'yyyy-MM');
 }
 
-export function prevMonth(month: string | Date): string {
+export function prevMonth(month: DateLike): string {
   return d.format(d.subMonths(_parse(month), 1), 'yyyy-MM');
 }
 
-export function addMonths(month: string | Date, n: number): string {
+export function addMonths(month: DateLike, n: number): string {
   return d.format(d.addMonths(_parse(month), n), 'yyyy-MM');
 }
 
-export function subMonths(month: string | Date, n: number): string {
+export function subMonths(month: DateLike, n: number): string {
   return d.format(d.subMonths(_parse(month), n), 'yyyy-MM');
 }
 
-export function addDays(day: string | Date, n: number): string {
+export function addDays(day: DateLike, n: number): string {
   return d.format(d.addDays(_parse(day), n), 'yyyy-MM-dd');
 }
 
-export function subDays(day: string | Date, n: number): string {
+export function subDays(day: DateLike, n: number): string {
   return d.format(d.subDays(_parse(day), n), 'yyyy-MM-dd');
 }
 
-export function isBefore(
-  month1: string | Date,
-  month2: string | Date,
-): boolean {
+export function isBefore(month1: DateLike, month2: DateLike): boolean {
   return d.isBefore(_parse(month1), _parse(month2));
 }
 
-export function isAfter(month1: string | Date, month2: string | Date): boolean {
+export function isAfter(month1: DateLike, month2: DateLike): boolean {
   return d.isAfter(_parse(month1), _parse(month2));
 }
 
 // TODO: This doesn't really fit in this module anymore, should
 // probably live elsewhere
-export function bounds(month: string | Date): { start: number; end: number } {
+export function bounds(month: DateLike): { start: number; end: number } {
   return {
     start: parseInt(d.format(d.startOfMonth(_parse(month)), 'yyyyMMdd')),
     end: parseInt(d.format(d.endOfMonth(_parse(month)), 'yyyyMMdd')),
@@ -144,8 +143,8 @@ export function bounds(month: string | Date): { start: number; end: number } {
 }
 
 export function _range(
-  start: string | Date,
-  end: string | Date,
+  start: DateLike,
+  end: DateLike,
   inclusive = false,
 ): string[] {
   const months = [];
@@ -162,20 +161,17 @@ export function _range(
   return months;
 }
 
-export function range(start: string | Date, end: string | Date): string[] {
+export function range(start: DateLike, end: DateLike): string[] {
   return _range(start, end);
 }
 
-export function rangeInclusive(
-  start: string | Date,
-  end: string | Date,
-): string[] {
+export function rangeInclusive(start: DateLike, end: DateLike): string[] {
   return _range(start, end, true);
 }
 
 export function _dayRange(
-  start: string | Date,
-  end: string | Date,
+  start: DateLike,
+  end: DateLike,
   inclusive = false,
 ): string[] {
   const days = [];
@@ -192,11 +188,11 @@ export function _dayRange(
   return days;
 }
 
-export function dayRange(start: string | Date, end: string | Date) {
+export function dayRange(start: DateLike, end: DateLike) {
   return _dayRange(start, end);
 }
 
-export function dayRangeInclusive(start: string | Date, end: string | Date) {
+export function dayRangeInclusive(start: DateLike, end: DateLike) {
   return _dayRange(start, end, true);
 }
 
