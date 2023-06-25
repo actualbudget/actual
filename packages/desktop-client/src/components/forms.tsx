cb922aa2b1d563a165db50af9acc2022b75dcf9f
biohzrddd <10577752+biohzrddd@users.noreply.github.com>
2023-06-25 12:10:20 +0100

    Simple checkbox!

--- packages/desktop-client/src/components/forms.tsx
+++ packages/desktop-client/src/components/forms.tsx
@@ -75,54 +75,10 @@ export const Checkbox = (props: CheckboxProps) => {
       {...css(
         [
           {
-            position: 'relative',
-            margin: 0,
-            marginRight: 6,
-            width: 15,
-            height: 15,
-            appearance: 'none',
-            outline: 0,
-            border: '1px solid ' + colorsm.formInputBorder,
-            borderRadius: 4,
-            display: 'flex',
-            alignItems: 'center',
-            justifyContent: 'center',
-            color: 'none',
-            backgroundColor: 'none',
-            ':checked': {
-              border: '1px solid ' + colorsm.formInputBorderSelected,
-              backgroundColor: colorsm.formInputBackgroundSelected,
-              color: colorsm.formInputTextSelected,
-              '::after': {
-                display: 'block',
-                background:
-                  colorsm.formInputBackgroundSelected +
-                  // eslint-disable-next-line rulesdir/typography
-                  ' url(\'data:image/svg+xml; utf8,<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path fill="' +
-                  // TODO: The # needs escaped for the SVG to render the checkmark with color. Really we need to replace this entire component with soemthing like SelectCell
-                  colorsm.formInputTextSelected +
-                  // eslint-disable-next-line rulesdir/typography
-                  '" d="M0 11l2-2 5 5L18 3l2 2L7 18z"/></svg>\') 9px 9px',
-                width: 9,
-                height: 9,
-                content: ' ',
-              },
-            },
-            '&.focus-visible:focus': {
-              '::before': {
-                position: 'absolute',
-                top: -5,
-                bottom: -5,
-                left: -5,
-                right: -5,
-                border: '2px solid ' + colorsm.formInputBorderSelected,
-                borderRadius: 6,
-                content: ' ',
-              },
-            },
+            accentColor: colorsm.formInputBackgroundSelected,
           },
         ],
-        props.style,
+        props.styles,
       )}
     />
   );
