15e1fc2193c8bea1c5a5d3592e5d2087a40bd467
biohzrddd <10577752+biohzrddd@users.noreply.github.com>
2023-06-26 23:11:46 +0100

    now no eslint error!?

--- packages/desktop-client/src/components/manager/DeleteFile.js
+++ packages/desktop-client/src/components/manager/DeleteFile.js
@@ -3,7 +3,6 @@ import React, { useState } from 'react';
 import { colorsm } from '../../style';
 import { View, Text, Modal, ButtonWithLoading } from '../common';
 
-// eslint-disable-next-line import/no-unused-modules
 export default function DeleteMenu({ modalProps, actions, file }) {
   let [loadingState, setLoadingState] = useState(null);
 
