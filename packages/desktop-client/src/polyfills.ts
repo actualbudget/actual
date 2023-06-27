196ca0a01d7df1e33b3725d0ea08485709fb767f
Jed Fox <git@jedfox.com>
2023-06-27 17:39:56 -0400

    quotes

--- packages/desktop-client/src/polyfills.ts
+++ packages/desktop-client/src/polyfills.ts
@@ -1,7 +1,7 @@
 export default async function installPolyfills(): Promise<void> {
   if ('ResizeObserver' in window === false) {
     const module = await import(
-      /* webpackChunkName: resize-observer-polyfill */ '@juggle/resize-observer'
+      /* webpackChunkName: 'resize-observer-polyfill' */ '@juggle/resize-observer'
     );
     window.ResizeObserver = module.ResizeObserver;
   }
