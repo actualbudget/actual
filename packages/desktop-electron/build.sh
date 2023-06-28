db17f72543cc0cda1b011289bffcc01c3b528655
Shazib Hussain <me@shazib.com>
2023-06-28 22:09:28 +0100

    Tidy up electron (#1221)
    Bumping package version to match actual/web, and deleting two scripts
    i'm pretty sure are legacy/unused.

--- packages/desktop-electron/build.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-find ../loot-core \
-     -not -ipath '*src/client*' \
-     -not -ipath '*src/bin*' \
-     -not -ipath '*tests*' \
-     -not -iname '*.test.js' \
-     -not -iname '.#*' |
-    xargs ../../node_modules/.bin/babel --plugins babel-plugin-transform-es2015-modules-commonjs,babel-plugin-transform-object-rest-spread --no-babelrc -d lib-node
