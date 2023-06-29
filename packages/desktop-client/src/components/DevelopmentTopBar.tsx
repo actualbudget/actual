205be8856d9e92ad2c809ae0c6ed148654a6afae
Matiss Janis Aboltins <matiss@mja.lv>
2023-06-29 21:26:13 +0100

    :sparkles:  add netlify development banner

---
+++ packages/desktop-client/src/components/DevelopmentTopBar.tsx
@@ -0,0 +1,31 @@
+import { colors } from '../style';
+
+import { ExternalLink } from './common';
+import View from './common/View';
+
+export default function DevelopmentTopBar() {
+  return (
+    <View
+      style={[
+        {
+          padding: '6px 20px',
+          display: 'flex',
+          flexDirection: 'row',
+          justifyContent: 'space-between',
+          color: colors.y2,
+          backgroundColor: colors.y8,
+          borderBottom: `1px solid ${colors.y6}`,
+        },
+      ]}
+    >
+      <View>This is a development build of Actual.</View>
+      <View>
+        <ExternalLink
+          to={`https://github.com/actualbudget/actual/pull/${process.env.REACT_APP_REVIEW_ID}`}
+        >
+          Open the PR: #{process.env.REVIEW_ID}
+        </ExternalLink>
+      </View>
+    </View>
+  );
+}
