21fe55d00199ca1f1a33794d590875ded37f9e7c
Matiss Janis Aboltins <matiss@mja.lv>
2023-06-29 17:22:24 +0100

    :fire:  remove unused scripts; simplify version script

--- packages/desktop-client/src/browser-preload.browser.js
+++ packages/desktop-client/src/browser-preload.browser.js
@@ -1,5 +1,7 @@
 import { initBackend as initSQLBackend } from 'absurd-sql/dist/indexeddb-main-thread';
 
+import packageJson from '../package.json';
+
 const backendWorkerUrl = new URL('./browser-server.js', import.meta.url);
 
 // This file installs global variables that the app expects.
@@ -8,7 +10,7 @@ const backendWorkerUrl = new URL('./browser-server.js', import.meta.url);
 // everything else.
 
 let IS_DEV = process.env.NODE_ENV === 'development';
-let ACTUAL_VERSION = process.env.REACT_APP_ACTUAL_VERSION;
+let ACTUAL_VERSION = packageJson.version;
 
 // *** Start the backend ***
 let worker;
