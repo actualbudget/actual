df582f5a50165cc8555b94e61a5dde22f137cd65
Jed Fox <git@jedfox.com>
2023-06-30 18:05:40 -0400

    Asynchronously load in the desktop/mobile components

---
+++ packages/desktop-client/src/components/responsive/wide.ts
@@ -0,0 +1,12 @@
+export { default as Reports } from '../reports';
+export { default as Budget } from '../budget';
+
+export { default as Schedules } from '../schedules';
+export { default as EditSchedule } from '../schedules/EditSchedule';
+export { default as LinkSchedule } from '../schedules/LinkSchedule';
+export { default as DiscoverSchedules } from '../schedules/DiscoverSchedules';
+
+export { default as NordigenLink } from '../nordigen/NordigenLink';
+
+export { default as Accounts } from '../accounts/Account';
+export { default as Account } from '../accounts/Account';
