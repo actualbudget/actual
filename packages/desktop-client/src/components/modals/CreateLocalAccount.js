8beec7e5371535627060785a598da0ad40c8854a
Jed Fox <git@jedfox.com>
2023-06-30 17:01:43 -0400

    Remove usage of Formik (#1212)

--- packages/desktop-client/src/components/modals/CreateLocalAccount.js
+++ packages/desktop-client/src/components/modals/CreateLocalAccount.js
@@ -1,7 +1,5 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
-
-import { Formik } from 'formik';
 
 import { toRelaxedNumber } from 'loot-core/src/shared/util';
 
@@ -21,140 +19,142 @@ import {
 
 function CreateLocalAccount({ modalProps, actions }) {
   let navigate = useNavigate();
+  let [name, setName] = useState('');
+  let [offbudget, setOffbudget] = useState(false);
+  let [balance, setBalance] = useState('0');
+
+  let [nameError, setNameError] = useState(false);
+  let [balanceError, setBalanceError] = useState(false);
+
+  let validateBalance = balance => !isNaN(parseFloat(balance));
+
   return (
     <Modal title="Create Local Account" {...modalProps} showBack={false}>
       {() => (
         <View>
-          <Formik
-            validateOnChange={false}
-            initialValues={{ name: '', balance: '0' }}
-            validate={() => ({})}
-            onSubmit={async (values, { setErrors }) => {
-              const errors = {};
-              if (!values.name) {
-                errors.name = 'required';
-              }
-              if (isNaN(parseFloat(values.balance))) {
-                errors.balance = 'format';
-              }
-              setErrors(errors);
+          <form
+            onSubmit={async event => {
+              event.preventDefault();
 
-              if (Object.keys(errors).length === 0) {
+              let nameError = !name;
+              setNameError(nameError);
+
+              let balanceError = !validateBalance(balance);
+              setBalanceError(balanceError);
+
+              if (!nameError && !balanceError) {
                 modalProps.onClose();
                 let id = await actions.createAccount(
-                  values.name,
-                  toRelaxedNumber(values.balance),
-                  values.offbudget,
+                  name,
+                  toRelaxedNumber(balance),
+                  offbudget,
                 );
                 navigate('/accounts/' + id);
               }
             }}
-            render={({
-              values,
-              errors,
-              touched,
-              handleChange,
-              handleBlur,
-              handleSubmit,
-              isSubmitting,
-              setFieldValue,
-            }) => (
-              <form onSubmit={handleSubmit}>
-                <InlineField label="Name" width="75%">
-                  <InitialFocus>
-                    <Input
-                      name="name"
-                      value={values.name}
-                      onChange={handleChange}
-                      onBlur={handleBlur}
-                      style={{ flex: 1 }}
-                    />
-                  </InitialFocus>
-                </InlineField>
-                {errors.name && (
-                  <FormError style={{ marginLeft: 75 }}>
-                    Name is required
-                  </FormError>
-                )}
+          >
+            <InlineField label="Name" width="75%">
+              <InitialFocus>
+                <Input
+                  name="name"
+                  value={name}
+                  onChange={event => setName(event.target.value)}
+                  onBlur={event => {
+                    let name = event.target.value.trim();
+                    setName(name);
+                    if (name && nameError) {
+                      setNameError(false);
+                    }
+                  }}
+                  style={{ flex: 1 }}
+                />
+              </InitialFocus>
+            </InlineField>
+            {nameError && (
+              <FormError style={{ marginLeft: 75 }}>Name is required</FormError>
+            )}
 
-                <View
+            <View
+              style={{
+                width: '75%',
+                flexDirection: 'row',
+                justifyContent: 'flex-end',
+              }}
+            >
+              <View style={{ flexDirection: 'column' }}>
+                <label
                   style={{
-                    width: '75%',
-                    flexDirection: 'row',
+                    userSelect: 'none',
+                    textAlign: 'right',
+                    width: '100%',
+                    display: 'flex',
+                    verticalAlign: 'center',
                     justifyContent: 'flex-end',
                   }}
+                  htmlFor="offbudget"
                 >
-                  <View style={{ flexDirection: 'column' }}>
-                    <label
-                      style={{
-                        userSelect: 'none',
-                        textAlign: 'right',
-                        width: '100%',
-                        display: 'flex',
-                        verticalAlign: 'center',
-                        justifyContent: 'flex-end',
-                      }}
-                      htmlFor="offbudget"
-                    >
-                      <input
-                        id="offbudget"
-                        name="offbudget"
-                        type="checkbox"
-                        checked={!!values.offbudget}
-                        onChange={handleChange}
-                        onBlur={handleBlur}
-                      />
-                      Off-budget
-                    </label>
-                    <div
-                      style={{
-                        textAlign: 'right',
-                        fontSize: '0.7em',
-                        color: colors.n5,
-                        marginTop: 3,
-                      }}
-                    >
-                      <Text>
-                        This cannot be changed later. <br /> {'\n'}
-                        See{' '}
-                        <ExternalLink
-                          linkColor="muted"
-                          to="https://actualbudget.org/docs/accounts/#off-budget-accounts"
-                        >
-                          Accounts Overview
-                        </ExternalLink>{' '}
-                        for more information.
-                      </Text>
-                    </div>
-                  </View>
-                </View>
-
-                <InlineField label="Balance" width="75%">
-                  <Input
-                    name="balance"
-                    value={values.balance}
-                    onChange={handleChange}
-                    onBlur={handleBlur}
-                    style={{ flex: 1 }}
+                  <input
+                    id="offbudget"
+                    name="offbudget"
+                    type="checkbox"
+                    checked={offbudget}
+                    onChange={event => setOffbudget(event.target.checked)}
                   />
-                </InlineField>
-                {errors.balance && (
-                  <FormError style={{ marginLeft: 75 }}>
-                    Balance must be a number
-                  </FormError>
-                )}
+                  Off-budget
+                </label>
+                <div
+                  style={{
+                    textAlign: 'right',
+                    fontSize: '0.7em',
+                    color: colors.n5,
+                    marginTop: 3,
+                  }}
+                >
+                  <Text>
+                    This cannot be changed later. <br /> {'\n'}
+                    See{' '}
+                    <ExternalLink
+                      linkColor="muted"
+                      to="https://actualbudget.org/docs/accounts/#off-budget-accounts"
+                    >
+                      Accounts Overview
+                    </ExternalLink>{' '}
+                    for more information.
+                  </Text>
+                </div>
+              </View>
+            </View>
 
-                <ModalButtons>
-                  <Button onClick={() => modalProps.onBack()} type="button">
-                    Back
-                  </Button>
-                  <Button primary style={{ marginLeft: 10 }}>
-                    Create
-                  </Button>
-                </ModalButtons>
-              </form>
+            <InlineField label="Balance" width="75%">
+              <Input
+                name="balance"
+                value={balance}
+                onChange={event => setBalance(event.target.value)}
+                onBlur={event => {
+                  let balance = event.target.value.trim();
+                  setBalance(balance);
+                  if (validateBalance(balance) && balanceError) {
+                    setBalanceError(false);
+                  }
+                }}
+                style={{ flex: 1 }}
+              />
+            </InlineField>
+            {balanceError && (
+              <FormError style={{ marginLeft: 75 }}>
+                Balance must be a number
+              </FormError>
             )}
-          />
+
+            <ModalButtons>
+              <Button onClick={() => modalProps.onBack()} type="button">
+                Back
+              </Button>
+              <Button primary style={{ marginLeft: 10 }}>
+                Create
+              </Button>
+            </ModalButtons>
+          </form>
         </View>
       )}
     </Modal>
