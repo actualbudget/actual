actualbudget/actual

commit 3f6332fd0c919709c1660e816c09cfd0c98e5144

chore: add types to `crdt` package (#1076)
This PR adds types to the `crdt` directory to make the structure of the
merkle tree (really a merkle radix trie) clearer.

---------

Co-authored-by: Jed Fox <git@jedfox.com>
Co-authored-by: Matiss Janis Aboltins <matiss@mja.lv>

packages/crdt/src/crdt/merkle.ts
@@ -7,11 +7,27 @@
 // * Need to check to make sure if account exists when handling
 // * transaction changes in syncing
 
-export function getKeys(trie) {
-  return Object.keys(trie).filter(x => x !== 'hash');
+import { Timestamp } from './timestamp';
+
+/**
+ * Represents a node within a trinary radix trie.
+ */
+export type TrieNode = {
+  '0'?: TrieNode;
+  '1'?: TrieNode;
+  '2'?: TrieNode;
+  hash: number;
+};
+
+export function emptyTrie(): TrieNode {
+  return { hash: 0 };
 }
 
-export function keyToTimestamp(key) {
+export function getKeys(trie: TrieNode): ('0' | '1' | '2')[] {
+  return Object.keys(trie).filter(x => x !== 'hash') as ('0' | '1' | '2')[];
+}
+
+export function keyToTimestamp(key: string): number {
   // 16 is the length of the base 3 value of the current time in
   // minutes. Ensure it's padded to create the full value
   let fullkey = key + '0'.repeat(16 - key.length);
@@ -20,7 +36,10 @@ export function keyToTimestamp(key) {
   return parseInt(fullkey, 3) * 1000 * 60;
 }
 
-export function insert(trie, timestamp) {
+/**
+ * Mutates `trie` to insert a node at `timestamp`
+ */
+export function insert(trie: TrieNode, timestamp: Timestamp) {
   let hash = timestamp.hash();
   let key = Number(Math.floor(timestamp.millis() / 1000 / 60)).toString(3);
 
@@ -28,7 +47,7 @@ export function insert(trie, timestamp) {
   return insertKey(trie, key, hash);
 }
 
-function insertKey(trie, key, hash) {
+function insertKey(trie: TrieNode, key: string, hash: number) {
   if (key.length === 0) {
     return trie;
   }
@@ -41,15 +60,15 @@ function insertKey(trie, key, hash) {
   });
 }
 
-export function build(timestamps) {
-  let trie = {};
+export function build(timestamps: Timestamp[]) {
+  let trie = emptyTrie();
   for (let timestamp of timestamps) {
     insert(trie, timestamp);
   }
   return trie;
 }
 
-export function diff(trie1, trie2) {
+export function diff(trie1: TrieNode, trie2: TrieNode): number {
   if (trie1.hash === trie2.hash) {
     return null;
   }
@@ -103,12 +122,12 @@ export function diff(trie1, trie2) {
     }
 
     k += diffkey;
-    node1 = node1[diffkey] || {};
-    node2 = node2[diffkey] || {};
+    node1 = node1[diffkey] || emptyTrie();
+    node2 = node2[diffkey] || emptyTrie();
   }
 }
 
-export function prune(trie, n = 2) {
+export function prune(trie: TrieNode, n = 2): TrieNode {
   // Do nothing if empty
   if (!trie.hash) {
     return trie;
@@ -118,12 +137,16 @@ export function prune(trie, n = 2) {
   keys.sort();
 
   let next = { hash: trie.hash };
-  keys = keys.slice(-n).map(k => (next[k] = prune(trie[k], n)));
+
+  // Prune child nodes.
+  for (let k of keys.slice(-n)) {
+    next[k] = prune(trie[k], n);
+  }
 
   return next;
 }
 
-export function debug(trie, k = '', indent = 0) {
+export function debug(trie: TrieNode, k = '', indent = 0) {
   const str =
     ' '.repeat(indent) +
     (k !== '' ? `k: ${k} ` : '') +

packages/crdt/src/crdt/timestamp.ts
@@ -1,6 +1,8 @@
 import murmurhash from 'murmurhash';
 import { v4 as uuidv4 } from 'uuid';
 
+import { TrieNode } from './merkle';
+
 /**
  * Hybrid Unique Logical Clock (HULC) timestamp generator
  *
@@ -24,29 +26,37 @@ import { v4 as uuidv4 } from 'uuid';
  * http://www.cse.buffalo.edu/tech-reports/2014-04.pdf
  */
 
-// A mutable global clock
-let clock = null;
+export type Clock = {
+  timestamp: MutableTimestamp;
+  merkle: TrieNode;
+};
 
-export function setClock(clock_) {
+// A mutable global clock
+let clock: Clock = null;
+
+export function setClock(clock_: Clock): void {
   clock = clock_;
 }
 
-export function getClock() {
+export function getClock(): Clock {
   return clock;
 }
 
-export function makeClock(timestamp, merkle = {}) {
+export function makeClock(
+  timestamp: Timestamp,
+  merkle: TrieNode = { hash: 0 },
+) {
   return { timestamp: MutableTimestamp.from(timestamp), merkle };
 }
 
-export function serializeClock(clock) {
+export function serializeClock(clock: Clock): string {
   return JSON.stringify({
     timestamp: clock.timestamp.toString(),
     merkle: clock.merkle,
   });
 }
 
-export function deserializeClock(clock) {
+export function deserializeClock(clock: string): Clock {
   let data;
   try {
     data = JSON.parse(clock);
@@ -79,18 +89,7 @@ const MAX_NODE_LENGTH = 16;
  * timestamp instance class
  */
 export class Timestamp {
-  static init;
-  static max;
-  static parse;
-  static recv;
-  static send;
-  static since;
-  static zero;
-  static ClockDriftError;
-  static DuplicateNodeError;
-  static OverflowError;
-
-  _state;
+  _state: { millis: number; counter: number; node: string };
 
   constructor(millis: number, counter: number, node: string) {
     this._state = {
@@ -127,10 +126,211 @@ export class Timestamp {
   hash() {
     return murmurhash.v3(this.toString());
   }
+
+  // Timestamp generator initialization
+  // * sets the node ID to an arbitrary value
+  // * useful for mocking/unit testing
+  static init(options: { maxDrift?: number; node?: string } = {}) {
+    if (options.maxDrift) {
+      config.maxDrift = options.maxDrift;
+    }
+
+    setClock(
+      makeClock(
+        new Timestamp(
+          0,
+          0,
+          options.node
+            ? ('0000000000000000' + options.node).toString().slice(-16)
+            : '',
+        ),
+      ),
+    );
+  }
+
+  /**
+   * maximum timestamp
+   */
+  static max = Timestamp.parse(
+    '9999-12-31T23:59:59.999Z-FFFF-FFFFFFFFFFFFFFFF',
+  );
+
+  /**
+   * timestamp parsing
+   * converts a fixed-length string timestamp to the structured value
+   */
+  static parse(timestamp: string | Timestamp): Timestamp | null {
+    if (timestamp instanceof Timestamp) {
+      return timestamp;
+    }
+    if (typeof timestamp === 'string') {
+      let parts = timestamp.split('-');
+      if (parts && parts.length === 5) {
+        let millis = Date.parse(parts.slice(0, 3).join('-')).valueOf();
+        let counter = parseInt(parts[3], 16);
+        let node = parts[4];
+        if (
+          !isNaN(millis) &&
+          millis >= 0 &&
+          !isNaN(counter) &&
+          counter <= MAX_COUNTER &&
+          typeof node === 'string' &&
+          node.length <= MAX_NODE_LENGTH
+        ) {
+          return new Timestamp(millis, counter, node);
+        }
+      }
+    }
+    return null;
+  }
+
+  /**
+   * Timestamp send. Generates a unique, monotonic timestamp suitable
+   * for transmission to another system in string format
+   */
+  static send(): Timestamp | null {
+    if (!clock) {
+      return null;
+    }
+
+    // retrieve the local wall time
+    let phys = Date.now();
+
+    // unpack the clock.timestamp logical time and counter
+    let lOld = clock.timestamp.millis();
+    let cOld = clock.timestamp.counter();
+
+    // calculate the next logical time and counter
+    // * ensure that the logical time never goes backward
+    // * increment the counter if phys time does not advance
+    let lNew = Math.max(lOld, phys);
+    let cNew = lOld === lNew ? cOld + 1 : 0;
+
+    // check the result for drift and counter overflow
+    if (lNew - phys > config.maxDrift) {
+      throw new Timestamp.ClockDriftError(lNew, phys, config.maxDrift);
+    }
+    if (cNew > MAX_COUNTER) {
+      throw new Timestamp.OverflowError();
+    }
+
+    // repack the logical time/counter
+    clock.timestamp.setMillis(lNew);
+    clock.timestamp.setCounter(cNew);
+
+    return new Timestamp(
+      clock.timestamp.millis(),
+      clock.timestamp.counter(),
+      clock.timestamp.node(),
+    );
+  }
+
+  // Timestamp receive. Parses and merges a timestamp from a remote
+  // system with the local timeglobal uniqueness and monotonicity are
+  // preserved
+  static recv(msg: Timestamp): Timestamp | null {
+    if (!clock) {
+      return null;
+    }
+
+    // retrieve the local wall time
+    let phys = Date.now();
+
+    // unpack the message wall time/counter
+    let lMsg = msg.millis();
+    let cMsg = msg.counter();
+
+    // assert the node id and remote clock drift
+    // if (msg.node() === clock.timestamp.node()) {
+    //   throw new Timestamp.DuplicateNodeError(clock.timestamp.node());
+    // }
+    if (lMsg - phys > config.maxDrift) {
+      throw new Timestamp.ClockDriftError();
+    }
+
+    // unpack the clock.timestamp logical time and counter
+    let lOld = clock.timestamp.millis();
+    let cOld = clock.timestamp.counter();
+
+    // calculate the next logical time and counter
+    // . ensure that the logical time never goes backward
+    // . if all logical clocks are equal, increment the max counter
+    // . if max = old > message, increment local counter
+    // . if max = messsage > old, increment message counter
+    // . otherwise, clocks are monotonic, reset counter
+    let lNew = Math.max(Math.max(lOld, phys), lMsg);
+    let cNew =
+      lNew === lOld && lNew === lMsg
+        ? Math.max(cOld, cMsg) + 1
+        : lNew === lOld
+        ? cOld + 1
+        : lNew === lMsg
+        ? cMsg + 1
+        : 0;
+
+    // check the result for drift and counter overflow
+    if (lNew - phys > config.maxDrift) {
+      throw new Timestamp.ClockDriftError();
+    }
+    if (cNew > MAX_COUNTER) {
+      throw new Timestamp.OverflowError();
+    }
+
+    // repack the logical time/counter
+    clock.timestamp.setMillis(lNew);
+    clock.timestamp.setCounter(cNew);
+
+    return new Timestamp(
+      clock.timestamp.millis(),
+      clock.timestamp.counter(),
+      clock.timestamp.node(),
+    );
+  }
+
+  /**
+   * zero/minimum timestamp
+   */
+  static zero = Timestamp.parse(
+    '1970-01-01T00:00:00.000Z-0000-0000000000000000',
+  );
+
+  static since = isoString => isoString + '-0000-0000000000000000';
+
+  /**
+   * error classes
+   */
+  static DuplicateNodeError = class DuplicateNodeError extends Error {
+    constructor(node: string) {
+      super('duplicate node identifier ' + node);
+      this.name = 'DuplicateNodeError';
+    }
+  };
+
+  static ClockDriftError = class ClockDriftError extends Error {
+    constructor(...args: unknown[]) {
+      super(
+        ['maximum clock drift exceeded'].concat(args as string[]).join(' '),
+      );
+      this.name = 'ClockDriftError';
+    }
+  };
+
+  static OverflowError = class OverflowError extends Error {
+    constructor() {
+      super('timestamp counter overflow');
+      this.name = 'OverflowError';
+    }
+  };
 }
 
 class MutableTimestamp extends Timestamp {
-  static from;
+  static from(timestamp) {
+    return new MutableTimestamp(
+      timestamp.millis(),
+      timestamp.counter(),
+      timestamp.node(),
+    );
+  }
 
   setMillis(n) {
     this._state.millis = n;
@@ -144,205 +344,3 @@ class MutableTimestamp extends Timestamp {
     this._state.node = n;
   }
 }
-
-MutableTimestamp.from = timestamp => {
-  return new MutableTimestamp(
-    timestamp.millis(),
-    timestamp.counter(),
-    timestamp.node(),
-  );
-};
-
-// Timestamp generator initialization
-// * sets the node ID to an arbitrary value
-// * useful for mocking/unit testing
-Timestamp.init = function (options: { maxDrift?: number; node?: string } = {}) {
-  if (options.maxDrift) {
-    config.maxDrift = options.maxDrift;
-  }
-
-  setClock(
-    makeClock(
-      new Timestamp(
-        0,
-        0,
-        options.node
-          ? ('0000000000000000' + options.node).toString().slice(-16)
-          : '',
-      ),
-    ),
-  );
-};
-
-/**
- * Timestamp send. Generates a unique, monotonic timestamp suitable
- * for transmission to another system in string format
- */
-Timestamp.send = function () {
-  if (!clock) {
-    return null;
-  }
-
-  // retrieve the local wall time
-  let phys = Date.now();
-
-  // unpack the clock.timestamp logical time and counter
-  let lOld = clock.timestamp.millis();
-  let cOld = clock.timestamp.counter();
-
-  // calculate the next logical time and counter
-  // * ensure that the logical time never goes backward
-  // * increment the counter if phys time does not advance
-  let lNew = Math.max(lOld, phys);
-  let cNew = lOld === lNew ? cOld + 1 : 0;
-
-  // check the result for drift and counter overflow
-  if (lNew - phys > config.maxDrift) {
-    throw new Timestamp.ClockDriftError(lNew, phys, config.maxDrift);
-  }
-  if (cNew > MAX_COUNTER) {
-    throw new Timestamp.OverflowError();
-  }
-
-  // repack the logical time/counter
-  clock.timestamp.setMillis(lNew);
-  clock.timestamp.setCounter(cNew);
-
-  return new Timestamp(
-    clock.timestamp.millis(),
-    clock.timestamp.counter(),
-    clock.timestamp.node(),
-  );
-};
-
-// Timestamp receive. Parses and merges a timestamp from a remote
-// system with the local timeglobal uniqueness and monotonicity are
-// preserved
-Timestamp.recv = function (msg) {
-  if (!clock) {
-    return null;
-  }
-
-  // retrieve the local wall time
-  let phys = Date.now();
-
-  // unpack the message wall time/counter
-  let lMsg = msg.millis();
-  let cMsg = msg.counter();
-
-  // assert the node id and remote clock drift
-  // if (msg.node() === clock.timestamp.node()) {
-  //   throw new Timestamp.DuplicateNodeError(clock.timestamp.node());
-  // }
-  if (lMsg - phys > config.maxDrift) {
-    throw new Timestamp.ClockDriftError();
-  }
-
-  // unpack the clock.timestamp logical time and counter
-  let lOld = clock.timestamp.millis();
-  let cOld = clock.timestamp.counter();
-
-  // calculate the next logical time and counter
-  // . ensure that the logical time never goes backward
-  // . if all logical clocks are equal, increment the max counter
-  // . if max = old > message, increment local counter
-  // . if max = messsage > old, increment message counter
-  // . otherwise, clocks are monotonic, reset counter
-  let lNew = Math.max(Math.max(lOld, phys), lMsg);
-  let cNew =
-    lNew === lOld && lNew === lMsg
-      ? Math.max(cOld, cMsg) + 1
-      : lNew === lOld
-      ? cOld + 1
-      : lNew === lMsg
-      ? cMsg + 1
-      : 0;
-
-  // check the result for drift and counter overflow
-  if (lNew - phys > config.maxDrift) {
-    throw new Timestamp.ClockDriftError();
-  }
-  if (cNew > MAX_COUNTER) {
-    throw new Timestamp.OverflowError();
-  }
-
-  // repack the logical time/counter
-  clock.timestamp.setMillis(lNew);
-  clock.timestamp.setCounter(cNew);
-
-  return new Timestamp(
-    clock.timestamp.millis(),
-    clock.timestamp.counter(),
-    clock.timestamp.node(),
-  );
-};
-
-/**
- * timestamp parsing
- * converts a fixed-length string timestamp to the structured value
- */
-Timestamp.parse = function (timestamp: string): Timestamp | null {
-  if (typeof timestamp === 'string') {
-    let parts = timestamp.split('-');
-    if (parts && parts.length === 5) {
-      let millis = Date.parse(parts.slice(0, 3).join('-')).valueOf();
-      let counter = parseInt(parts[3], 16);
-      let node = parts[4];
-      if (
-        !isNaN(millis) &&
-        millis >= 0 &&
-        !isNaN(counter) &&
-        counter <= MAX_COUNTER &&
-        typeof node === 'string' &&
-        node.length <= MAX_NODE_LENGTH
-      ) {
-        return new Timestamp(millis, counter, node);
-      }
-    }
-  }
-  return null;
-};
-
-/**
- * zero/minimum timestamp
- */
-let zero = Timestamp.parse('1970-01-01T00:00:00.000Z-0000-0000000000000000');
-Timestamp.zero = function () {
-  return zero;
-};
-
-/**
- * maximum timestamp
- */
-let max = Timestamp.parse('9999-12-31T23:59:59.999Z-FFFF-FFFFFFFFFFFFFFFF');
-Timestamp.max = function () {
-  return max;
-};
-
-Timestamp.since = isoString => {
-  return isoString + '-0000-0000000000000000';
-};
-
-/**
- * error classes
- */
-Timestamp.DuplicateNodeError = class extends Error {
-  constructor(node) {
-    super('duplicate node identifier ' + node);
-    this.name = 'DuplicateNodeError';
-  }
-};
-
-Timestamp.ClockDriftError = class extends Error {
-  constructor(...args) {
-    super(['maximum clock drift exceeded'].concat(args).join(' '));
-    this.name = 'ClockDriftError';
-  }
-};
-
-Timestamp.OverflowError = class extends Error {
-  constructor() {
-    super('timestamp counter overflow');
-    this.name = 'OverflowError';
-  }
-};

packages/crdt/src/crdt/index.ts
@@ -8,5 +8,6 @@ export {
   makeClientId,
   serializeClock,
   deserializeClock,
+  Clock,
   Timestamp,
 } from './timestamp';

packages/crdt/src/crdt/merkle.test.ts
@@ -17,7 +17,7 @@ function insertMessages(trie, messages) {
 describe('merkle trie', () => {
   test('adding an item works', () => {
     let trie = merkle.insert(
-      {},
+      merkle.emptyTrie(),
       Timestamp.parse('2018-11-12T13:21:40.122Z-0000-0123456789ABCDEF'),
     );
     trie = merkle.insert(
@@ -28,8 +28,8 @@ describe('merkle trie', () => {
   });
 
   test('diff returns the correct time difference', () => {
-    let trie1: { hash?: unknown } = {};
-    let trie2: { hash?: unknown } = {};
+    let trie1 = merkle.emptyTrie();
+    let trie2 = merkle.emptyTrie();
 
     const messages = [
       // First client messages
@@ -65,9 +65,9 @@ describe('merkle trie', () => {
   });
 
   test('diffing works with empty tries', () => {
-    let trie1 = {};
+    let trie1 = merkle.emptyTrie();
     let trie2 = merkle.insert(
-      {},
+      merkle.emptyTrie(),
       Timestamp.parse('2009-01-02T10:17:37.789Z-0000-0000testinguuid1'),
     );
 
@@ -90,7 +90,7 @@ describe('merkle trie', () => {
       message('2018-11-01T02:37:00.000Z-0000-0123456789ABCDEF', 2100),
     ];
 
-    let trie: { hash?: unknown } = {};
+    let trie = merkle.emptyTrie();
     messages.forEach(msg => {
       trie = merkle.insert(trie, msg.timestamp);
     });
@@ -122,10 +122,10 @@ describe('merkle trie', () => {
 
     // Case 0: It always returns a base time when comparing with an
     // empty trie
-    expect(new Date(merkle.diff({}, trie)).toISOString()).toBe(
+    expect(new Date(merkle.diff(merkle.emptyTrie(), trie)).toISOString()).toBe(
       '1970-01-01T00:00:00.000Z',
     );
-    expect(new Date(merkle.diff(trie, {})).toISOString()).toBe(
+    expect(new Date(merkle.diff(trie, merkle.emptyTrie())).toISOString()).toBe(
       '1970-01-01T00:00:00.000Z',
     );
 

packages/crdt/src/crdt/timestamp.test.ts
@@ -16,10 +16,10 @@ describe('Timestamp', function () {
 
   describe('comparison', function () {
     it('should be in order', function () {
-      expect(Timestamp.zero()).toBe(Timestamp.zero());
-      expect(Timestamp.max() > Timestamp.zero()).toBeTruthy();
-      expect(Timestamp.send() > Timestamp.zero()).toBeTruthy();
-      expect(Timestamp.send() < Timestamp.max()).toBeTruthy();
+      expect(Timestamp.zero).toBe(Timestamp.zero);
+      expect(Timestamp.max > Timestamp.zero).toBeTruthy();
+      expect(Timestamp.send() > Timestamp.zero).toBeTruthy();
+      expect(Timestamp.send() < Timestamp.max).toBeTruthy();
     });
   });
 
@@ -42,7 +42,7 @@ describe('Timestamp', function () {
         '9999-12-31T23:59:59.999Z-FFFF-10000000000000000',
       ];
       for (let invalidInput of invalidInputs) {
-        expect(Timestamp.parse(invalidInput)).toBe(null);
+        expect(Timestamp.parse(invalidInput as string)).toBe(null);
       }
     });
 

packages/crdt/src/main.ts
@@ -7,6 +7,7 @@ export {
   makeClientId,
   serializeClock,
   deserializeClock,
+  Clock,
   Timestamp,
 } from './crdt';
 

packages/loot-core/src/server/main.ts
@@ -61,7 +61,6 @@ import {
   setSyncingMode,
   makeTestMessage,
   clearFullSyncTimeout,
-  syncAndReceiveMessages,
   resetSync,
   repairSync,
 } from './sync';
@@ -2462,7 +2461,6 @@ export const lib = {
     return res;
   },
   on: (name, func) => app.events.on(name, func),
-  syncAndReceiveMessages,
   q,
   db,
 

packages/loot-core/src/server/migrate/migrations.ts
@@ -1,6 +1,7 @@
 // We have to bundle in JS migrations manually to avoid having to `eval`
 // them which doesn't play well with CSP. There isn't great, and eventually
 // we can remove this migration.
+import { Database } from 'better-sqlite3';
 import { v4 as uuidv4 } from 'uuid';
 
 import m1632571489012 from '../../../migrations/1632571489012_remove_cache';
@@ -13,18 +14,21 @@ let javascriptMigrations = {
   1632571489012: m1632571489012,
 };
 
-export async function withMigrationsDir(dir, func) {
+export async function withMigrationsDir(
+  dir: string,
+  func: () => Promise<void>,
+): Promise<void> {
   let oldDir = MIGRATIONS_DIR;
   MIGRATIONS_DIR = dir;
   await func();
   MIGRATIONS_DIR = oldDir;
 }
 
-export function getMigrationsDir() {
+export function getMigrationsDir(): string {
   return MIGRATIONS_DIR;
 }
 
-function getMigrationId(name) {
+function getMigrationId(name: string): number {
   return parseInt(name.match(/^(\d)+/)[0]);
 }
 
@@ -36,7 +40,7 @@ export function getUpMigration(id, names) {
   }
 }
 
-export async function getAppliedMigrations(db) {
+export async function getAppliedMigrations(db: Database): Promise<number[]> {
   const rows = await sqlite.runQuery<{ id: number }>(
     db,
     'SELECT * FROM __migrations__ ORDER BY id ASC',
@@ -46,7 +50,9 @@ export async function getAppliedMigrations(db) {
   return rows.map(row => row.id);
 }
 
-export async function getMigrationList(migrationsDir) {
+export async function getMigrationList(
+  migrationsDir: string,
+): Promise<string[]> {
   const files = await fs.listDir(migrationsDir);
   return files
     .filter(name => name.match(/(\.sql|\.js)$/))
@@ -62,7 +68,7 @@ export async function getMigrationList(migrationsDir) {
     });
 }
 
-export function getPending(appliedIds, all) {
+export function getPending(appliedIds: number[], all: string[]): string[] {
   return all.filter(name => {
     const id = getMigrationId(name);
     return appliedIds.indexOf(id) === -1;
@@ -94,7 +100,11 @@ async function applySql(db, sql) {
   }
 }
 
-export async function applyMigration(db, name, migrationsDir) {
+export async function applyMigration(
+  db: Database,
+  name: string,
+  migrationsDir: string,
+): Promise<void> {
   const code = await fs.readFile(fs.join(migrationsDir, name));
   if (name.match(/\.js$/)) {
     await applyJavaScript(db, getMigrationId(name));
@@ -106,7 +116,10 @@ export async function applyMigration(db, name, migrationsDir) {
   ]);
 }
 
-function checkDatabaseValidity(appliedIds, available) {
+function checkDatabaseValidity(
+  appliedIds: number[],
+  available: string[],
+): void {
   for (let i = 0; i < appliedIds.length; i++) {
     if (
       i >= available.length ||
@@ -121,7 +134,7 @@ function checkDatabaseValidity(appliedIds, available) {
   }
 }
 
-export async function migrate(db) {
+export async function migrate(db: Database): Promise<string[]> {
   let appliedIds = await getAppliedMigrations(db);
   let available = await getMigrationList(MIGRATIONS_DIR);
 

commit 04b0a4cccd5b43b2bf302ba476ea8e692fdee09d

🐛 fix new budget files not syncing (#1291)
Fix a small regression introduced in
https://github.com/actualbudget/actual/pull/1076

Tested locally and this seems to have solved the problem

packages/crdt/src/crdt/merkle.ts
@@ -16,7 +16,7 @@ export type TrieNode = {
   '0'?: TrieNode;
   '1'?: TrieNode;
   '2'?: TrieNode;
-  hash: number;
+  hash?: number;
 };
 
 export function emptyTrie(): TrieNode {

packages/crdt/src/crdt/timestamp.ts
@@ -42,10 +42,7 @@ export function getClock(): Clock {
   return clock;
 }
 
-export function makeClock(
-  timestamp: Timestamp,
-  merkle: TrieNode = { hash: 0 },
-) {
+export function makeClock(timestamp: Timestamp, merkle: TrieNode = {}) {
   return { timestamp: MutableTimestamp.from(timestamp), merkle };
 }
 