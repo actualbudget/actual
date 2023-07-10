f01a14c30611a7cdb12fdd01d97f12325bd2dfd9
Sean Tsai <aleetsaiya@gmail.com>
2023-07-10 21:35:55 +0800

    add JSdoc for custom select

--- packages/desktop-client/src/components/common/CustomSelect.tsx
+++ packages/desktop-client/src/components/common/CustomSelect.tsx
@@ -19,6 +19,21 @@ type CustomSelectProps = {
   disabledKeys?: string[];
 };
 
+/**
+ * @param {Array<[string, string]>} options - An array of options value-label pairs.
+ * @param {string} value - The currently selected option value.
+ * @param {string} [defaultLabel] - The label to display when the selected value is not in the options.
+ * @param {function} [onChange] - A callback function invoked when the selected value changes.
+ * @param {CSSProperties} [style] - Custom styles to apply to the selected button.
+ * @param {CSSProperties} [wrapperStyle] - Custom style to apply to the select wrapper.
+ * @param {string[]} [disabledKeys] - An array of option values to disable.
+ *
+ * @example
+ * // Usage:
+ * // <CustomSelect options={[['1', 'Option 1'], ['2', 'Option 2']]} value="1" onChange={handleOnChange} />
+ * // <CustomSelect options={[['1', 'Option 1'], ['2', 'Option 2']]} value="3" defaultLabel="Select an option"  onChange={handleOnChange} />
+ */
+
 export default function CustomSelect({
   options,
   value,
