026675bfa4c0300b46198004a49d5b2d72ef35cf
shall0pass <20625555+shall0pass@users.noreply.github.com>
2023-07-15 15:30:11 -0500

    use setZero function within goal templates

--- packages/loot-core/src/server/budget/goaltemplates.ts
+++ packages/loot-core/src/server/budget/goaltemplates.ts
@@ -7,7 +7,7 @@ import { amountToInteger, integerToAmount } from '../../shared/util';
 import * as db from '../db';
 import { getRuleForSchedule, getNextDate } from '../schedules/app';
 
-import { setBudget, getSheetValue } from './actions';
+import { setBudget, setZero, getSheetValue } from './actions';
 import { parse } from './goal-template.pegjs';
 
 export function applyTemplate({ month }) {
@@ -63,13 +63,9 @@ async function processTemplate(month, force) {
             ? template[l].priority
             : lowestPriority;
       }
-      await setBudget({
-        category: category.id,
-        month,
-        amount: 0,
-      });
     }
   }
+  setZero({ month });
   // find all remainder templates, place them after all other templates
   let remainder_found;
   let remainder_priority = lowestPriority + 1;
