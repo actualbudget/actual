c539a85225572f504b8eda401ef587937b8eeb01
Michael Gillett <51103663+migillett@users.noreply.github.com>
2023-07-18 21:07:50 -0400

    Update FixSplits.js

--- packages/desktop-client/src/components/settings/FixSplits.js
+++ packages/desktop-client/src/components/settings/FixSplits.js
@@ -79,8 +79,8 @@ export default function FixSplitsTool() {
         <strong>Repair split transactions</strong> if you are experiencing bugs
         relating to split transactions and the “Reset budget cache” button above
         does not help, this tool may fix them. Some examples of bugs include
-        seeing blank payees on splits or incorrect account balances.
-        This tool does two things:
+        seeing blank payees on splits or incorrect account balances. This tool does two things:
+
       </Text>
       <ul style={{ margin: 0, paddingLeft: '1.5em' }}>
         <li style={{ marginBottom: '0.5em' }}>
