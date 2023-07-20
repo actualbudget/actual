fcd99ef267790b36443f81b82d1b14f2735836ef
Neil <55785687+carkom@users.noreply.github.com>
2023-07-20 17:36:37 +0100

    Add "not" checkbox for string filters (#1287)

--- packages/loot-core/src/shared/rules.ts
+++ packages/loot-core/src/shared/rules.ts
@@ -8,7 +8,7 @@ export const TYPE_INFO = {
     nullable: false,
   },
   id: {
-    ops: ['is', 'contains', 'oneOf'],
+    ops: ['is', 'contains', 'oneOf', 'isNot', 'doesNotContain', 'notOneOf'],
     nullable: true,
   },
   saved: {
@@ -16,7 +16,7 @@ export const TYPE_INFO = {
     nullable: false,
   },
   string: {
-    ops: ['is', 'contains', 'oneOf'],
+    ops: ['is', 'contains', 'oneOf', 'isNot', 'doesNotContain', 'notOneOf'],
     nullable: false,
   },
   number: {
@@ -71,14 +71,20 @@ export function friendlyOp(op, type) {
   switch (op) {
     case 'oneOf':
       return 'one of';
+    case 'notOneOf':
+      return 'not one of';
     case 'is':
       return 'is';
+    case 'isNot':
+      return 'is not';
     case 'isapprox':
       return 'is approx';
     case 'isbetween':
       return 'is between';
     case 'contains':
       return 'contains';
+    case 'doesNotContain':
+      return 'does not contain';
     case 'gt':
       if (type === 'date') {
         return 'is after';
