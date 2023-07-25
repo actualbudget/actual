fbdc5af5ddb711be9c15feb72f08fdc82f59fb21
Jed Fox <git@jedfox.com>
2023-07-25 16:18:31 -0400

    Remove unused fetchBinary

--- packages/loot-core/src/platform/server/fetch/index.d.ts
+++ packages/loot-core/src/platform/server/fetch/index.d.ts
@@ -2,7 +2,3 @@ export function fetch(
   input: RequestInfo | URL,
   options?: unknown,
 ): Promise<Response>;
-export function fetchBinary(
-  input: RequestInfo | URL,
-  filepath: string,
-): Promise<unknown>;
--- packages/loot-core/src/platform/server/fetch/index.web.ts
+++ packages/loot-core/src/platform/server/fetch/index.web.ts
@@ -1,5 +1,1 @@
 export const fetch = self.fetch;
-
-export const fetchBinary = function () {
-  throw new Error('fetchBinary not implemented');
-};
