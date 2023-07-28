5d443136a7ec4354c0dae72c9d27ec5a2cf96e40
Jed Fox <git@jedfox.com>
2023-07-27 20:02:30 -0700

    Remove unused withThemeColor

--- packages/desktop-client/src/hooks/useSetThemeColor.ts
+++ packages/desktop-client/src/hooks/useSetThemeColor.ts
@@ -1,9 +1,13 @@
 import { useEffect } from 'react';
 
-import { setThemeColor } from '../util/withThemeColor';
-
-export function useSetThemeColor(color) {
+export function useSetThemeColor(color: string) {
   useEffect(() => {
     setThemeColor(color);
   }, [color]);
+}
+
+function setThemeColor(color: string) {
+  const metaTags = document.getElementsByTagName('meta');
+  const themeTag = [...metaTags].find(tag => tag.name === 'theme-color');
+  themeTag.setAttribute('content', color);
 }
