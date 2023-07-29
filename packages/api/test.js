2f6e0905a0ce4c85d725b2597fa42f99f316556e
Jed Fox <git@jedfox.com>
2023-07-29 04:56:57 -0700

    Delete test.js

--- packages/api/test.js
+++
@@ -1,9 +0,0 @@
-let api = require('./dist/index');
-
-async function run() {
-  let app = await api.init({ dataDir: '/tmp' });
-  await app.send('create-budget', { testMode: true });
-  await api.shutdown();
-}
-
-run();
