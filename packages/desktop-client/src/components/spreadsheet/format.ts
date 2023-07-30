482bf6fc779ec42b8cf05930e9884099d44fad73
Matiss Janis Aboltins <matiss@mja.lv>
2023-07-30 18:03:00 +0100

    :bug:  fix number formatter not listening to state changes
    Closes #1415

--- packages/desktop-client/src/components/spreadsheet/format.ts
+++ packages/desktop-client/src/components/spreadsheet/format.ts
@@ -1,6 +1,16 @@
-import { integerToCurrency } from 'loot-core/src/shared/util';
+import { useCallback } from 'react';
+import { useSelector } from 'react-redux';
 
-export default function format(value, type = 'string'): string {
+import { integerToCurrency, getNumberFormat } from 'loot-core/src/shared/util';
+
+/**
+ * @deprecated Please do not use this directly. Use `useFormat` hook
+ */
+export default function format(
+  value: unknown,
+  type = 'string',
+  formatter?: Intl.NumberFormat,
+): string {
   switch (type) {
     case 'string':
       const val = JSON.stringify(value);
@@ -12,14 +22,14 @@ export default function format(value, type = 'string'): string {
     case 'number':
       return '' + value;
     case 'financial-with-sign':
-      let formatted = format(value, 'financial');
+      let formatted = format(value, 'financial', formatter);
       if (value >= 0) {
         return '+' + formatted;
       }
       return formatted;
     case 'financial':
       if (value == null || value === '' || value === 0) {
-        return integerToCurrency(0);
+        return integerToCurrency(0, formatter);
       } else if (typeof value === 'string') {
         const parsed = parseFloat(value);
         value = isNaN(parsed) ? 0 : parsed;
@@ -31,8 +41,23 @@ export default function format(value, type = 'string'): string {
         );
       }
 
-      return integerToCurrency(value);
+      return integerToCurrency(value, formatter);
     default:
       throw new Error('Unknown format type: ' + type);
   }
+}
+
+export function useFormat() {
+  const numberFormat = useSelector(state =>
+    getNumberFormat({
+      format: state.prefs.local.numberFormat,
+      hideFraction: state.prefs.local.hideFraction,
+    }),
+  );
+
+  return useCallback(
+    (value: unknown, type = 'string') =>
+      format(value, type, numberFormat.formatter),
+    [numberFormat],
+  );
 }
