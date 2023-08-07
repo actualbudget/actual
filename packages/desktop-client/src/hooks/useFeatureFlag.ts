e17d90ce5f723d0418da268e43d195ba28362b34
Johannes Löthberg <johannes@kyriasis.com>
2023-08-07 20:04:56 +0200

    Add category spending report (#1382)

--- packages/desktop-client/src/hooks/useFeatureFlag.ts
+++ packages/desktop-client/src/hooks/useFeatureFlag.ts
@@ -3,6 +3,7 @@ import { useSelector } from 'react-redux';
 import type { FeatureFlag } from 'loot-core/src/types/prefs';
 
 const DEFAULT_FEATURE_FLAG_STATE: Record<FeatureFlag, boolean> = {
+  categorySpendingReport: false,
   reportBudget: false,
   goalTemplatesEnabled: false,
   privacyMode: false,
