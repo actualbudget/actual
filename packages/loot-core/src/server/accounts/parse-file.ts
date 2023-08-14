3401d49de14a9f056e05c94be702210faf7357e2
Joel Jeremy Marquez <joeljeremy.marquez@gmail.com>
2023-08-14 14:00:15 -0700

    Revert to own ofx implementation + regex changes

--- packages/loot-core/src/server/accounts/parse-file.ts
+++ packages/loot-core/src/server/accounts/parse-file.ts
@@ -1,5 +1,5 @@
 import csv2json from 'csv-parse/lib/sync';
-import { Ofx } from 'ofx-data-extractor';
+import { parseStringPromise } from 'xml2js';
 
 import * as fs from '../../platform/server/fs';
 import { dayFromDate } from '../../shared/months';
@@ -40,7 +40,7 @@ export async function parseFile(
     message: 'Invalid file type',
     internal: '',
   });
-  return { errors, transactions: undefined };
+  return { errors, transactions: [] };
 }
 
 async function parseCSV(
@@ -106,10 +106,9 @@ async function parseOFX(filepath): Promise<ParseFileResult> {
   let errors = Array<ParseError>();
   let contents = await fs.readFile(filepath);
 
-  let stmtTrn;
+  let data;
   try {
-    let ofx = new Ofx(contents);
-    stmtTrn = ofx.getBankTransferList();
+    data = await parse(contents);
   } catch (err) {
     errors.push({
       message: 'Failed importing file',
@@ -120,20 +119,90 @@ async function parseOFX(filepath): Promise<ParseFileResult> {
 
   return {
     errors,
-    transactions: stmtTrn.map(mapTransaction),
+    transactions: getStmtTrn(data).map(mapTransaction),
   };
+}
+
+function getStmtTrn(data) {
+  let ofx = data?.['OFX'];
+  let isCc = ofx?.['CREDITCARDMSGSRSV1'] != null;
+  let msg = isCc ? ofx?.['CREDITCARDMSGSRSV1'] : ofx?.['BANKMSGSRSV1'];
+  let stmtTrnRs = msg?.[`${isCc ? 'CC' : ''}STMTTRNRS`];
+  let stmtRs = stmtTrnRs?.[`${isCc ? 'CC' : ''}STMTRS`];
+  let bankTranList = stmtRs?.['BANKTRANLIST'];
+  // Could be an array or a single object.
+  // xml2js serializes single item to an object and multiple to an array.
+  let stmtTrn = bankTranList?.['STMTTRN'];
+  if (!Array.isArray(stmtTrn)) {
+    return [stmtTrn];
+  }
+  return stmtTrn;
 }
 
 function mapTransaction(stmtTrn) {
   // Banks don't always implement the OFX standard properly
   // If no payee is available try and fallback to memo
   let useName = stmtTrn['NAME'] != null;
+  // YYYYMMDDHHMMSS format. We just need the date.
+  let dtPosted = stmtTrn['DTPOSTED'];
+  let transactionDate = dtPosted
+    ? new Date(
+        Number(dtPosted.substring(0, 4)), // year
+        Number(dtPosted.substring(4, 6)) - 1, // month (zero-based index)
+        Number(dtPosted.substring(6, 8)), // date
+      )
+    : null;
+
   return {
     amount: stmtTrn['TRNAMT'],
     imported_id: stmtTrn['FITID'],
-    date: dayFromDate(stmtTrn['DTPOSTED']),
+    date: dayFromDate(transactionDate),
     payee_name: useName ? stmtTrn['NAME'] : stmtTrn['MEMO'],
     imported_payee: useName ? stmtTrn['NAME'] : stmtTrn['MEMO'],
     notes: useName ? stmtTrn['MEMO'] || null : null, //memo used for payee
   };
+}
+
+function sgml2Xml(sgml) {
+  return sgml
+    .replace(/>\s+</g, '><') // remove whitespace inbetween tag close/open
+    .replace(/\s+</g, '<') // remove whitespace before a close tag
+    .replace(/>\s+/g, '>') // remove whitespace after a close tag
+    .replace(/<([A-Z\d_]*)+\.+([A-Z\d_]*)>([^<]+)/g, '<$1$2>$3')
+    .replace(/<(\w+?)>([^<]+)/g, '<$1>$2</$1>');
+}
+
+async function parseXml(content) {
+  return await parseStringPromise(content, { explicitArray: false });
+}
+
+async function parse(data) {
+  // firstly, split into the header attributes and the footer sgml
+  const ofx = data.split('<OFX>', 2);
+
+  // firstly, parse the headers
+  const headerString = ofx[0].split(/\r?\n/);
+  const header = {};
+  headerString.forEach(attrs => {
+    const headAttr = attrs.split(/:/, 2);
+    header[headAttr[0]] = headAttr[1];
+  });
+
+  // make the SGML and the XML
+  const content = `<OFX>${ofx[1]}`;
+
+  // Parse the XML/SGML portion of the file into an object
+  // Try as XML first, and if that fails do the SGML->XML mangling
+  let dataParsed = null;
+  try {
+    dataParsed = await parseXml(content);
+  } catch (e) {
+    let converted = sgml2Xml(content);
+    dataParsed = await parseXml(converted);
+  }
+
+  // put the headers into the returned data
+  dataParsed.header = header;
+
+  return dataParsed;
 }
