37cec4c46f4910fa66e9d6bcd648de54408bd31c
Johannes Löthberg <johannes@kyriasis.com>
2023-08-22 20:12:13 +0200

    Don't update transaction date when syncing from GoCardless. (#1559)
    We should not override the date in case the user has manually corrected
    it.
    
    Signed-off-by: Johannes Löthberg <johannes@kyriasis.com>

--- packages/loot-core/src/server/accounts/sync.ts
+++ packages/loot-core/src/server/accounts/sync.ts
@@ -498,7 +498,6 @@ export async function reconcileGoCardlessTransactions(acctId, transactions) {
 
       // Update the transaction
       const updates = {
-        date: trans.date,
         imported_id: trans.imported_id || null,
         payee: existing.payee || trans.payee || null,
         category: existing.category || trans.category || null,
