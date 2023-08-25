e0fe39fc472c9bd1e71cf19e4f535806903f1df9
OlegWock <olegwock@gmail.com>
2023-08-25 19:32:26 +0200

    Add support for markdown in notes

--- packages/desktop-client/src/components/NotesButton.tsx
+++ packages/desktop-client/src/components/NotesButton.tsx
@@ -1,6 +1,9 @@
 import React, { createRef, useState, useEffect } from 'react';
+import ReactMarkdown from 'react-markdown';
 
 import { type CSSProperties, css } from 'glamor';
+import remarkBreaks from 'remark-breaks';
+import remarkGfm from 'remark-gfm';
 
 import q from 'loot-core/src/client/query-helpers';
 import { useLiveQuery } from 'loot-core/src/client/query-hooks';
@@ -13,6 +16,105 @@ import Button from './common/Button';
 import Text from './common/Text';
 import View from './common/View';
 import { Tooltip, useTooltip } from './tooltips';
+
+function sequentialNewlinesPlugin() {
+  // Adapted from https://codesandbox.io/s/create-react-app-forked-h3rmcy?file=/src/sequentialNewlinePlugin.js:0-774
+  const data = this.data();
+
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  function add(field: string, value: any) {
+    const list = data[field] ? data[field] : (data[field] = []);
+
+    list.push(value);
+  }
+
+  add('fromMarkdownExtensions', {
+    enter: {
+      lineEndingBlank: function enterLineEndingBlank(token: unknown) {
+        this.enter(
+          {
+            type: 'break',
+            value: '',
+            data: {},
+            children: [],
+          },
+          token,
+        );
+      },
+    },
+    exit: {
+      lineEndingBlank: function exitLineEndingBlank(token: unknown) {
+        this.exit(token);
+      },
+    },
+  });
+}
+
+const remarkPlugins = [sequentialNewlinesPlugin, remarkGfm, remarkBreaks];
+
+const markdownStyles = css({
+  display: 'block',
+  maxWidth: 350,
+  padding: 8,
+  overflowWrap: 'break-word',
+  '& p': {
+    margin: 0,
+    ':not(:first-child)': {
+      marginTop: '0.25rem',
+    },
+  },
+  '& ul, & ol': {
+    listStylePosition: 'inside',
+    margin: 0,
+    paddingLeft: 0,
+  },
+  '&>* ul, &>* ol': {
+    marginLeft: '1.5rem',
+  },
+  '& li>p': {
+    display: 'contents',
+  },
+  '& blockquote': {
+    paddingLeft: '0.75rem',
+    borderLeft: '3px solid ' + colors.p6,
+    margin: 0,
+  },
+  '& hr': {
+    borderTop: 'none',
+    borderLeft: 'none',
+    borderRight: 'none',
+    borderBottom: '1px solid ' + colors.p9,
+  },
+  '& code': {
+    backgroundColor: colors.p10,
+    padding: '0.1rem 0.5rem',
+    borderRadius: '0.25rem',
+  },
+  '& pre': {
+    padding: '0.5rem',
+    backgroundColor: colors.p10,
+    borderRadius: '0.5rem',
+    margin: 0,
+    ':not(:first-child)': {
+      marginTop: '0.25rem',
+    },
+    '& code': {
+      background: 'inherit',
+      padding: 0,
+      borderRadius: 0,
+    },
+  },
+  '& table, & th, & td': {
+    border: '1px solid ' + colors.p9,
+  },
+  '& table': {
+    borderCollapse: 'collapse',
+    wordBreak: 'break-word',
+  },
+  '& td': {
+    padding: '0.25rem 0.75rem',
+  },
+});
 
 type NotesTooltipProps = {
   editable?: boolean;
@@ -43,7 +145,7 @@ function NotesTooltip({
           {...css({
             border: '1px solid ' + colors.border,
             padding: 7,
-            minWidth: 300,
+            minWidth: 350,
             minHeight: 120,
             outline: 'none',
           })}
@@ -51,16 +153,12 @@ function NotesTooltip({
           onChange={e => setNotes(e.target.value)}
         />
       ) : (
-        <Text
-          {...css({
-            display: 'block',
-            maxWidth: 225,
-            padding: 8,
-            whiteSpace: 'pre-wrap',
-            overflowWrap: 'break-word',
-          })}
-        >
-          {notes}
+        <Text {...markdownStyles}>
+          <ReactMarkdown
+            remarkPlugins={remarkPlugins}
+            linkTarget="_blank"
+            children={notes}
+          />
         </Text>
       )}
     </Tooltip>
