a78397bb7f3be60fb1e1ed468d26b05d75e7d043
Joel Jeremy Marquez <joeljeremy.marquez@gmail.com>
2023-08-27 22:27:40 -0700

    Move experimental ofx parser to ofx2json

--- packages/loot-core/src/server/accounts/parse-file.ts
+++ packages/loot-core/src/server/accounts/parse-file.ts
@@ -1,10 +1,10 @@
 import csv2json from 'csv-parse/lib/sync';
-import { parseStringPromise } from 'xml2js';
 
 import * as fs from '../../platform/server/fs';
 import { dayFromDate } from '../../shared/months';
 import { looselyParseAmount } from '../../shared/util';
 
+import ofx2json from './ofx2json';
 import qif2json from './qif2json';
 
 type ParseError = { message: string; internal: string };
@@ -113,13 +113,12 @@ async function parseQIF(filepath): Promise<ParseFileResult> {
 }
 
 async function parseOFX(filepath): Promise<ParseFileResult> {
-  let errors = Array<ParseError>();
-  let contents = await fs.readFile(filepath);
+  const errors = Array<ParseError>();
+  const contents = await fs.readFile(filepath);
 
-  let transactions;
+  let data;
   try {
-    let data = await parse(contents);
-    transactions = getStmtTrn(data).map(mapTransaction);
+    data = await ofx2json(contents);
   } catch (err) {
     errors.push({
       message: 'Failed importing file',
@@ -130,95 +129,20 @@ async function parseOFX(filepath): Promise<ParseFileResult> {
 
   return {
     errors,
-    transactions: transactions ?? [],
+    transactions: data.transactions.map(trans => {
+      // Banks don't always implement the OFX standard properly
+      // If no payee is available try and fallback to memo
+      const useName = trans.name != null;
+      return {
+        amount: trans.amount,
+        imported_id: trans.fitId,
+        date: trans.date,
+        payee_name: useName ? trans.name : trans.memo,
+        imported_payee: useName ? trans.name : trans.memo,
+        notes: useName ? trans.memo || null : null, //memo used for payee
+      };
+    }),
   };
-}
-
-function getStmtTrn(data) {
-  let ofx = data?.['OFX'];
-  let isCc = ofx?.['CREDITCARDMSGSRSV1'] != null;
-  let msg = isCc ? ofx?.['CREDITCARDMSGSRSV1'] : ofx?.['BANKMSGSRSV1'];
-  let stmtTrnRs = msg?.[`${isCc ? 'CC' : ''}STMTTRNRS`];
-  let stmtRs = stmtTrnRs?.[`${isCc ? 'CC' : ''}STMTRS`];
-  let bankTranList = stmtRs?.['BANKTRANLIST'];
-  // Could be an array or a single object.
-  // xml2js serializes single item to an object and multiple to an array.
-  let stmtTrn = bankTranList?.['STMTTRN'];
-  if (!Array.isArray(stmtTrn)) {
-    return [stmtTrn];
-  }
-  return stmtTrn;
-}
-
-function mapTransaction(stmtTrn) {
-  // Banks don't always implement the OFX standard properly
-  // If no payee is available try and fallback to memo
-  let useName = stmtTrn['NAME'] != null;
-  // YYYYMMDDHHMMSS format. We just need the date.
-  let dtPosted = stmtTrn['DTPOSTED'];
-  let transactionDate = dtPosted
-    ? new Date(
-        Number(dtPosted.substring(0, 4)), // year
-        Number(dtPosted.substring(4, 6)) - 1, // month (zero-based index)
-        Number(dtPosted.substring(6, 8)), // date
-      )
-    : null;
-
-  return {
-    amount: stmtTrn['TRNAMT'],
-    imported_id: stmtTrn['FITID'],
-    date: dayFromDate(transactionDate),
-    payee_name: useName ? stmtTrn['NAME'] : stmtTrn['MEMO'],
-    imported_payee: useName ? stmtTrn['NAME'] : stmtTrn['MEMO'],
-    notes: useName ? stmtTrn['MEMO'] || null : null, //memo used for payee
-  };
-}
-
-function sgml2Xml(sgml) {
-  return sgml
-    .replace(/&/g, '&#038;') // Replace ampersand
-    .replace(/&amp;/g, '&#038;')
-    .replace(/>\s+</g, '><') // remove whitespace inbetween tag close/open
-    .replace(/\s+</g, '<') // remove whitespace before a close tag
-    .replace(/>\s+/g, '>') // remove whitespace after a close tag
-    .replace(/\.(?=[^<>]*>)/g, '') // Remove dots in tag names
-    .replace(/<(\w+?)>([^<]+)/g, '<$1>$2</<added>$1>') // Add a new end-tags for the ofx elements
-    .replace(/<\/<added>(\w+?)>(<\/\1>)?/g, '</$1>'); // Remove duplicate end-tags
-}
-
-async function parseXml(content) {
-  return await parseStringPromise(content, { explicitArray: false });
-}
-
-async function parse(data) {
-  // firstly, split into the header attributes and the footer sgml
-  const ofx = data.split('<OFX>', 2);
-
-  // firstly, parse the headers
-  const headerString = ofx[0].split(/\r?\n/);
-  const header = {};
-  headerString.forEach(attrs => {
-    const headAttr = attrs.split(/:/, 2);
-    header[headAttr[0]] = headAttr[1];
-  });
-
-  // make the SGML and the XML
-  const content = `<OFX>${ofx[1]}`;
-
-  // Parse the XML/SGML portion of the file into an object
-  // Try as XML first, and if that fails do the SGML->XML mangling
-  let dataParsed = null;
-  try {
-    dataParsed = await parseXml(content);
-  } catch (e) {
-    let sanitized = sgml2Xml(content);
-    dataParsed = await parseXml(sanitized);
-  }
-
-  // put the headers into the returned data
-  dataParsed.header = header;
-
-  return dataParsed;
 }
 
 async function parseOFXNodeLibOFX(filepath, options): Promise<ParseFileResult> {
