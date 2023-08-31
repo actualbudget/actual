00d16d9b598c2e61588b8f3111c55bd8d42b3c23
Matiss Janis Aboltins <matiss@mja.lv>
2023-08-31 20:40:24 +0100

    Fix tooltip selector logic

--- packages/desktop-client/src/components/tooltips.js
+++ packages/desktop-client/src/components/tooltips.js
@@ -43,10 +43,7 @@ export class Tooltip extends Component {
         // Allow clicking reach popover that mount outside of
         // tooltips. Might need to think about this more, like what
         // kind of things can be click that shouldn't close a tooltip?
-        if (
-          node.dataset['is-tooltip'] === 'true' ||
-          node.dataset.reachPopover != null
-        ) {
+        if (node.dataset.istooltip || node.dataset.reachPopover != null) {
           break;
         }
 
@@ -319,7 +316,7 @@ export class Tooltip extends Component {
             {...css(contentStyle, style, styles.darkScrollbar)}
             ref={this.contentRef}
             data-testid={this.props['data-testid'] || 'tooltip'}
-            data-is-tooltip="true"
+            data-istooltip
             onClick={e => {
               // Click events inside a tooltip (e.g. when selecting a menu item) will bubble up
               // through the portal to parents in the React tree (as opposed to DOM parents).
