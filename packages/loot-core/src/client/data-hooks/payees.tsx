dc47c2d9b86071a12045321b0a1ec77a6a86407c
Mohamed Muhsin <62111075+muhsinkamil@users.noreply.github.com>
2023-09-17 20:05:32 +0200

    [refactor] Migrate Schedules Table to typescript (#1691)

--- packages/loot-core/src/client/data-hooks/payees.tsx
+++ packages/loot-core/src/client/data-hooks/payees.tsx
@@ -1,26 +1,33 @@
 import React, { createContext, useContext } from 'react';
 
+import { type PayeeEntity } from '../../types/models';
 import q from '../query-helpers';
 import { useLiveQuery } from '../query-hooks';
 import { getPayeesById } from '../reducers/queries';
 
-function usePayees() {
+function usePayees(): PayeeEntity[] {
   return useLiveQuery(() => q('payees').select('*'), []);
 }
 
-let PayeesContext = createContext(null);
+const PayeesContext = createContext<PayeeEntity[]>(null);
 
 export function PayeesProvider({ children }) {
-  let data = usePayees();
+  const data = usePayees();
   return <PayeesContext.Provider value={data} children={children} />;
 }
 
 export function CachedPayees({ children, idKey }) {
-  let data = useCachedPayees({ idKey });
+  const data = useCachedPayees({ idKey });
   return children(data);
 }
 
-export function useCachedPayees({ idKey }: { idKey? } = {}) {
-  let data = useContext(PayeesContext);
+export function useCachedPayees(): PayeeEntity[];
+export function useCachedPayees({
+  idKey,
+}: {
+  idKey: boolean;
+}): Record<PayeeEntity['id'], PayeeEntity>;
+export function useCachedPayees({ idKey }: { idKey?: boolean } = {}) {
+  const data = useContext(PayeesContext);
   return idKey && data ? getPayeesById(data) : data;
 }
