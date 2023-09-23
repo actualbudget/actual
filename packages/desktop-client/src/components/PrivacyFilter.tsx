f1fda6229c39829ee80e4b722b652714e724a562
Crazypkr1099 <nicholas.lacasse430@gmail.com>
2023-09-22 22:07:18 -0400

    Cleaned up commented out code

--- packages/desktop-client/src/components/PrivacyFilter.tsx
+++ packages/desktop-client/src/components/PrivacyFilter.tsx
@@ -52,7 +52,6 @@ type PrivacyFilterProps = ComponentPropsWithRef<typeof View> & {
 };
 export default function PrivacyFilter({
   activationFilters,
-  //blurIntensity,
   children,
   ...props
 }: PrivacyFilterProps) {
@@ -67,15 +66,10 @@ export default function PrivacyFilter({
         typeof value === 'boolean' ? value : value(),
       ));
 
-  //let blurAmount = blurIntensity != null ? `${blurIntensity}px` : '3px';
-
   return !activate ? (
     <>{Children.toArray(children)}</>
   ) : (
     <PrivacyReplacement originalContent={children} {...props} />
-    // <BlurredOverlay blurIntensity={blurAmount} {...props}>
-    //   {children}
-    // </BlurredOverlay>
   );
 }
 
@@ -96,38 +90,6 @@ function PrivacyReplacement({
   );
 }
 
-// function BlurredOverlay({ blurIntensity, children, ...props }) {
-//   let [hovered, setHovered] = useState(false);
-//   let onHover = useCallback(() => setHovered(true), [setHovered]);
-//   let onHoverEnd = useCallback(() => setHovered(false), [setHovered]);
-
-//   let blurStyle = {
-//     ...(!hovered && {
-//       filter: `blur(${blurIntensity})`,
-//       WebkitFilter: `blur(${blurIntensity})`,
-//       // To fix blur performance issue in Safari.
-//       // https://graffino.com/til/CjT2jrcLHP-how-to-fix-filter-blur-performance-issue-in-safari
-//       transform: `translate3d(0, 0, 0)`,
-//     }),
-//   };
-
-//   let { style, ...restProps } = props;
-
-//   return (
-//     <View
-//       style={{
-//         display: style?.display ? style.display : 'inline-flex',
-//         ...blurStyle,
-//         ...style,
-//       }}
-//       onPointerEnter={onHover}
-//       onPointerLeave={onHoverEnd}
-//       {...restProps}
-//     >
-//       {children}
-//     </View>
-//   );
-// }
 export function mergeConditionalPrivacyFilterProps(
   defaultPrivacyFilterProps: PrivacyFilterProps = {},
   privacyFilter: ConditionalPrivacyFilterProps['privacyFilter'],
