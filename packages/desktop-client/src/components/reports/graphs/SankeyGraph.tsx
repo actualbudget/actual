6245d61536d533fd2b969969c4692ba90c5d3f43
shaan <skhoslajr@gmail.com>
2023-10-15 19:47:02 +0000

    remove is out

--- packages/desktop-client/src/components/reports/graphs/SankeyGraph.tsx
+++ packages/desktop-client/src/components/reports/graphs/SankeyGraph.tsx
@@ -23,7 +23,8 @@ const numberFormatterTooltip = (value: PotentialNumber): number | null => {
 };
 
 function SankeyNode({ x, y, width, height, index, payload }) {
-  const isOut = x + width + 6 > width;
+  // const isOut = x + width + 6 > containerWidth;
+  const isOut = false // not configured yet
   let payloadValue = Math.round(payload.value / 1000).toString();
   if (payload.value < 1000) {
     payloadValue = '<1k';
