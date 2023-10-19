5cc6bad34b0b1aa164020402b08538a73bb8ab21
Michael Clark <5285928+MikesGlitch@users.noreply.github.com>
2023-10-19 08:13:04 +0100

    Maintenance: Updating icons generator to output typescript (#1785)

---
+++ packages/desktop-client/src/icons/v1/LocationCurrent.tsx
@@ -0,0 +1,16 @@
+import * as React from 'react';
+import type { SVGProps } from 'react';
+const SvgLocationCurrent = (props: SVGProps<SVGSVGElement>) => (
+  <svg
+    {...props}
+    xmlns="http://www.w3.org/2000/svg"
+    viewBox="0 0 20 20"
+    style={{
+      color: 'inherit',
+      ...props.style,
+    }}
+  >
+    <path d="m0 0 20 8-8 4-2 8z" fill="currentColor" />
+  </svg>
+);
+export default SvgLocationCurrent;
---
+++ packages/desktop-client/src/icons/v1/Pause.tsx
@@ -0,0 +1,16 @@
+import * as React from 'react';
+import type { SVGProps } from 'react';
+const SvgPause = (props: SVGProps<SVGSVGElement>) => (
+  <svg
+    {...props}
+    xmlns="http://www.w3.org/2000/svg"
+    viewBox="0 0 20 20"
+    style={{
+      color: 'inherit',
+      ...props.style,
+    }}
+  >
+    <path d="M5 4h3v12H5V4zm7 0h3v12h-3V4z" fill="currentColor" />
+  </svg>
+);
+export default SvgPause;
