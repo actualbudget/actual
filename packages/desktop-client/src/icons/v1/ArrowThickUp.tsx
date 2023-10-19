817f225b65c5acd1aba8337b35db8e44c27f7191
Michael Clark <5285928+MikesGlitch@users.noreply.github.com>
2023-10-19 08:13:04 +0100

    Maintenance: Updating icons generator to output typescript (#1785)

---
+++ packages/desktop-client/src/icons/v1/ArrowThickUp.tsx
@@ -0,0 +1,16 @@
+import * as React from 'react';
+import type { SVGProps } from 'react';
+const SvgArrowThickUp = (props: SVGProps<SVGSVGElement>) => (
+  <svg
+    {...props}
+    xmlns="http://www.w3.org/2000/svg"
+    viewBox="0 0 20 20"
+    style={{
+      color: 'inherit',
+      ...props.style,
+    }}
+  >
+    <path d="M7 10v8h6v-8h5l-8-8-8 8h5z" fill="currentColor" />
+  </svg>
+);
+export default SvgArrowThickUp;
