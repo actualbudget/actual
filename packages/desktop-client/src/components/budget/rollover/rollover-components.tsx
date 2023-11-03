cfc21eba8bcafaee2f2d2514d5dc961e3232cfdc
Matiss Janis Aboltins <matiss@mja.lv>
2023-11-03 18:30:54 +0000

    :bug:  fix: bring back rollover arrow in budget page
    Closes #1845

--- packages/desktop-client/src/components/budget/rollover/rollover-components.tsx
+++ packages/desktop-client/src/components/budget/rollover/rollover-components.tsx
@@ -311,6 +311,7 @@ export const ExpenseCategoryMonth = memo(function ExpenseCategoryMonth({
       </Field>
       <Field
         name="balance"
+        truncate={false}
         width="flex"
         style={{ paddingRight: styles.monthRightPadding, textAlign: 'right' }}
       >
