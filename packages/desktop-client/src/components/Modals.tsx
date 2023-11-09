634fd216139ca27680257e3b786ca618e5b9f4d8
Julian Dominguez-Schatz <julian.dominguezschatz@gmail.com>
2023-11-09 00:29:01 -0500

    Undo shouldn't open modal if it wasn't open before

--- packages/desktop-client/src/components/Modals.tsx
+++ packages/desktop-client/src/components/Modals.tsx
@@ -137,7 +137,7 @@ export default function Modals() {
           return (
             <ManageRulesModal
               modalProps={modalProps}
-              payeeId={options.payeeId}
+              payeeId={options?.payeeId}
             />
           );
 
