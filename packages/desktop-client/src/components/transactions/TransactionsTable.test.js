b593c9d9ab62100127e3c82a835d48c882f50ce8
Joel Jeremy Marquez <joeljeremy.marquez@gmail.com>
2023-11-12 01:09:51 -0800

    Update tests

--- packages/desktop-client/src/components/transactions/TransactionsTable.test.js
+++ packages/desktop-client/src/components/transactions/TransactionsTable.test.js
@@ -463,13 +463,13 @@ describe('Transactions', () => {
     let tooltip = container.querySelector('[data-testid="autocomplete"]');
 
     // No item should be highlighted
-    let highlighted = tooltip.querySelector('[data-highlighted=true]');
+    let highlighted = tooltip.querySelector('[data-highlighted="true"]');
     expect(highlighted).toBe(null);
 
     await userEvent.keyboard('[ArrowDown][ArrowDown][ArrowDown][ArrowDown]');
 
     // The right item should be highlighted
-    highlighted = tooltip.querySelector('[data-highlighted=true]');
+    highlighted = tooltip.querySelector('[data-highlighted="true"]');
     expect(highlighted).toBeTruthy();
     expect(highlighted.textContent).toBe('General');
 
@@ -504,14 +504,14 @@ describe('Transactions', () => {
 
     // Make sure none of the items are highlighted
     let items = tooltip.querySelectorAll('[data-testid="category-item"]');
-    let highlighted = tooltip.querySelector('[data-highlighted=true]');
+    let highlighted = tooltip.querySelector('[data-highlighted="true"]');
     expect(highlighted).toBe(null);
 
     // Hover over an item
     await userEvent.hover(items[2]);
 
     // Make sure the expected category is highlighted
-    highlighted = tooltip.querySelector('[data-highlighted=true]');
+    highlighted = tooltip.querySelector('[data-highlighted="true"]');
     expect(highlighted).toBeTruthy();
     expect(highlighted.textContent).toBe('General');
 
@@ -545,7 +545,7 @@ describe('Transactions', () => {
     await userEvent.hover(items[3]);
 
     // Make sure one of them is highlighted
-    let highlighted = tooltip.querySelector('[data-highlighted=true]');
+    let highlighted = tooltip.querySelector('[data-highlighted="true"]');
     expect(highlighted).toBeTruthy();
 
     // Navigate away from the field with the keyboard
