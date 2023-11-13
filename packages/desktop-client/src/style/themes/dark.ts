8c96010b9545b1837690ea494a640f2f98a2ef9b
carkom <neilmccreery@yahoo.com>
2023-11-13 16:14:17 +0000

    darkTheme header background

--- packages/desktop-client/src/style/themes/dark.ts
+++ packages/desktop-client/src/style/themes/dark.ts
@@ -70,7 +70,7 @@ export const menuAutoCompleteTextHeader = menuItemTextHeader;
 
 export const modalBackground = colorPalette.gray800;
 export const modalBorder = colorPalette.navy600;
-export const mobileHeaderBackground = colorPalette.purple600;
+export const mobileHeaderBackground = colorPalette.purple800;
 export const mobileHeaderText = colorPalette.navy150;
 export const mobileHeaderTextSubdued = colorPalette.gray200;
 export const mobileHeaderTextHover = 'rgba(200, 200, 200, .15)';
