cd6bd9ece8b5142e911ebc153f425048658382a7
Julian Dominguez-Schatz <julian.dominguezschatz@gmail.com>
2023-11-21 03:19:31 -0500

    Fix crash when undoing a rule application (#1885)
    * Undo shouldn't open modal if it wasn't open before
    
    * Add release notes

--- packages/desktop-client/src/components/Modals.tsx
+++ packages/desktop-client/src/components/Modals.tsx
@@ -139,7 +139,7 @@ export default function Modals() {
           return (
             <ManageRulesModal
               modalProps={modalProps}
-              payeeId={options.payeeId}
+              payeeId={options?.payeeId}
             />
           );
 
