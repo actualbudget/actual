90defa2261b3296abd8ca8b45bfba7ec95e7d9fc
DJ Mountney <david.mountney@twkie.net>
2023-11-23 00:36:15 -0800

    Create category rules for ynab imports (#1944)

--- packages/loot-core/src/server/accounts/sync.ts
+++ packages/loot-core/src/server/accounts/sync.ts
@@ -705,7 +705,7 @@ export async function reconcileTransactions(acctId, transactions) {
 export async function addTransactions(
   acctId,
   transactions,
-  { runTransfers = true } = {},
+  { runTransfers = true, learnCategories = false } = {},
 ) {
   const added = [];
 
@@ -737,8 +737,12 @@ export async function addTransactions(
   await createNewPayees(payeesToCreate, added);
 
   let newTransactions;
-  if (runTransfers) {
-    let res = await batchUpdateTransactions({ added });
+  if (runTransfers || learnCategories) {
+    let res = await batchUpdateTransactions({
+      added,
+      learnCategories,
+      runTransfers,
+    });
     newTransactions = res.added.map(t => t.id);
   } else {
     await batchMessages(async () => {
