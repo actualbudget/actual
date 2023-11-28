29f323e721107ce1acbcdfadeae1d8dd195334a5
Stefan Hall <stefan-hall@hotmail.com>
2023-11-29 08:19:18 +1300

    Validates minimum node version to 18.12.0 for @actual-app/api npm package (#1980)

--- packages/api/index.js
+++ packages/api/index.js
@@ -3,6 +3,7 @@
 // eslint-disable-next-line import/extensions
 import * as bundle from './app/bundle.api.js';
 import * as injected from './injected';
+import { validateNodeVersion } from './validateNodeVersion';
 
 let actualApp;
 export const internal = bundle.lib;
@@ -17,6 +18,8 @@ export async function init(config = {}) {
   if (actualApp) {
     return;
   }
+
+  validateNodeVersion();
 
   global.fetch = (...args) =>
     import('node-fetch').then(({ default: fetch }) => fetch(...args));
