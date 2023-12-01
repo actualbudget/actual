2ba235c13fd4c3b931b787a7419bdce7254c9763
DJ Mountney <david@twkie.net>
2023-11-30 16:06:16 -0800

    Ensure test --watch doesn't loop

--- packages/api/jest.config.js
+++ packages/api/jest.config.js
@@ -15,6 +15,7 @@ module.exports = {
   ],
   testEnvironment: 'node',
   testPathIgnorePatterns: ['/node_modules/'],
+  watchPathIgnorePatterns: ['<rootDir>/mocks/budgets/'],
   setupFilesAfterEnv: ['<rootDir>/../loot-core/src/mocks/setup.ts'],
   transformIgnorePatterns: ['/node_modules/'],
   transform: {
