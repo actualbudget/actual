2a0ddf99fba6199d26563677650c46ca98ef5e1b
Ameek Singh <ameeks.singh@mail.utoronto.ca>
2023-12-02 14:48:42 -0500

    migrating usereport to tsx

---
+++ packages/desktop-client/src/components/reports/useReport.tsx
@@ -0,0 +1,33 @@
+import { useState, useEffect } from 'react';
+
+import { useSpreadsheet } from 'loot-core/src/client/SpreadsheetProvider';
+import Spreadsheet from 'loot-core/src/server/spreadsheet/spreadsheet';
+
+type useReportProps = {
+  sheetName: string;
+  getData: (
+    spreadsheet: ReturnType<typeof useSpreadsheet>,
+    useResults: (results: unknown) => void,
+  ) => Promise<unknown>;
+};
+
+function useReport(props: useReportProps) {
+  const spreadsheet = useSpreadsheet();
+  const [results, setResults] = useState(null);
+
+  useEffect(() => {
+    let cleanup;
+    props
+      .getData(spreadsheet, results => setResults(results))
+      .then(c => {
+        cleanup = c;
+      });
+    return () => {
+      cleanup?.();
+    };
+  }, [props.getData]);
+
+  return results;
+}
+
+export default useReport;
