b0b6ce9555154b551b98ff05025254cffe4fe1fa
Ameek Singh <ameeks.singh@mail.utoronto.ca>
2023-12-02 14:26:38 -0500

    fixing typo in var name, rearranging imports

--- packages/desktop-client/src/components/reports/DateRange.tsx
+++ packages/desktop-client/src/components/reports/DateRange.tsx
@@ -2,33 +2,34 @@ import React from 'react';
 
 import * as d from 'date-fns';
 
-import Block from '../common/Block';
 import { theme } from '../../style';
+import Block from '../common/Block';
 
 type DateRangeProps = {
   start: string;
   end: string;
-}
+};
 
 function DateRange({ start, end }: DateRangeProps): JSX.Element {
-  const parseStart  = d.parseISO(start);
-  const parsedEnd = d.parseISO(end);
+  const parseStart = d.parseISO(start);
+  const parseEnd = d.parseISO(end);
 
   let content: string | React.ReactElement;
-  if (parseStart.getFullYear() !== parsedEnd.getFullYear()) {
-    content = ( 
-      <div>
-        `${d.format(parseStart, 'MMM yyyy')} - ${d.format(parsedEnd, 'MMM yyyy')}`
-      </div>
-    );
-  } else if (parseStart.getMonth() !== parsedEnd.getMonth()) {
+  if (parseStart.getFullYear() !== parseEnd.getFullYear()) {
     content = (
       <div>
-        {d.format(parseStart, 'MMM')} - {d.format(parsedEnd, 'MMM yyyy')}
+        `${d.format(parseStart, 'MMM yyyy')} - ${d.format(parseEnd, 'MMM yyyy')}
+        `
+      </div>
+    );
+  } else if (parseStart.getMonth() !== parseEnd.getMonth()) {
+    content = (
+      <div>
+        {d.format(parseStart, 'MMM')} - {d.format(parseEnd, 'MMM yyyy')}
       </div>
     );
   } else {
-    content = d.format(parsedEnd, 'MMMM yyyy');
+    content = d.format(parseEnd, 'MMMM yyyy');
   }
 
   return <Block style={{ color: theme.pageTextSubdued }}>{content}</Block>;
