141bd1290da7017eb723cccc4641f174a38cb3b7
Tuhin Ghose <tuhin.ghose@mail.utoronto.ca>
2023-12-04 15:43:52 -0500

    tried implementing generics for indexCashFlow

--- packages/desktop-client/src/components/reports/util.ts
+++ packages/desktop-client/src/components/reports/util.ts
@@ -24,7 +24,7 @@ export function index<T, K extends keyof T>(
 ) {
   const result: Record<K, T[K]> = {} as Record<K, T[K]>;
   data.forEach(item => {
-    result[mapper ? mapper(item[field]) : (item[field] as unknown as string)] =
+    result[mapper ? mapper(item[field]) : (item[field] as string)] =
       item;
   });
   return result;
@@ -42,7 +42,7 @@ export function indexStack<T, K extends keyof T>(
   return result;
 }
 
-export function indexCashFlow(data, date: string, isTransfer: string) {
+export function indexCashFlow<T, K extends keyof T>(data, date: K, isTransfer: K) {
   const results = {};
   data.forEach(item => {
     let findExisting = results[item.date]
