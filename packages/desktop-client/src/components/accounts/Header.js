42776f8029bf0c2c391ba6290bede752156f35f7
Michael Clark <5285928+MikesGlitch@users.noreply.github.com>
2023-12-04 18:58:57 +0000

    Update packages/desktop-client/src/components/accounts/Header.js
    Co-authored-by: Matiss Janis Aboltins <matiss@mja.lv>

--- packages/desktop-client/src/components/accounts/Header.js
+++ packages/desktop-client/src/components/accounts/Header.js
@@ -168,7 +168,7 @@ export function AccountHeader({
                 )}
                 <Button
                   type="bare"
-                  aria-label="Edit"
+                  aria-label="Edit account name"
                   className="hover-visible"
                   onClick={() => onExposeName(true)}
                 >
