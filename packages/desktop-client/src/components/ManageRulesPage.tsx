795187c9e64a70d0174a83160ff6318d49fe1b7f
Joel Jeremy Marquez <joeljeremy.marquez@gmail.com>
2023-12-05 11:57:56 -0800

    Remove Page titleStyle

--- packages/desktop-client/src/components/ManageRulesPage.tsx
+++ packages/desktop-client/src/components/ManageRulesPage.tsx
@@ -1,23 +1,11 @@
 import React from 'react';
 
-import { useResponsive } from '../ResponsiveProvider';
-import { theme } from '../style';
-
 import { ManageRules } from './ManageRules';
 import { Page } from './Page';
 
 export function ManageRulesPage() {
-  const { isNarrowWidth } = useResponsive();
   return (
-    <Page
-      title="Rules"
-      titleStyle={{
-        ...(isNarrowWidth && {
-          backgroundColor: theme.mobileHeaderBackground,
-          color: theme.mobileHeaderText,
-        }),
-      }}
-    >
+    <Page title="Rules">
       <ManageRules isModal={false} payeeId={null} />
     </Page>
   );
--- packages/desktop-client/src/components/payees/ManagePayeesPage.jsx
+++ packages/desktop-client/src/components/payees/ManagePayeesPage.jsx
@@ -1,25 +1,14 @@
 import React from 'react';
 import { useLocation } from 'react-router-dom';
 
-import { useResponsive } from '../../ResponsiveProvider';
-import { theme } from '../../style';
 import { Page } from '../Page';
 
 import { ManagePayeesWithData } from './ManagePayeesWithData';
 
 export function ManagePayeesPage() {
   const location = useLocation();
-  const { isNarrowWidth } = useResponsive();
   return (
-    <Page
-      title="Payees"
-      titleStyle={{
-        ...(isNarrowWidth && {
-          backgroundColor: theme.mobileHeaderBackground,
-          color: theme.mobileHeaderText,
-        }),
-      }}
-    >
+    <Page title="Payees">
       <ManagePayeesWithData
         initialSelectedIds={
           location.state && location.state.selectedPayee
