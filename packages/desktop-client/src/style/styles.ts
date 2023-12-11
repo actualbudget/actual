d653e72b47bad5d92ecf3bbb51a510a30a1d8da4
Mike Clark <mclarkgb@gmail.com>
2023-12-11 16:51:23 +0000

    save that 20px for the demo env

--- packages/desktop-client/src/style/styles.ts
+++ packages/desktop-client/src/style/styles.ts
@@ -8,7 +8,7 @@ import { theme } from './theme';
 import { type CSSProperties } from './types';
 
 export const styles = {
-  incomeHeaderHeight: 70,
+  incomeHeaderHeight: 50,
   cardShadow: '0 1px 3px rgba(0,0,0,0.12), 0 1px 2px rgba(0,0,0,0.24)',
   monthRightPadding: 5,
   menuBorderRadius: 4,
