6b4de0f710bdb47b5615a636a7386f86cfb04a82
Matiss Janis Aboltins <matiss@mja.lv>
2023-12-12 13:22:10 +0000

    Test and lint fixes

--- packages/desktop-client/src/components/transactions/TransactionsTable.js
+++ packages/desktop-client/src/components/transactions/TransactionsTable.js
@@ -1722,7 +1722,7 @@ function TransactionTableInner({
       >
         <Table
           navigator={tableNavigator}
-          ref={tableRef}
+          tableRef={tableRef}
           items={props.transactions}
           renderItem={renderRow}
           renderEmpty={renderEmpty}
