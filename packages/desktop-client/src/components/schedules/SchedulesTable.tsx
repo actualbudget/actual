6d7e67fa4f7814af428ed43cce9b916632e7d802
Matiss Janis Aboltins <matiss@mja.lv>
2023-12-30 17:41:02 +0000

    ♻️ (TypeScript) fix strictFunctionTypes violations (pt 4)

--- packages/desktop-client/src/components/schedules/SchedulesTable.tsx
+++ packages/desktop-client/src/components/schedules/SchedulesTable.tsx
@@ -118,7 +118,7 @@ function OverflowMenu({
           onClose={() => setOpen(false)}
         >
           <Menu
-            onMenuSelect={(name: ScheduleItemAction) => {
+            onMenuSelect={name => {
               onAction(name, schedule.id);
               setOpen(false);
             }}
