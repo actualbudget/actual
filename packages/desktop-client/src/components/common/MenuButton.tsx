bc6251710db732cfb0acb7ecbfcb6d8ecf03ea35
carkom <neilmccreery@yahoo.com>
2024-01-18 10:58:38 +0000

    vrt fix

--- packages/desktop-client/src/components/common/MenuButton.tsx
+++ packages/desktop-client/src/components/common/MenuButton.tsx
@@ -1,11 +1,17 @@
-// @ts-strict-ignore
 import React from 'react';
 
 import { SvgDotsHorizontalTriple } from '../../icons/v1';
+import { type CSSProperties } from '../../style';
 
 import { Button } from './Button';
 
-export function MenuButton({ onClick, style }) {
+export function MenuButton({
+  onClick,
+  style,
+}: {
+  onClick: () => void;
+  style?: CSSProperties;
+}) {
   return (
     <Button type="bare" onClick={onClick} aria-label="Menu" style={style}>
       <SvgDotsHorizontalTriple
