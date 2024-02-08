09042f3f822fc597babbd8641c55f34ad90d0ebb
Matiss Janis Aboltins <matiss@mja.lv>
2024-02-08 20:36:32 +0000

    :bug: (typescript) patching sendCatch type

--- packages/loot-core/src/platform/client/fetch/index.d.ts
+++ packages/loot-core/src/platform/client/fetch/index.d.ts
@@ -14,7 +14,10 @@ export type Send = typeof send;
 export function sendCatch<K extends keyof Handlers>(
   name: K,
   args?: Parameters<Handlers[K]>[0],
-): ReturnType<Handlers[K]>;
+): ReturnType<
+  | { data: Handlers[K] }
+  | { error: { type: 'APIError' | 'InternalError'; message: string } }
+>;
 export type SendCatch = typeof sendCatch;
 
 export function listen<K extends keyof ServerEvents>(
