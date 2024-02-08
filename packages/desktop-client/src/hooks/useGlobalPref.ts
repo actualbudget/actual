7d8200f797f81dea9b93fe8791e37213b1c68d6e
Joel Jeremy Marquez <joeljeremy.marquez@gmail.com>
2024-02-08 10:53:25 -0800

    Fix typecheck error

--- packages/desktop-client/src/hooks/useGlobalPref.ts
+++ packages/desktop-client/src/hooks/useGlobalPref.ts
@@ -5,13 +5,17 @@ import { saveGlobalPrefs } from 'loot-core/src/client/actions';
 import { type State } from 'loot-core/src/client/state-types';
 import { type GlobalPrefs } from 'loot-core/src/types/prefs';
 
+type SetGlobalPrefActon<K extends keyof GlobalPrefs> = (
+  value: GlobalPrefs[K],
+) => void;
+
 export function useGlobalPref<K extends keyof GlobalPrefs>(
   prefName: K,
   defaultValue?: GlobalPrefs[K],
-): [GlobalPrefs[K], (value: GlobalPrefs[K]) => void] {
+): [GlobalPrefs[K], SetGlobalPrefActon<K>] {
   const dispatch = useDispatch();
   const setGlobalPref = useCallback(
-    (value: GlobalPrefs[K]) => {
+    value => {
       dispatch(saveGlobalPrefs({ [prefName]: value }));
     },
     [prefName, dispatch],
--- packages/desktop-client/src/hooks/useLocalPref.ts
+++ packages/desktop-client/src/hooks/useLocalPref.ts
@@ -5,13 +5,17 @@ import { savePrefs } from 'loot-core/src/client/actions';
 import { type State } from 'loot-core/src/client/state-types';
 import { type LocalPrefs } from 'loot-core/src/types/prefs';
 
+type SetLocalPrefAction<K extends keyof LocalPrefs> = (
+  value: LocalPrefs[K],
+) => void;
+
 export function useLocalPref<K extends keyof LocalPrefs>(
   prefName: K,
   defaultValue?: LocalPrefs[K],
-): [LocalPrefs[K], (value: LocalPrefs[K]) => void] {
+): [LocalPrefs[K], SetLocalPrefAction<K>] {
   const dispatch = useDispatch();
   const setLocalPref = useCallback(
-    (value: LocalPrefs[K]) => {
+    value => {
       dispatch(savePrefs({ [prefName]: value }));
     },
     [prefName, dispatch],
