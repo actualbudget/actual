a6df36e65cef6d3e5389afe14046276de4f81da8
Joel Jeremy Marquez <joeljeremy.marquez@gmail.com>
2024-02-10 02:23:26 -0800

    Set default theme

--- packages/desktop-client/src/style/theme.tsx
+++ packages/desktop-client/src/style/theme.tsx
@@ -24,11 +24,8 @@ export const themeOptions = Object.entries(themes).map(
 );
 
 export function useTheme() {
-  const [theme, setThemePref] = useGlobalPref('theme');
-  return [theme || 'light', setThemePref] as [
-    typeof theme,
-    typeof setThemePref,
-  ];
+  const [theme = 'light', setThemePref] = useGlobalPref('theme');
+  return [theme, setThemePref] as [typeof theme, typeof setThemePref];
 }
 
 export function ThemeStyle() {
