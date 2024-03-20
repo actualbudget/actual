4569a3032d2b23dc93b6dd99c16e67b44c856114
Joel Jeremy Marquez <joeljeremy.marquez@gmail.com>
2024-03-20 09:38:54 -0700

    Pass this to apply

--- packages/desktop-client/src/components/modals/CategoryAutocompleteModal.tsx
+++ packages/desktop-client/src/components/modals/CategoryAutocompleteModal.tsx
@@ -112,7 +112,7 @@ export function CategoryAutocompleteModal({
               {...defaultAutocompleteProps}
               {...autocompleteProps}
               onSelect={(...args) => {
-                autocompleteProps?.onSelect?.apply(null, args);
+                autocompleteProps?.onSelect?.apply(this, args);
                 _onClose();
               }}
             />
