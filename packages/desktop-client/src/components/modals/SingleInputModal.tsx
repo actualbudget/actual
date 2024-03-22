1dee0b7dcd1367659065262f14aa287cbd0ab7f9
Joel Jeremy Marquez <joeljeremy.marquez@gmail.com>
2024-03-21 22:17:45 -0700

    Improve SingleInputModal

--- packages/desktop-client/src/components/modals/SingleInputModal.tsx
+++ packages/desktop-client/src/components/modals/SingleInputModal.tsx
@@ -9,6 +9,7 @@ import { Modal } from '../common/Modal';
 import { View } from '../common/View';
 import { InputField } from '../mobile/MobileForms';
 import { type CommonModalProps } from '../Modals';
+import { InputField } from '../mobile/MobileForms';
 
 type SingleInputModalProps = {
   modalProps: Partial<CommonModalProps>;
