c6d2227825aef0b1ee7fa37f6fb95e8d695b4cb0
Joel Jeremy Marquez <joeljeremy.marquez@gmail.com>
2024-03-23 18:16:23 -0700

    Modal logo

--- packages/desktop-client/src/components/modals/AccountMenuModal.tsx
+++ packages/desktop-client/src/components/modals/AccountMenuModal.tsx
@@ -10,7 +10,7 @@ import { SvgNotesPaper } from '../../icons/v2';
 import { type CSSProperties, styles, theme } from '../../style';
 import { Button } from '../common/Button';
 import { Menu } from '../common/Menu';
-import { Modal } from '../common/Modal';
+import { Modal, ModalTitle } from '../common/Modal';
 import { View } from '../common/View';
 import { type CommonModalProps } from '../Modals';
 import { Notes } from '../Notes';
@@ -88,8 +88,9 @@ export function AccountMenuModal({
 
   return (
     <Modal
-      title={account.name}
-      titleStyle={styles.underlinedText}
+      title={
+        <ModalTitle isEditable title={account.name} onTitleUpdate={onRename} />
+      }
       showHeader
       focusAfterClose={false}
       {...modalProps}
@@ -101,8 +102,6 @@ export function AccountMenuModal({
         padding: '0 10px',
         borderRadius: '6px',
       }}
-      editableTitle={true}
-      onTitleUpdate={onRename}
       leftHeaderContent={
         <AdditionalAccountMenu
           account={account}
@@ -111,63 +110,57 @@ export function AccountMenuModal({
         />
       }
     >
-      {({ isEditingTitle }) => (
+      <View
+        style={{
+          flex: 1,
+          flexDirection: 'column',
+        }}
+      >
         <View
           style={{
+            overflowY: 'auto',
             flex: 1,
-            flexDirection: 'column',
           }}
         >
-          <View
-            style={{
-              overflowY: 'auto',
-              flex: 1,
-            }}
-          >
-            <Notes
-              notes={
-                originalNotes && originalNotes.length > 0
-                  ? originalNotes
-                  : 'No notes'
-              }
-              editable={false}
-              focused={false}
-              getStyle={() => ({
-                borderRadius: 6,
-                ...((!originalNotes || originalNotes.length === 0) && {
-                  justifySelf: 'center',
-                  alignSelf: 'center',
-                  color: theme.pageTextSubdued,
-                }),
-              })}
-            />
-          </View>
-          <View
-            style={{
-              flexDirection: 'row',
-              flexWrap: 'wrap',
-              justifyContent: 'space-between',
-              alignContent: 'space-between',
-              margin: '10px 0',
-            }}
-          >
-            <Button
-              style={{
-                ...buttonStyle,
-                display: isEditingTitle ? 'none' : undefined,
-              }}
-              onClick={_onEditNotes}
-            >
-              <SvgNotesPaper
-                width={20}
-                height={20}
-                style={{ paddingRight: 5 }}
-              />
-              Edit notes
-            </Button>
-          </View>
+          <Notes
+            notes={
+              originalNotes && originalNotes.length > 0
+                ? originalNotes
+                : 'No notes'
+            }
+            editable={false}
+            focused={false}
+            getStyle={() => ({
+              borderRadius: 6,
+              ...((!originalNotes || originalNotes.length === 0) && {
+                justifySelf: 'center',
+                alignSelf: 'center',
+                color: theme.pageTextSubdued,
+              }),
+            })}
+          />
         </View>
-      )}
+        <View
+          style={{
+            flexDirection: 'row',
+            flexWrap: 'wrap',
+            justifyContent: 'space-between',
+            alignContent: 'space-between',
+            margin: '10px 0',
+          }}
+        >
+          <Button
+            style={{
+              ...buttonStyle,
+              display: isEditingTitle ? 'none' : undefined,
+            }}
+            onClick={_onEditNotes}
+          >
+            <SvgNotesPaper width={20} height={20} style={{ paddingRight: 5 }} />
+            Edit notes
+          </Button>
+        </View>
+      </View>
     </Modal>
   );
 }
