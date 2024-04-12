a2c128e0f9eefbf226ee5d71e6c7b9837d91a657
Joel Jeremy Marquez <joeljeremy.marquez@gmail.com>
2024-04-12 16:51:26 -0700

    Fix lint error

--- packages/desktop-client/src/hooks/useCategoryGroup.ts
+++ packages/desktop-client/src/hooks/useCategoryGroup.ts
@@ -4,5 +4,8 @@ import { useCategories } from './useCategories';
 
 export function useCategoryGroup(id: string) {
   const { grouped: categoryGroups } = useCategories();
-  return useMemo(() => categoryGroups.find(g => g.id === id), [id, categoryGroups]);
+  return useMemo(
+    () => categoryGroups.find(g => g.id === id),
+    [id, categoryGroups],
+  );
 }
