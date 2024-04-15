d1256354c7a30a8b927670de36ebef5e28ac66bb
Joseph Livecchi <joewashear007@gmail.com>
2024-04-15 12:07:56 -0400

    Update packages/desktop-client/src/components/manager/subscribe/Login.tsx
    Co-authored-by: DJ Mountney <david.mountney@twkie.net>

--- packages/desktop-client/src/components/manager/subscribe/Login.tsx
+++ packages/desktop-client/src/components/manager/subscribe/Login.tsx
@@ -10,7 +10,7 @@ import { send } from 'loot-core/src/platform/client/fetch';
 import { AnimatedLoading } from '../../../icons/AnimatedLoading';
 import { theme } from '../../../style';
 import { Button, ButtonWithLoading } from '../../common/Button';
-import { ButtonLink } from '../../common/ButtonLink';
+import { ButtonLink } from '../../common/Link';
 import { BigInput } from '../../common/Input';
 import { Text } from '../../common/Text';
 import { View } from '../../common/View';
