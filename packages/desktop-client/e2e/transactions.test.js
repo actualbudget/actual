a86d58d749f3767b00325e3325f9442bc19dc065
Matiss Janis Aboltins <matiss@mja.lv>
2024-04-18 20:50:15 +0100

    Patch datepicker selector

--- packages/desktop-client/e2e/transactions.test.js
+++ packages/desktop-client/e2e/transactions.test.js
@@ -36,7 +36,7 @@ test.describe('Transactions', () => {
       await accountPage.removeFilter(0);
     });
 
-    test('by date', async () => {
+    test.only('by date', async () => {
       const filterTooltip = await accountPage.filterBy('Date');
       await expect(filterTooltip.page).toMatchThemeScreenshots();
 
@@ -46,7 +46,7 @@ test.describe('Transactions', () => {
       await expect(datepicker).toMatchThemeScreenshots();
 
       // Select "is xxxxx"
-      await datepicker.getByRole('button', { name: '20' }).click();
+      await datepicker.getByText('20', { exact: true }).click();
       await filterTooltip.applyButton.click();
 
       // Assert that there are no transactions
