843f7aa99779be7211c59aa34661862421c03b30
Joel Jeremy Marquez <joeljeremy.marquez@gmail.com>
2024-04-22 08:14:55 -0700

    Fix lint error

--- packages/desktop-client/src/components/modals/TransferModal.tsx
+++ packages/desktop-client/src/components/modals/TransferModal.tsx
@@ -1,4 +1,4 @@
-import React, { useMemo, useRef, useState } from 'react';
+import React, { useMemo, useState } from 'react';
 import { useDispatch } from 'react-redux';
 
 import { pushModal } from 'loot-core/client/actions';
