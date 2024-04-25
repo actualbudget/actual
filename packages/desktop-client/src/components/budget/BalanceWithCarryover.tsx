6aa68f43474911150c6abda6bc6e88f1003fb3ee
Joel Jeremy Marquez <joeljeremy.marquez@gmail.com>
2024-04-25 15:19:49 -0700

    Add amount colors to mobile transaction list

--- packages/desktop-client/src/components/budget/BalanceWithCarryover.tsx
+++ packages/desktop-client/src/components/budget/BalanceWithCarryover.tsx
@@ -8,7 +8,7 @@ import { View } from '../common/View';
 import { CellValue } from '../spreadsheet/CellValue';
 import { useSheetValue } from '../spreadsheet/useSheetValue';
 
-import { makeAmountStyle } from './util';
+import { makeBalanceAmountStyle } from './util';
 
 type BalanceWithCarryoverProps = {
   carryover: ComponentProps<typeof CellValue>['binding'];
@@ -41,7 +41,7 @@ export function BalanceWithCarryover({
         binding={balance}
         type="financial"
         getStyle={value =>
-          makeAmountStyle(
+          makeBalanceAmountStyle(
             value,
             isGoalTemplatesEnabled ? goalValue : null,
             budgetedValue,
@@ -72,7 +72,7 @@ export function BalanceWithCarryover({
           <SvgArrowThinRight
             width={carryoverStyle?.width || 7}
             height={carryoverStyle?.height || 7}
-            style={makeAmountStyle(balanceValue, goalValue, budgetedValue)}
+            style={makeBalanceAmountStyle(balanceValue, goalValue, budgetedValue)}
           />
         </View>
       )}
