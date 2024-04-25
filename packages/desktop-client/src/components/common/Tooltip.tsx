90cec965bf672840c65be4044c0fc076a7e594f3
Joel Jeremy Marquez <joeljeremy.marquez@gmail.com>
2024-04-25 14:40:35 -0700

    [Mobile] Add schedule name and date to scheduled transaction modal

--- packages/desktop-client/src/components/common/Tooltip.tsx
+++ packages/desktop-client/src/components/common/Tooltip.tsx
@@ -1,12 +1,7 @@
-import React, {
-  useCallback,
-  useEffect,
-  useRef,
-  useState,
-  type ComponentProps,
-  type ReactNode,
-} from 'react';
+import React, { useRef, type ComponentProps, type ReactNode } from 'react';
 import { Tooltip as AriaTooltip, TooltipTrigger } from 'react-aria-components';
+
+import { useHover } from 'usehooks-ts';
 
 import { styles } from '../../style';
 
@@ -25,44 +20,12 @@ export const Tooltip = ({
   ...props
 }: TooltipProps) => {
   const triggerRef = useRef(null);
-  const [hover, setHover] = useState(false);
-
-  const [delayHandler, setDelayHandler] = useState<ReturnType<
-    typeof setTimeout
-  > | null>(null);
-
-  const handleMouseEnter = useCallback(() => {
-    const timeout = setTimeout(() => {
-      setHover(true);
-    }, triggerProps.delay ?? 300);
-
-    setDelayHandler(timeout);
-    return () => {
-      clearTimeout(timeout);
-    };
-  }, [triggerProps.delay]);
-
-  const handleMouseLeave = useCallback(() => {
-    if (delayHandler) {
-      clearTimeout(delayHandler);
-    }
-
-    setHover(false);
-  }, [delayHandler]);
-
-  // Force closing the tooltip whenever the disablement state changes
-  useEffect(() => {
-    setHover(false);
-  }, [triggerProps.isDisabled]);
+  const isHovered = useHover(triggerRef);
 
   return (
-    <View
-      ref={triggerRef}
-      onMouseEnter={handleMouseEnter}
-      onMouseLeave={handleMouseLeave}
-    >
+    <View ref={triggerRef}>
       <TooltipTrigger
-        isOpen={hover && !triggerProps.isDisabled}
+        isOpen={isHovered && !triggerProps.isDisabled}
         {...triggerProps}
       >
         {children}
