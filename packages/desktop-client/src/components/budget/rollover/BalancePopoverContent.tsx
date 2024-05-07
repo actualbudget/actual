42776a4d5cd2b35d6d030fbe0b716503046f05ed
Matiss Janis Aboltins <matiss@mja.lv>
2024-05-07 19:03:19 +0100

    Rename BalanceTooltip to BalancePopoverContent

--- packages/desktop-client/src/components/budget/rollover/BalancePopoverContent.tsx
+++ packages/desktop-client/src/components/budget/rollover/BalancePopoverContent.tsx
@@ -8,19 +8,19 @@ import { BalanceMenu } from './BalanceMenu';
 import { CoverMenu } from './CoverMenu';
 import { TransferMenu } from './TransferMenu';
 
-type BalanceMenuProps = {
+type BalancePopoverContentProps = {
   categoryId: string;
   month: string;
   onBudgetAction: (month: string, action: string, arg?: unknown) => void;
   onClose?: () => void;
 };
 
-export function BalanceMenu({
+export function BalancePopoverContent({
   categoryId,
   month,
   onBudgetAction,
   onClose = () => {},
-}: BalanceMenuProps) {
+}: BalancePopoverContentProps) {
   const catBalance = useSheetValue(rolloverBudget.catBalance(categoryId));
   const [menu, setMenu] = useState('menu');
 
