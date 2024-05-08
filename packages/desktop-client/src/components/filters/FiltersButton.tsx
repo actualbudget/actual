4b99fd38b53c49c8cef18a10ccd41430b7e0ae5a
carkom <neilmccreery@yahoo.com>
2024-05-08 20:25:13 +0100

    filter icon change

--- packages/desktop-client/src/components/filters/FiltersButton.tsx
+++ packages/desktop-client/src/components/filters/FiltersButton.tsx
@@ -1,13 +1,13 @@
 import React from 'react';
 
-import { SvgSettingsSliderAlternate } from '../../icons/v2';
+import { SvgFilter } from '../../icons/v1/Filter';
 import { Button } from '../common/Button';
 
 export function FiltersButton({ onClick }: { onClick: () => void }) {
   return (
     <Button type="bare" onClick={onClick} title="Filters">
-      <SvgSettingsSliderAlternate
-        style={{ width: 16, height: 16, marginRight: 5 }}
+      <SvgFilter
+        style={{ width: 12, height: 12, marginRight: 5 }}
       />{' '}
       Filter
     </Button>
