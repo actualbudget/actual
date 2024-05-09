189713291b45e78d4a00f50b68a3ebd2a8b86018
Joel Jeremy Marquez <joeljeremy.marquez@gmail.com>
2024-05-09 13:11:56 -0700

    Reports page data-testid

--- packages/desktop-client/src/components/reports/index.tsx
+++ packages/desktop-client/src/components/reports/index.tsx
@@ -1,14 +1,16 @@
+import { View } from '../common/View';
 import { LoadComponent } from '../util/LoadComponent';
 
 export function Reports() {
   return (
-    <LoadComponent
-      data-testid="reports-page"
-      name="ReportRouter"
-      message="Loading reports..."
-      importer={() =>
-        import(/* webpackChunkName: 'reports' */ './ReportRouter')
-      }
-    />
+    <View style={{ flex: 1 }} data-testid="reports-page">
+      <LoadComponent
+        name="ReportRouter"
+        message="Loading reports..."
+        importer={() =>
+          import(/* webpackChunkName: 'reports' */ './ReportRouter')
+        }
+      />
+    </View>
   );
 }
