5681c51d069b821c75b95f643d49341905e9bf8a
carkom <neilmccreery@yahoo.com>
2024-05-16 23:04:01 +0100

    Table Totals Callback

--- packages/desktop-client/src/components/reports/graphs/tableGraph/ReportTableTotals.tsx
+++ packages/desktop-client/src/components/reports/graphs/tableGraph/ReportTableTotals.tsx
@@ -1,59 +1,67 @@
 import React, {
-  type UIEventHandler,
+  type ReactNode,
   useLayoutEffect,
   useState,
   type RefObject,
 } from 'react';
 
 import {
-  amountToCurrency,
-  amountToInteger,
-  integerToCurrency,
-} from 'loot-core/src/shared/util';
-import { type DataEntity } from 'loot-core/src/types/models/reports';
-import { type RuleConditionEntity } from 'loot-core/types/models/rule';
+  type GroupedEntity,
+  type DataEntity,
+} from 'loot-core/src/types/models/reports';
 
-import { useAccounts } from '../../../../hooks/useAccounts';
-import { useCategories } from '../../../../hooks/useCategories';
-import { useNavigate } from '../../../../hooks/useNavigate';
-import { useResponsive } from '../../../../ResponsiveProvider';
 import { theme } from '../../../../style';
 import { styles } from '../../../../style/styles';
 import { type CSSProperties } from '../../../../style/types';
 import { View } from '../../../common/View';
-import { Row, Cell } from '../../../table';
-import { showActivity } from '../showActivity';
+
+import { type renderTotalsProps } from './ReportTable';
+
+type RenderTotalsRowProps = {
+  metadata: GroupedEntity;
+  mode: string;
+  totalsStyle: CSSProperties;
+  testStyle: CSSProperties;
+  scrollWidthTotals: number;
+  renderTotals: (arg: renderTotalsProps) => ReactNode;
+};
+function RenderTotalsRow({
+  metadata,
+  mode,
+  totalsStyle,
+  testStyle,
+  scrollWidthTotals,
+  renderTotals,
+}: RenderTotalsRowProps) {
+  return (
+    <View>
+      {renderTotals({
+        metadata,
+        mode,
+        totalsStyle,
+        testStyle,
+        scrollWidthTotals,
+      })}
+    </View>
+  );
+}
 
 type ReportTableTotalsProps = {
   data: DataEntity;
-  balanceTypeOp: 'totalAssets' | 'totalDebts' | 'totalTotals';
   mode: string;
-  intervalsCount: number;
   totalScrollRef: RefObject<HTMLDivElement>;
-  handleScroll: UIEventHandler<HTMLDivElement>;
   compact: boolean;
   style?: CSSProperties;
-  compactStyle?: CSSProperties;
-  groupBy: string;
-  filters?: RuleConditionEntity[];
-  showHiddenCategories?: boolean;
-  showOffBudget?: boolean;
+  renderTotals: (arg: renderTotalsProps) => ReactNode;
 };
 
 export function ReportTableTotals({
   data,
-  balanceTypeOp,
   mode,
-  intervalsCount,
   totalScrollRef,
-  handleScroll,
   compact,
   style,
-  compactStyle,
-  groupBy,
-  filters = [],
-  showHiddenCategories = false,
-  showOffBudget = false,
+  renderTotals,
 }: ReportTableTotalsProps) {
   const [scrollWidthTotals, setScrollWidthTotals] = useState(0);
 
@@ -70,214 +78,47 @@ export function ReportTableTotals({
       setScrollWidthTotals(parent > 0 && child > 0 ? parent - child : 0);
     }
   });
-  const average = amountToInteger(data[balanceTypeOp]) / intervalsCount;
 
-  const navigate = useNavigate();
-  const { isNarrowWidth } = useResponsive();
-  const categories = useCategories();
-  const accounts = useAccounts();
+  const metadata: GroupedEntity = {
+    id: '',
+    name: 'Totals',
+    intervalData: data.intervalData,
+    totalAssets: data.totalAssets,
+    totalDebts: data.totalDebts,
+    totalTotals: data.totalTotals,
+  };
 
-  const pointer =
-    !isNarrowWidth && !['Group', 'Interval'].includes(groupBy)
-      ? 'pointer'
-      : 'inherit';
+  const totalsStyle: CSSProperties = {
+    borderTopWidth: 1,
+    borderColor: theme.tableBorder,
+    justifyContent: 'center',
+    color: theme.tableHeaderText,
+    backgroundColor: theme.tableHeaderBackground,
+    fontWeight: 600,
+    ...style,
+  };
 
-  const hoverUnderline =
-    !isNarrowWidth && !['Group', 'Interval'].includes(groupBy)
-      ? {
-          cursor: pointer,
-          ':hover': { textDecoration: 'underline' },
-          flexGrow: 0,
-        }
-      : {};
+  const testStyle: CSSProperties = {
+    overflowX: 'auto',
+    scrollbarWidth: compact ? 'none' : 'inherit',
+    ...styles.horizontalScrollbar,
+    '::-webkit-scrollbar': {
+      backgroundColor: theme.tableBackground,
+      height: 12,
+      dispaly: compact && 'none',
+    },
+    flexDirection: 'row',
+    flex: 1,
+  };
 
   return (
-    <Row
-      collapsed={true}
-      height={32 + scrollWidthTotals}
-      style={{
-        borderTopWidth: 1,
-        borderColor: theme.tableBorder,
-        justifyContent: 'center',
-        color: theme.tableHeaderText,
-        backgroundColor: theme.tableHeaderBackground,
-        fontWeight: 600,
-        ...style,
-      }}
-    >
-      <View
-        innerRef={totalScrollRef}
-        onScroll={handleScroll}
-        id="total"
-        style={{
-          overflowX: 'auto',
-          scrollbarWidth: compact ? 'none' : 'inherit',
-          ...styles.horizontalScrollbar,
-          '::-webkit-scrollbar': {
-            backgroundColor: theme.tableBackground,
-            height: 12,
-            dispaly: compact && 'none',
-          },
-          flexDirection: 'row',
-          flex: 1,
-        }}
-      >
-        <Cell
-          style={{
-            width: compact ? 80 : 125,
-            flexShrink: 0,
-          }}
-          valueStyle={compactStyle}
-          value="Totals"
-        />
-        {mode === 'time'
-          ? data.intervalData.map(item => {
-              return (
-                <Cell
-                  style={{
-                    minWidth: compact ? 50 : 85,
-                  }}
-                  linkStyle={hoverUnderline}
-                  valueStyle={compactStyle}
-                  key={amountToCurrency(item[balanceTypeOp])}
-                  value={amountToCurrency(item[balanceTypeOp])}
-                  title={
-                    Math.abs(item[balanceTypeOp]) > 100000
-                      ? amountToCurrency(item[balanceTypeOp])
-                      : undefined
-                  }
-                  onClick={() =>
-                    !isNarrowWidth &&
-                    !['Group', 'Interval'].includes(groupBy) &&
-                    showActivity({
-                      navigate,
-                      categories,
-                      accounts,
-                      balanceTypeOp,
-                      filters,
-                      showHiddenCategories,
-                      showOffBudget,
-                      type: 'time',
-                      startDate: item.dateStart || '',
-                    })
-                  }
-                  width="flex"
-                  privacyFilter
-                />
-              );
-            })
-          : balanceTypeOp === 'totalTotals' && (
-              <>
-                <Cell
-                  style={{
-                    minWidth: compact ? 50 : 85,
-                  }}
-                  linkStyle={hoverUnderline}
-                  valueStyle={compactStyle}
-                  value={amountToCurrency(data.totalAssets)}
-                  title={
-                    Math.abs(data.totalAssets) > 100000
-                      ? amountToCurrency(data.totalAssets)
-                      : undefined
-                  }
-                  onClick={() =>
-                    !isNarrowWidth &&
-                    !['Group', 'Interval'].includes(groupBy) &&
-                    showActivity({
-                      navigate,
-                      categories,
-                      accounts,
-                      balanceTypeOp,
-                      filters,
-                      showHiddenCategories,
-                      showOffBudget,
-                      type: 'assets',
-                      startDate: data.startDate || '',
-                      endDate: data.endDate,
-                    })
-                  }
-                  width="flex"
-                  privacyFilter
-                />
-                <Cell
-                  style={{
-                    minWidth: compact ? 50 : 85,
-                  }}
-                  linkStyle={hoverUnderline}
-                  valueStyle={compactStyle}
-                  value={amountToCurrency(data.totalDebts)}
-                  title={
-                    Math.abs(data.totalDebts) > 100000
-                      ? amountToCurrency(data.totalDebts)
-                      : undefined
-                  }
-                  onClick={() =>
-                    !isNarrowWidth &&
-                    !['Group', 'Interval'].includes(groupBy) &&
-                    showActivity({
-                      navigate,
-                      categories,
-                      accounts,
-                      balanceTypeOp,
-                      filters,
-                      showHiddenCategories,
-                      showOffBudget,
-                      type: 'debts',
-                      startDate: data.startDate || '',
-                      endDate: data.endDate,
-                    })
-                  }
-                  width="flex"
-                  privacyFilter
-                />
-              </>
-            )}
-        <Cell
-          style={{
-            minWidth: compact ? 50 : 85,
-          }}
-          linkStyle={hoverUnderline}
-          valueStyle={compactStyle}
-          value={amountToCurrency(data[balanceTypeOp])}
-          title={
-            Math.abs(data[balanceTypeOp]) > 100000
-              ? amountToCurrency(data[balanceTypeOp])
-              : undefined
-          }
-          onClick={() =>
-            !isNarrowWidth &&
-            !['Group', 'Interval'].includes(groupBy) &&
-            showActivity({
-              navigate,
-              categories,
-              accounts,
-              balanceTypeOp,
-              filters,
-              showHiddenCategories,
-              showOffBudget,
-              type: 'totals',
-              startDate: data.startDate || '',
-              endDate: data.endDate,
-            })
-          }
-          width="flex"
-          privacyFilter
-        />
-        <Cell
-          style={{
-            minWidth: compact ? 50 : 85,
-          }}
-          valueStyle={compactStyle}
-          value={integerToCurrency(Math.round(average))}
-          title={
-            Math.abs(Math.round(average / 100)) > 100000
-              ? integerToCurrency(Math.round(average))
-              : undefined
-          }
-          width="flex"
-          privacyFilter
-        />
-      </View>
-    </Row>
+    <RenderTotalsRow
+      metadata={metadata}
+      mode={mode}
+      totalsStyle={totalsStyle}
+      testStyle={testStyle}
+      scrollWidthTotals={scrollWidthTotals}
+      renderTotals={renderTotals}
+    />
   );
 }
