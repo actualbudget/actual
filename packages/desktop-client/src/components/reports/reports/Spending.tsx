9450758a342ba00a03b8010eb98bdb32467450bc
Crazypkr1099 <nicholas.lacasse430@gmail.com>
2024-06-06 23:42:52 -0400

    removed useEffect

--- packages/desktop-client/src/components/reports/reports/Spending.tsx
+++ packages/desktop-client/src/components/reports/reports/Spending.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useMemo, useEffect } from 'react';
+import React, { useState, useMemo } from 'react';
 
 import * as monthUtils from 'loot-core/src/shared/months';
 import { amountToCurrency } from 'loot-core/src/shared/util';
