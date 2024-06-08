4c5759611714c7db276178bf9be3db26196702e3
Crazypkr <Nicholas.lacasse430@gmail.com>
2024-06-08 12:04:26 -0400

    linting fix

--- packages/desktop-client/src/components/budget/BudgetTable.jsx
+++ packages/desktop-client/src/components/budget/BudgetTable.jsx
@@ -1,4 +1,4 @@
-import React, { useRef, useState, useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 
 import { useCategories } from '../../hooks/useCategories';
 import { useLocalPref } from '../../hooks/useLocalPref';
