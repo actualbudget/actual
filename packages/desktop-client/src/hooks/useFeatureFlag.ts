8ff87d75cecfff28049c9877659426ec65d0d81c
Leandro Menezes <lelemm@gmail.com>
2024-06-17 08:02:13 -0300

    Iterable topological sort enabled by default

--- packages/desktop-client/src/hooks/useFeatureFlag.ts
+++ packages/desktop-client/src/hooks/useFeatureFlag.ts
@@ -9,7 +9,7 @@ const DEFAULT_FEATURE_FLAG_STATE: Record<FeatureFlag, boolean> = {
   customReports: false,
   spendingReport: false,
   simpleFinSync: false,
-  iterableTopologicalSort: false,
+  iterableTopologicalSort: true,
 };
 
 export function useFeatureFlag(name: FeatureFlag): boolean {
