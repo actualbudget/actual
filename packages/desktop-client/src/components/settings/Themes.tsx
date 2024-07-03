a67c969189c2b8f60f25c01988d55883b4494890
Joel Jeremy Marquez <joeljeremy.marquez@gmail.com>
2024-07-03 13:04:26 -0700

    React Aria Button on Settings and Rules Page (#2913)
    * React Aria Button on rules and settings page
    
    * Release notes
    
    * VRT

--- packages/desktop-client/src/components/settings/Themes.tsx
+++ packages/desktop-client/src/components/settings/Themes.tsx
@@ -2,8 +2,7 @@ import React from 'react';
 
 import { type Theme } from 'loot-core/types/prefs';
 
-import { themeOptions, useTheme } from '../../style';
-import { Button } from '../common/Button';
+import { themeOptions, useTheme, theme as themeStyle } from '../../style';
 import { Select } from '../common/Select';
 import { Text } from '../common/Text';
 
@@ -15,14 +14,18 @@ export function ThemeSettings() {
   return (
     <Setting
       primaryAction={
-        <Button bounce={false} style={{ padding: 0 }}>
-          <Select<Theme>
-            bare
-            onChange={switchTheme}
-            value={theme}
-            options={themeOptions}
-          />
-        </Button>
+        <Select<Theme>
+          onChange={value => {
+            switchTheme(value);
+          }}
+          value={theme}
+          options={themeOptions}
+          buttonStyle={{
+            ':hover': {
+              backgroundColor: themeStyle.buttonNormalBackgroundHover,
+            },
+          }}
+        />
       }
     >
       <Text>
