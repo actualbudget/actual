0cd55cd34a1ff8469718841726578c6c6cafa7a1
Mike Clark <mclarkgb@gmail.com>
2024-07-07 12:28:39 +0100

    bundle added to module types

--- packages/desktop-electron/modules.d.ts
+++ packages/desktop-electron/modules.d.ts
@@ -1,5 +1,7 @@
-declare module 'loot-core/lib-dist/bundle.desktop.js';
-
 declare module 'module' {
   const globalPaths: string[];
 }
+
+// bundles not available until we build them
+declare module 'loot-core/lib-dist/bundle.desktop.js';
+declare module './app/bundle.api.js';
