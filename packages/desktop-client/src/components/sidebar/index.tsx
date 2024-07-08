adacd94d2cc26ce4052b7409f987a8bb090848b1
Yusef Ouda <YusefOuda@users.noreply.github.com>
2024-07-08 05:33:24 -0500

    Update index.tsx

--- packages/desktop-client/src/components/sidebar/index.tsx
+++ packages/desktop-client/src/components/sidebar/index.tsx
@@ -24,7 +24,7 @@ export function FloatableSidebar() {
       onMouseOver={
         sidebarShouldFloat
           ? e => {
-              debouncedHideSidebar.cancel();
+              debouncedHideSidebar.clear();
               e.stopPropagation();
               sidebar.setHidden(false);
             }
