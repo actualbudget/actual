a41d7f991a0358530fa1529d1d5e08dc651db094
Yusef Ouda <YusefOuda@users.noreply.github.com>
2024-07-08 05:56:35 -0500

    Update index.tsx

--- packages/desktop-client/src/components/sidebar/index.tsx
+++ packages/desktop-client/src/components/sidebar/index.tsx
@@ -17,8 +17,8 @@ export function FloatableSidebar() {
 
   const sidebarShouldFloat = floatingSidebar || sidebar.alwaysFloats;
   const debouncedHideSidebar = useDebounceCallback(
-  () => sidebar.setHidden(true),
-    350
+    () => sidebar.setHidden(true),
+    350,
   );
 
   return isNarrowWidth ? null : (
