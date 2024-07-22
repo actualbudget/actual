2e53b33199a7275fa5f64b2313aa948760d16e39
matt <matt@fiddaman.net>
2024-07-22 09:46:39 +0000

    suggestions from joel-jeremy

--- packages/desktop-client/src/components/budget/rollover/BalanceMovementMenu.tsx
+++ packages/desktop-client/src/components/budget/rollover/BalanceMovementMenu.tsx
@@ -58,7 +58,7 @@ export function BalanceMovementMenu({
 
       {menu === 'cover' && (
         <CoverMenu
-          targetCategory={categoryId}
+          category={categoryId}
           onClose={onClose}
           onSubmit={fromCategoryId => {
             onBudgetAction(month, 'cover-overspending', {
