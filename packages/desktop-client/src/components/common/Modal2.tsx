b2cf3e80aec768144df5f364b5592a1fb8fd6ed4
Joel Jeremy Marquez <joeljeremy.marquez@gmail.com>
2024-07-27 09:01:40 -0700

    Add Modal2 backdrop

--- packages/desktop-client/src/components/common/Modal2.tsx
+++ packages/desktop-client/src/components/common/Modal2.tsx
@@ -80,6 +80,7 @@ export const Modal = ({
         alignItems: 'center',
         justifyContent: 'center',
         fontSize: 14,
+        backdropFilter: 'blur(0.5px) brightness(0.9)',
         ...style,
       }}
       {...props}
