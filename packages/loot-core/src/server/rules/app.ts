e42f25aa0d376196d2c6c59fdfa58c5fd40e5c59
Robert Dyer <rdyer@unl.edu>
2024-08-07 09:48:49 -0500

    fix typecheck error

--- packages/loot-core/src/server/rules/app.ts
+++ packages/loot-core/src/server/rules/app.ts
@@ -56,13 +56,15 @@ function validateRule(rule: Partial<RuleEntity>) {
         )
       : action.op === 'link-schedule'
         ? new Action(action.op, null, action.value, null, ruleFieldTypes)
-        : new Action(
-            action.op,
-            action.field,
-            action.value,
-            action.options,
-            ruleFieldTypes,
-          ),
+        : action.op === 'prepend-notes' || action.op === 'append-notes'
+          ? new Action(action.op, null, action.value, null, ruleFieldTypes)
+          : new Action(
+              action.op,
+              action.field,
+              action.value,
+              action.options,
+              ruleFieldTypes,
+            ),
   );
 
   if (conditionErrors || actionErrors) {
