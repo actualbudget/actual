055f1cf3b8ee9c334948760c61c566f5a6eed6f6
Nathan Swaim <nathanswaim1999@gmail.comm>
2024-08-13 14:35:01 -0400

    Remove NewTransactionEntity object

--- packages/loot-core/src/server/transactions/types/handlers.ts
+++ packages/loot-core/src/server/transactions/types/handlers.ts
@@ -1,29 +1,28 @@
 // @ts-strict-ignore
 import { QueryState } from '../../../shared/query';
 import {
-  type TransactionEntity,
-  NewTransactionEntity,
+  type TransactionEntity
 } from '../../../types/models';
 import { ParseFileResult } from '../../accounts/parse-file';
 
 export interface TransactionsHandler {
   'transactions-batch-update': (arg: {
-    added?: Partial<TransactionEntity | NewTransactionEntity>[];
-    deleted?: Partial<TransactionEntity | NewTransactionEntity>[];
-    updated?: Partial<TransactionEntity | NewTransactionEntity>[];
+    added?: Partial<TransactionEntity>[];
+    deleted?: Partial<TransactionEntity>[];
+    updated?: Partial<TransactionEntity>[];
     learnCategories?: boolean;
   }) => Promise<object>;
 
   'transaction-add': (
-    transaction: Partial<TransactionEntity | NewTransactionEntity>,
+    transaction: Partial<TransactionEntity>,
   ) => Promise<object>;
 
   'transaction-update': (
-    transaction: Partial<TransactionEntity | NewTransactionEntity>,
+    transaction: Partial<TransactionEntity>,
   ) => Promise<object>;
 
   'transaction-delete': (
-    transaction: Partial<TransactionEntity | NewTransactionEntity>,
+    transaction: Partial<TransactionEntity>,
   ) => Promise<object>;
 
   'transactions-parse-file': (arg: {
@@ -32,7 +31,7 @@ export interface TransactionsHandler {
   }) => Promise<ParseFileResult>;
 
   'transactions-export': (arg: {
-    transactions: Partial<TransactionEntity | NewTransactionEntity>[];
+    transactions: Partial<TransactionEntity>[];
     accounts: unknown;
     categoryGroups: unknown;
     payees: unknown;
@@ -42,7 +41,7 @@ export interface TransactionsHandler {
 
   'transactions-import': (arg: {
     accountId: string;
-    transactions: Partial<TransactionEntity | NewTransactionEntity>[];
+    transactions: Partial<TransactionEntity>[];
     isPreview: boolean;
   }) => Promise<object>;
 }
