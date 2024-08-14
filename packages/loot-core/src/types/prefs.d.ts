de78aeb6d6481f8f9fa91f4206034fbea788586b
Matiss Janis Aboltins <matiss@mja.lv>
2024-08-14 18:15:06 +0100

    :recycle: cleanup iterableTopologicalSort feature flag

--- packages/loot-core/src/types/prefs.d.ts
+++ packages/loot-core/src/types/prefs.d.ts
@@ -6,8 +6,7 @@ export type FeatureFlag =
   | 'reportBudget'
   | 'goalTemplatesEnabled'
   | 'spendingReport'
-  | 'simpleFinSync'
-  | 'iterableTopologicalSort';
+  | 'simpleFinSync';
 
 /**
  * Cross-device preferences. These sync across devices when they are changed.
