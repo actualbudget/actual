bec104d96aaaf1802d72deee2ae9f8ead06afa6a
Robert Dyer <rdyer@unl.edu>
2024-08-15 10:59:23 -0500

    fix lint

--- packages/desktop-electron/menu.ts
+++ packages/desktop-electron/menu.ts
@@ -1,3 +1,5 @@
+import { useTranslation } from 'react-i18next';
+
 import {
   MenuItemConstructorOptions,
   Menu,
@@ -5,7 +7,6 @@ import {
   app,
   shell,
 } from 'electron';
-import { useTranslation } from 'react-i18next';
 
 export function getMenu(
   isDev: boolean,
