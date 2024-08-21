296ad069c71dae5f54916f12fb3667a1bbaa563b
Leandro Menezes <lelemm@gmail.com>
2024-08-21 20:31:49 -0300

    closest for testing in case it doesn't have

--- packages/desktop-client/src/setupTests.js
+++ packages/desktop-client/src/setupTests.js
@@ -4,6 +4,17 @@ import { installPolyfills } from './polyfills';
 
 installPolyfills();
 
+if (!Element.prototype.closest) {
+  Element.prototype.closest = function (selector) {
+    let el = this;
+    do {
+      if (el.matches(selector)) return el;
+      el = el.parentElement || el.parentNode;
+    } while (el !== null && el.nodeType === 1);
+    return null;
+  };
+}
+
 global.IS_TESTING = true;
 global.Actual = {};
 
