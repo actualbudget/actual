8d569b369e32511958031c8cf6028e914625819f
Mike Clark <mclarkgb@gmail.com>
2024-08-23 21:25:04 +0100

    sake...

--- packages/loot-core/src/platform/server/fetch/index.electron.ts
+++ packages/loot-core/src/platform/server/fetch/index.electron.ts
@@ -1,16 +1,28 @@
 // // @ts-strict-ignore
+import fs from 'fs';
 import https from 'https';
+import tls from 'tls';
 
 import nodeFetch from 'node-fetch';
 
 export const fetch = (input: RequestInfo | URL, options?: RequestInit) => {
-  const agent = new https.Agent({ rejectUnauthorized: false }); // Insecure - leaves vulnerable to MITM attacks
+  const cas = [...tls.rootCertificates];
+  // const cert = fs.readFileSync(`${process.env.ACTUAL_DATA_DIR}/localhost.pem`);
+  // const key = fs.readFileSync(
+  //   `${process.env.ACTUAL_DATA_DIR}/localhost-key.pem`,
+  // );
+  cas.push(fs.readFileSync(`${process.env.ACTUAL_DATA_DIR}/cert.pem`, 'ascii'));
+  // https.globalAgent.options.cert = cert;
+  https.globalAgent.options.ca = cas; // this should work but doesn't.....
+  // https.globalAgent.options.key = key;
+  // https.globalAgent.options.ca = cas;
+  // https.globalAgent.options.rejectUnauthorized = false; // Insecure - leaves vulnerable to MITM attacks
+
   return nodeFetch(input, {
     ...options,
     headers: {
       ...options?.headers,
       origin: 'app://actual',
     },
-    agent,
   });
 };
