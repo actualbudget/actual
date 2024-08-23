4c8611c804c494ace143f7643dc3e0394b74f146
Mike Clark <mclarkgb@gmail.com>
2024-08-23 19:40:22 +0100

    solves the problem but creates a vulnerability

--- packages/loot-core/src/platform/server/fetch/index.electron.ts
+++ packages/loot-core/src/platform/server/fetch/index.electron.ts
@@ -1,12 +1,16 @@
 // // @ts-strict-ignore
+import https from 'https';
+
 import nodeFetch from 'node-fetch';
 
 export const fetch = (input: RequestInfo | URL, options?: RequestInit) => {
+  const agent = new https.Agent({ rejectUnauthorized: false }); // Insecure - leaves vulnerable to MITM attacks
   return nodeFetch(input, {
     ...options,
     headers: {
       ...options?.headers,
       origin: 'app://actual',
     },
+    agent,
   });
 };
