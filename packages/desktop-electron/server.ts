7bfbc68fdeb1a87cd58286052dbad32600063459
Mike Clark <mclarkgb@gmail.com>
2024-08-24 19:47:02 +0100

    being flexible on the cert names, as long as its a crt or pem

--- packages/desktop-electron/server.ts
+++ packages/desktop-electron/server.ts
@@ -1,22 +1,26 @@
-// @ts-strict-ignore
-
 import fs from 'fs';
 import https from 'https';
 import Module from 'module';
 import tls from 'tls';
 
+// @ts-strict-ignore
 import fetch from 'node-fetch';
 
 const supportSelfSignedCertificates = () => {
-  // If we have specified a custom authority, we do this code
-  // Read it from settings?
-  if (true) {
-    const cas = [...tls.rootCertificates];
-    https.globalAgent.options.ca = cas;
-    // https.globalAgent.options.ca.push(fs.readFileSync(`path/to/rootCA.pem`));
-    https.globalAgent.options.ca.push(
-      fs.readFileSync(`D:/GitWork/actual-server/selfhost.crt`),
+  try {
+    // Scan for self signed certificates in the data directory - we support .crt and .pem
+    const certFileNames = fs
+      .readdirSync(process.env.ACTUAL_DATA_DIR)
+      .filter(fn => fn.endsWith('.crt') || fn.endsWith('.pem'));
+
+    const certFiles = certFileNames.map(filename =>
+      fs.readFileSync(`${process.env.ACTUAL_DATA_DIR}/${filename}`),
     );
+
+    https.globalAgent.options.ca = [...tls.rootCertificates, ...certFiles];
+    console.info('Added the self signed certificate');
+  } catch (error) {
+    console.error('Unable to add the self signed certificate', error);
   }
 };
 
