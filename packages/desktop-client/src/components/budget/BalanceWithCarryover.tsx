5b6df1784c25733cd76a903b32b9284ca8fa9c46
NickFR <20896232+nmathey@users.noreply.github.com>
2024-09-03 14:35:55 +1100

    Lint

--- packages/desktop-client/src/components/budget/BalanceWithCarryover.tsx
+++ packages/desktop-client/src/components/budget/BalanceWithCarryover.tsx
@@ -139,14 +139,14 @@ export function BalanceWithCarryover({
                   </span>
                 ) : differenceToGoal > 0 ? (
                   <span style={{ color: theme.noticeText }}>
-                    {t('Overfunded ({{amount}})', { 
-                      amount: format(differenceToGoal, 'financial')
+                    {t('Overfunded ({{amount}})', {
+                      amount: format(differenceToGoal, 'financial'),
                     })}
                   </span>
                 ) : (
                   <span style={{ color: theme.errorText }}>
                     {t('Underfunded ({{amount}})', {
-                      amount: format(differenceToGoal, 'financial')
+                      amount: format(differenceToGoal, 'financial'),
                     })}
                   </span>
                 )}
