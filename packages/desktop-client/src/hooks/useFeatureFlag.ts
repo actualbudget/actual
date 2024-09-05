c81841f694f106b4ea3b85f38d261944811b31dc
Leandro Menezes <lelemm@gmail.com>
2024-09-05 17:24:10 -0300

    typecheck

--- packages/desktop-client/src/hooks/useFeatureFlag.ts
+++ packages/desktop-client/src/hooks/useFeatureFlag.ts
@@ -9,6 +9,7 @@ const DEFAULT_FEATURE_FLAG_STATE: Record<FeatureFlag, boolean> = {
   spendingReport: false,
   simpleFinSync: false,
   dashboards: false,
+  openidAuth: false,
 };
 
 export function useFeatureFlag(name: FeatureFlag): boolean {
