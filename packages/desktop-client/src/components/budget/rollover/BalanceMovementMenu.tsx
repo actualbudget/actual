15f0b36c8d320d5caa6961d4a556da88d795fddf
UnderKoen <koenvanstaveren@hotmail.com>
2024-09-09 18:49:19 +0200

    chore: lint

--- packages/desktop-client/src/components/budget/rollover/BalanceMovementMenu.tsx
+++ packages/desktop-client/src/components/budget/rollover/BalanceMovementMenu.tsx
@@ -1,10 +1,4 @@
-import React, {
-  useCallback,
-  useEffect,
-  useMemo,
-  useRef,
-  useState,
-} from 'react';
+import React, { useCallback, useMemo, useRef, useState } from 'react';
 
 import { runQuery } from 'loot-core/client/query-helpers';
 import { send } from 'loot-core/platform/client/fetch';
