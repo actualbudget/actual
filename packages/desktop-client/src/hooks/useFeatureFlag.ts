8498d7f78816edea6f48eec25b64232cdf4b41ee
Matiss Janis Aboltins <matiss@mja.lv>
2024-09-09 08:04:41 +0100

    :recycle: (synced-prefs) refactor number formatter away from redux (#3397)

--- packages/desktop-client/src/hooks/useFeatureFlag.ts
+++ packages/desktop-client/src/hooks/useFeatureFlag.ts
@@ -1,7 +1,6 @@
-import { useSelector } from 'react-redux';
-
-import { type State } from 'loot-core/src/client/state-types';
 import type { FeatureFlag } from 'loot-core/src/types/prefs';
+
+import { useSyncedPref } from './useSyncedPref';
 
 const DEFAULT_FEATURE_FLAG_STATE: Record<FeatureFlag, boolean> = {
   reportBudget: false,
@@ -12,11 +11,9 @@ const DEFAULT_FEATURE_FLAG_STATE: Record<FeatureFlag, boolean> = {
 };
 
 export function useFeatureFlag(name: FeatureFlag): boolean {
-  return useSelector((state: State) => {
-    const value = state.prefs.local[`flags.${name}`];
+  const [value] = useSyncedPref(`flags.${name}`);
 
-    return value === undefined
-      ? DEFAULT_FEATURE_FLAG_STATE[name] || false
-      : String(value) === 'true';
-  });
+  return value === undefined
+    ? DEFAULT_FEATURE_FLAG_STATE[name] || false
+    : String(value) === 'true';
 }
