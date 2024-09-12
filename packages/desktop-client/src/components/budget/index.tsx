578a1e51d58f156027c44b47d79e431375cdf6bf
AGradina <agradina0@gmail.com>
2024-09-12 20:51:34 +0200

    added punctuations

--- packages/desktop-client/src/components/budget/index.tsx
+++ packages/desktop-client/src/components/budget/index.tsx
@@ -176,7 +176,7 @@ function BudgetInner(props: BudgetInnerProps) {
       addNotification({
         type: 'error',
         message: t(
-          'Category {{name}} already exists in group (May be Hidden)',
+          'Category ‘{{name}}‘ already exists in group (May be Hidden)',
           { name },
         ),
       }),
