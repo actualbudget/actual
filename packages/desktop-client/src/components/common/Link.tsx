e507b8ff436e344c4377d3f285127d23d61ecead
Joel Jeremy Marquez <joeljeremy.marquez@gmail.com>
2024-09-19 16:12:57 -0700

    Fix React Aria Button hover styles  (#3453)
    * Fiox hover styles and use className instead of inline to prepare for future css migration
    
    * Release notes
    
    * Cleanup
    
    * Update edit rule hover style

--- packages/desktop-client/src/components/common/Link.tsx
+++ packages/desktop-client/src/components/common/Link.tsx
@@ -95,10 +95,13 @@ const ButtonLink = ({ to, style, activeStyle, ...props }: ButtonLinkProps) => {
   const match = useMatch({ path });
   return (
     <Button
-      style={({ isPressed }) => ({
-        ...style,
-        ...(match || isPressed ? activeStyle : {}),
-      })}
+      className={String(
+        css({
+          ...style,
+          '&[data-pressed]': activeStyle,
+          ...(match ? activeStyle : {}),
+        }),
+      )}
       {...props}
       variant={props.buttonVariant}
       onPress={e => {
