a8b3fdaf77624f878f758e3c55115d16af4c17b1
qedi-r <1435081+qedi-r@users.noreply.github.com>
2024-09-22 11:53:09 -0400

    revert to using ts-ignore-error

--- packages/loot-core/src/platform/server/sqlite/index.web.test.ts
+++ packages/loot-core/src/platform/server/sqlite/index.web.test.ts
@@ -19,16 +19,6 @@ CREATE TABLE numbers (id TEXT PRIMARY KEY, number INTEGER);
 CREATE TABLE textstrings (id TEXT PRIMARY KEY, string TEXT);
 `;
 
-interface NumberRowType {
-  id: string;
-  number: number;
-}
-
-interface StringRowType {
-  id: string;
-  string: string;
-}
-
 describe('Web sqlite', () => {
   it('should rollback transactions', async () => {
     const db = await openDatabase();
@@ -38,7 +28,8 @@ describe('Web sqlite', () => {
 
     let rows = runQuery(db, 'SELECT * FROM numbers', null, true);
     expect(rows.length).toBe(1);
-    expect((rows[0] as NumberRowType).number).toBe(4);
+    //@ts-expect-error: Property 'number' does not exist on type 'unknown'
+    expect(rows[0].number).toBe(4);
 
     const consoleSpy = jest.spyOn(console, 'log').mockImplementation();
     expect(() => {
@@ -54,7 +45,8 @@ describe('Web sqlite', () => {
     // Nothing should have changed in the db
     rows = runQuery(db, 'SELECT * FROM numbers', null, true);
     expect(rows.length).toBe(1);
-    expect((rows[0] as NumberRowType).number).toBe(4);
+    //@ts-expect-error: Property 'number' does not exist on type 'unknown'
+    expect(rows[0].number).toBe(4);
   });
 
   it('should support nested transactions', async () => {
@@ -65,7 +57,8 @@ describe('Web sqlite', () => {
 
     let rows = runQuery(db, 'SELECT * FROM numbers', null, true);
     expect(rows.length).toBe(1);
-    expect((rows[0] as NumberRowType).number).toBe(4);
+    //@ts-expect-error: Property 'number' does not exist on type 'unknown'
+    expect(rows[0].number).toBe(4);
 
     transaction(db, () => {
       runQuery(db, "INSERT INTO numbers (id, number) VALUES ('id2', 5)");
@@ -86,9 +79,12 @@ describe('Web sqlite', () => {
     // Nothing should have changed in the db
     rows = runQuery(db, 'SELECT * FROM numbers', null, true);
     expect(rows.length).toBe(3);
-    expect((rows[0] as NumberRowType).number).toBe(4);
-    expect((rows[1] as NumberRowType).number).toBe(5);
-    expect((rows[2] as NumberRowType).number).toBe(6);
+    //@ts-expect-error: Property 'number' does not exist on type 'unknown'
+    expect(rows[0].number).toBe(4);
+    //@ts-expect-error: Property 'number' does not exist on type 'unknown'
+    expect(rows[1].number).toBe(5);
+    //@ts-expect-error: Property 'number' does not exist on type 'unknown'
+    expect(rows[2].number).toBe(6);
   });
 
   it('should match regex on text fields', async () => {
@@ -108,6 +104,7 @@ describe('Web sqlite', () => {
       true,
     );
     expect(rows.length).toBe(1);
-    expect((rows[0] as StringRowType).id).toBe('id1');
+    //@ts-expect-error: Property 'id' does not exist on type 'unknown'
+    expect(rows[0].id).toBe('id1');
   });
 });
