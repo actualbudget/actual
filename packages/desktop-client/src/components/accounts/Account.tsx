5cbadc41bb8d8b2a465a43694060d5b8ba56d78b
a-gradina <agradina0@gmail.com>
2024-09-24 18:05:08 +0200

    Revert ":bug: fix account filters being overridden (#3441)"
    This reverts commit 7336badffdb1e99411eaa6b3e7a0046a9c9de68c.

--- packages/desktop-client/src/components/accounts/Account.tsx
+++ packages/desktop-client/src/components/accounts/Account.tsx
@@ -1357,18 +1357,6 @@ class AccountInternal extends PureComponent<
 
   onApplyFilter = async (conditionOrSavedFilter: ConditionEntity) => {
     let filterConditions = this.state.filterConditions;
-
-    if (
-      'customName' in conditionOrSavedFilter &&
-      conditionOrSavedFilter.customName
-    ) {
-      filterConditions = filterConditions.filter(
-        c =>
-          !isTransactionFilterEntity(c) &&
-          c.customName !== conditionOrSavedFilter.customName,
-      );
-    }
-
     if (isTransactionFilterEntity(conditionOrSavedFilter)) {
       // A saved filter was passed in.
       const savedFilter = conditionOrSavedFilter;
@@ -1378,6 +1366,12 @@ class AccountInternal extends PureComponent<
       this.setState({ filterConditionsOp: savedFilter.conditionsOp });
       this.applyFilters([...savedFilter.conditions]);
     } else {
+      filterConditions = filterConditions.filter(
+        c =>
+          !isTransactionFilterEntity(c) &&
+          c.customName !== conditionOrSavedFilter.customName,
+      );
+
       // A condition was passed in.
       const condition = conditionOrSavedFilter;
       this.setState({
