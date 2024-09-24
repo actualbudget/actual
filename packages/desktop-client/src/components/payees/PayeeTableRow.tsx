d91c0182ed97499b2605f900bf23a9b002f3e4f7
a-gradina <agradina0@gmail.com>
2024-09-24 19:53:46 +0200

    added PayeeTableRow.tsx

--- packages/desktop-client/src/components/payees/PayeeTableRow.tsx
+++ packages/desktop-client/src/components/payees/PayeeTableRow.tsx
@@ -1,5 +1,6 @@
 // @ts-strict-ignore
 import { memo } from 'react';
+import { Trans, useTranslation } from 'react-i18next';
 
 import { type PayeeEntity } from 'loot-core/src/types/models';
 
@@ -46,11 +47,11 @@ function RuleButton({ ruleCount, focused, onEdit, onClick }: RuleButtonProps) {
       >
         <Text style={{ paddingRight: 5 }}>
           {ruleCount > 0 ? (
-            <>
+            <Trans>
               {ruleCount} associated {ruleCount === 1 ? 'rule' : 'rules'}
-            </>
+            </Trans>
           ) : (
-            <>Create rule</>
+            <Trans>Create rule</Trans>
           )}
         </Text>
         <SvgArrowThinRight style={{ width: 8, height: 8 }} />
@@ -95,6 +96,8 @@ export const PayeeTableRow = memo(
     onUpdate,
     style,
   }: PayeeTableRowProps) => {
+    const { t } = useTranslation();
+
     const { id } = payee;
     const dispatchSelected = useSelectedDispatch();
     const borderColor = selected
@@ -153,7 +156,7 @@ export const PayeeTableRow = memo(
           }}
         </CustomCell>
         <InputCell
-          value={(payee.transfer_acct ? 'Transfer: ' : '') + payee.name}
+          value={(payee.transfer_acct ? t('Transfer: ') : '') + payee.name}
           valueStyle={
             (!selected &&
               payee.transfer_acct && { color: theme.pageTextSubdued }) ||
