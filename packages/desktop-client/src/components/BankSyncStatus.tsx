aa93d79e54d09e5b1a06fa908a3b35c108cdea5c
Andrew Vagliano <andrew.vagliano1@gmail.com>
2024-10-02 21:59:05 +0000

    Hover 'Syncing' animation over notification button

--- packages/desktop-client/src/components/BankSyncStatus.tsx
+++ packages/desktop-client/src/components/BankSyncStatus.tsx
@@ -35,7 +35,7 @@ export function BankSyncStatus() {
         right: 0,
         marginTop: 5,
         alignItems: 'center',
-        zIndex: 501,
+        zIndex: 1000,
       }}
     >
       {transitions(
