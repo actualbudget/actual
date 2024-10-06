1b2b6a0317ad804c0990aef9f688ec733d269409
Travis Lesicka <TravisLesicka@gmail.com>
2024-10-06 15:03:54 +1000

    Updated code to remove @ts-strict-ignore

--- packages/desktop-client/src/components/sidebar/Accounts.tsx
+++ packages/desktop-client/src/components/sidebar/Accounts.tsx
@@ -1,4 +1,3 @@
-// @ts-strict-ignore
 import React, { useState } from 'react';
 import { useTranslation } from 'react-i18next';
 import { useDispatch, useSelector } from 'react-redux';
@@ -45,7 +44,7 @@ export function Accounts() {
     'ui.showClosedAccounts',
   );
 
-  function onDragChange(drag) {
+  function onDragChange(drag: { state: string }) {
     setIsDragging(drag.state === 'start');
   }
 
