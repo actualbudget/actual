aacd3d43ad0ff063ea89f5792565f55ac7a94278
SamBobBarnes <samuelrbarnes@outlook.com>
2024-10-12 18:33:54 -0500

    Revert "added info circle and box on click"
    This reverts commit db16c0a16b8b4e6efe9ab00a0bcf9030b988a5dc.

--- packages/desktop-client/src/components/settings/Upcoming.tsx
+++ packages/desktop-client/src/components/settings/Upcoming.tsx
@@ -8,7 +8,6 @@ import { type SyncedPrefs } from 'loot-core/types/prefs';
 
 import { useFeatureFlag } from '../../hooks/useFeatureFlag';
 import { useSyncedPref } from '../../hooks/useSyncedPref';
-import { SvgInformationCircle } from '../../icons/v2';
 import { type CSSProperties, theme } from '../../style';
 import { Button } from '../common/Button2';
 import { Select } from '../common/Select';
@@ -43,7 +42,6 @@ export function UpcomingLengthSettings() {
 
   const location = useLocation();
   const [expanded, setExpanded] = useState(location.hash === '#upcomingLength');
-  const [infoExpanded, setInfoExpanded] = useState(location.hash === '#info');
 
   if (!enabled) return null;
 
@@ -52,17 +50,12 @@ export function UpcomingLengthSettings() {
       primaryAction={
         <View style={{ flexDirection: 'row', gap: '1em' }}>
           <Column title="Upcoming Length">
-            <View
-              style={{ flexDirection: 'row', alignItems: 'center', gap: 20 }}
-            >
-              <Select
-                options={options.map(x => [x.value || '7', x.label])}
-                value={upcomingLength}
-                onChange={newValue => setUpcomingLength(newValue)}
-                style={selectButtonStyle}
-              />
-              <InfoCircle />
-            </View>
+            <Select
+              options={options.map(x => [x.value || '7', x.label])}
+              value={upcomingLength}
+              onChange={newValue => setUpcomingLength(newValue)}
+              style={selectButtonStyle}
+            />
           </Column>
         </View>
       }
@@ -92,48 +85,6 @@ export function UpcomingLengthSettings() {
           {options.find(x => x.value === upcomingLength)?.label ?? '1 Week'})
         </Trans>
       </Button>
-    </View>
-  );
-}
-
-function InfoCircle() {
-  const location = useLocation();
-  const [visible, setVisible] = useState(location.hash === '#info');
-
-  return visible ? (
-    <View style={{ userSelect: 'none' }}>
-      <SvgInformationCircle
-        style={{ height: '15px', cursor: 'pointer' }}
-        onClick={() => setVisible(false)}
-      />
-      <View
-        style={{
-          position: 'absolute',
-          left: '20px',
-          top: '-20px',
-          color: 'white',
-          backgroundColor: 'black',
-          borderRadius: '5px',
-          borderStyle: 'solid',
-          borderWidth: '1px',
-          borderColor: 'white',
-          padding: '5px',
-          width: '200px',
-          zIndex: 300,
-        }}
-      >
-        <Text>
-          <strong>Only</strong> the first instance of a recurring transaction
-          will be shown.
-        </Text>
-      </View>
-    </View>
-  ) : (
-    <View style={{ userSelect: 'none' }}>
-      <SvgInformationCircle
-        style={{ height: '15px', cursor: 'pointer' }}
-        onClick={() => setVisible(true)}
-      />
     </View>
   );
 }
