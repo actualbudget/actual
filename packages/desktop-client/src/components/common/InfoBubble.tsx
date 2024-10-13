9713b6b10efe6e8f637d667a6a80f84a8fcb9acf
SamBobBarnes <samuelrbarnes@outlook.com>
2024-10-12 20:48:58 -0500

    text longer than 500px will be split

--- packages/desktop-client/src/components/common/InfoBubble.tsx
+++ packages/desktop-client/src/components/common/InfoBubble.tsx
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import { useLocation } from 'react-router-dom';
 
 import { SvgInformationCircle } from '../../icons/v2';
+import { theme } from '../../style';
 
 import { Text } from './Text';
 import { View } from './View';
@@ -10,9 +11,37 @@ type InfoBubbleProps = {
   label: string;
 };
 
+function getBubbleWidth(label: string): number {
+  const font = '13px Inter var';
+  const textWidth = getTextWidth(label, font);
+  console.log(textWidth);
+  // if textWidth longer than 500, split into multiple lines of close to equal length
+  if (textWidth > 500) {
+    const words = label.split(' ');
+    const half = Math.ceil(words.length / 2);
+    const firstHalf = words.slice(0, half).join(' ');
+    const secondHalf = words.slice(half).join(' ');
+    return Math.ceil(
+      Math.max(getTextWidth(firstHalf, font), getTextWidth(secondHalf, font)),
+    );
+  }
+  return textWidth;
+}
+
+function getTextWidth(text: string, font: string): number {
+  const canvas = document.createElement('canvas');
+  const context = canvas.getContext('2d')!;
+  context.font = font || getComputedStyle(document.body).font;
+
+  return Math.ceil(context.measureText(text).width);
+}
+
 export function InfoBubble({ label }: InfoBubbleProps) {
   const location = useLocation();
   const [visible, setVisible] = useState(location.hash === '#info');
+
+  const bubbleWidth = getBubbleWidth(label);
+  console.log(bubbleWidth);
 
   return visible ? (
     <View style={{ userSelect: 'none' }}>
@@ -25,19 +54,23 @@ export function InfoBubble({ label }: InfoBubbleProps) {
         style={{
           position: 'absolute',
           left: '20px',
-          top: '-20px',
-          color: 'white',
-          backgroundColor: 'black',
-          borderRadius: '5px',
-          borderStyle: 'solid',
-          borderWidth: '1px',
-          borderColor: 'white',
-          padding: '5px',
-          width: '200px',
+          top: '-7px',
+          color: theme.buttonNormalText,
+          backgroundColor: theme.buttonNormalBackground,
+          padding: 5,
+          borderRadius: 4,
+          border: '1px solid ' + theme.buttonNormalBorder,
           zIndex: 300,
+          boxShadow: theme.buttonNormalShadow,
         }}
       >
-        <Text>{label}</Text>
+        <Text
+          style={{
+            width: bubbleWidth,
+          }}
+        >
+          {label}
+        </Text>
       </View>
     </View>
   ) : (
