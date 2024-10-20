45bf31df6f4a19a7de3057a7f91fd37c8549c167
deathmasia <halo2ofdeath@gmail.com>
2024-10-20 15:48:19 -0400

    Added Translation, change text formating to table (styling wip), change headers to use react-aria-component headings

--- packages/desktop-client/src/components/modals/GoalTemplateModal.tsx
+++ packages/desktop-client/src/components/modals/GoalTemplateModal.tsx
@@ -1,16 +1,19 @@
+import { useTranslation } from 'react-i18next';
 import { theme } from '../../style';
 import { Link } from '../common/Link';
 import { Modal, ModalCloseButton, ModalHeader } from '../common/Modal';
 import { Text } from '../common/Text';
 import { View } from '../common/View';
+import { Heading } from 'react-aria-components';
 
 export function GoalTemplateModal() {
+  const { t } = useTranslation();
   return (
     <Modal name="goal-templates">
       {({ state: { close } }) => (
         <>
           <ModalHeader
-            title="Goal Templates"
+            title={t('Goal Templates')}
             rightContent={<ModalCloseButton onPress={close} />}
           />
           <View
@@ -20,49 +23,134 @@ export function GoalTemplateModal() {
             }}
           >
             <Text style={{ textAlign: 'left' }}>
-              <h3>Weekly:</h3>
-              <strong>
-                #template $10 repeat every week starting 2025-01-03
-              </strong>{' '}
-              - Budget $10 a week <br />
-              {'\n'}
-              <strong>
-                #template $10 repeat every week starting 2025-01-03 up to 80
-              </strong>{' '}
-              - Budget $10 a week, up to a maximum of $80
-              <h3>Monthly:</h3>
-              <strong>#template $50</strong> - Budget $50 each month <br />
-              {'\n'}
-              <strong>#template up to $150</strong> - Budget up to $150 each
-              month, and remove extra funds over $150 <br /> {'\n'}
-              <strong>#template up to $150 hold</strong> - Budget up to $150
-              each month, but retain any funds over $150 <br />
-              {'\n'}
-              <h3>Longer Term:</h3>
-              <strong>#template $500 by 2025-03 repeat every 6 months</strong> -
-              Break down large, less-frequent expenses into manageable monthly
-              expenses <br />
-              {'\n'}
-              <strong>#template $500 by 2025-03 repeat every year</strong> -
-              Break down large, less-frequent expenses into manageable monthly
-              expenses <br />
-              {'\n'}
-              <strong>#template $500 by 2025-03 repeat every 2 years</strong> -
-              Break down large, less-frequent expenses into manageable monthly
-              expenses
-              <h3>Schedules:</h3>
-              <strong>#template schedule SCHEDULE_NAME</strong> - Fund upcoming
-              scheduled transactions over time <br />
-              {'\n'}
-              <strong>#template schedule full SCHEDULE_NAME</strong> - Fund
-              upcoming scheduled transaction only on needed month
-              <h3>Goals:</h3>
-              <strong>#goal 1000</strong> - Set a long term goal instead of a
-              monthly goal <br /> {'\n'}
+            <Heading level={3}>{t('Weekly')}</Heading>
+            
+              <table>
+                <tr>
+                  <th>{t('Syntax')}</th>
+                  <th>{t('Description')}</th>
+                </tr>
+                <tr>
+                  <td>
+                    <strong>
+                      {t('#template $10 repeat every week starting 2025-01-03')}
+                    </strong>{' '}
+                  </td>
+                  <td>{t('$10 a week')}</td>
+                </tr>
+                <tr>
+                  <td>
+                    <strong>
+                      {t('#template $10 repeat every week starting 2025-01-03 up to $80')}
+                    </strong>
+                  </td>
+                  <td>{t('$10 a week, up to a maximum of $80')}</td>
+                </tr>
+              </table>
+
+              <Heading level={3}>{t('Monthly')}</Heading>
+              <table>
+                <tr>
+                <th>{t('Syntax')}</th>
+                <th>{t('Description')}</th>
+                </tr>
+                <tr>
+                  <td>
+                    <strong>{t('#template $50')}</strong>
+                  </td>
+                  <td>{t('$50 each month')}</td>
+                </tr>
+                <tr>
+                  <td>
+                    <strong>{t('#template up to $150')}</strong>
+                  </td>
+                  <td>
+                    {t('Up to $150 each month, and remove extra funds over $150')}
+                  </td>
+                </tr>
+                <tr>
+                  <td>
+                    <strong>{t('#template up to $150 hold')}</strong>
+                  </td>
+                  <td>{t('Up to $150 each month, but retain any funds over $150')}</td>
+                </tr>
+              </table>
+
+              <Heading level={3}>{t('Longer Term')}</Heading>
+              <table>
+                <tr>
+                  <th>{t('Syntax')}</th>
+                  <th>{t('Description')}</th>
+                </tr>
+                <tr>
+                  <td>
+                    <strong>
+                      {t('#template $500 by 2025-03 repeat every 6 months')}
+                    </strong>
+                  </td>
+                  <td>
+                    {t('Break down large, less-frequent expenses into manageable monthly expenses')}
+                  </td>
+                </tr>
+                <tr>
+                  <td>
+                    <strong>{t('#template $500 by 2025-03 repeat every year')}</strong>
+                  </td>
+                  <td>
+                    {t('Break down large, less-frequent expenses into manageable monthly expenses')}
+                  </td>
+                </tr>
+                <tr>
+                  <td>
+                    <strong>
+                      {t('#template $500 by 2025-03 repeat every 2 years')}
+                    </strong>
+                  </td>
+                  <td>
+                    {t('Break down large, less-frequent expenses into manageable monthly expenses')}
+                  </td>
+                </tr>
+              </table>
+
+              <Heading level={3}>{t('Schedules')}</Heading>
+              <table>
+                <tr>
+                  <th>{t('Syntax')}</th>
+                  <th>{t('Description')}</th>
+                </tr>
+                <tr>
+                  <td>
+                    <strong>{t('#template schedule SCHEDULE_NAME')}</strong>
+                  </td>
+                  <td>{t('Fund upcoming scheduled transactions over time')}</td>
+                </tr>
+                <tr>
+                  <td>
+                    <strong>{t('#template schedule full SCHEDULE_NAME')}</strong>
+                  </td>
+                  <td>
+                    {t('Fund upcoming scheduled transaction only on needed month')}
+                  </td>
+                </tr>
+              </table>
+
+              <Heading level={3}>{t('Goals')}</Heading>
+              <table>
+                <tr>
+                  <th>{t('Syntax')}</th>
+                  <th>{t('Description')}</th>
+                </tr>
+                <tr>
+                  <td>
+                    <strong>{t('#goal 1000')}</strong>
+                  </td>
+                  <td>{t('Set a long-term goal instead of a monthly goal')}</td>
+                </tr>
+              </table>
               <div
                 style={{
                   textAlign: 'right',
-                  fontSize: '0.7em',
+                  fontSize: '1em',
                   color: theme.pageTextLight,
                   marginTop: 3,
                 }}
