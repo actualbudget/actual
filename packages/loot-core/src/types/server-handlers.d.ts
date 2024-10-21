8c5bd2bf1bb7c1fa5ad363ed3f60612aaf1e1b0a
dreptschar <dreptschar@gmail.com>
2024-10-21 19:19:39 +0200

    add notification and clean up

--- packages/loot-core/src/types/server-handlers.d.ts
+++ packages/loot-core/src/types/server-handlers.d.ts
@@ -1,5 +1,4 @@
 import type { Notification } from '../client/state-types/notifications';
-
 import { ParseFileResult } from '../server/accounts/parse-file';
 import { batchUpdateTransactions } from '../server/accounts/transactions';
 import { Backup } from '../server/backups';
