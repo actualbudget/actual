d8ce3068226261f39e3bccd829859c6e132a1d5c
Ryan Bianchi <1435081+qedi-r@users.noreply.github.com>
2024-10-21 19:20:35 -0400

    adding tests

--- packages/loot-core/src/server/aql/compiler.test.ts
+++ packages/loot-core/src/server/aql/compiler.test.ts
@@ -951,4 +951,13 @@ describe('Type conversions', () => {
     );
     expect(result.sql).toMatch('WHERE (transactions.payee IS NULL)');
   });
+
+  it('allows fields to be not nullable', () => {
+    // With validated refs
+    let result = generateSQLWithState(
+      q('transactions').filter({ payee: { '$ne': null }}).select().serialize(),
+      schemaWithRefs,
+    );
+    expect(result.sql).toMatch('WHERE (payees1.id IS NOT NULL)');
+  });
 });
