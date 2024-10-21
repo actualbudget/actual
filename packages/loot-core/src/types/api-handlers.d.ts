2b620e7598ab604b80548e384b02bb9734f0538e
dreptschar <dreptschar@gmail.com>
2024-10-21 19:19:39 +0200

    add notification and clean up

--- packages/loot-core/src/types/api-handlers.d.ts
+++ packages/loot-core/src/types/api-handlers.d.ts
@@ -7,6 +7,7 @@ import type {
   APIFileEntity,
   APIPayeeEntity,
 } from '../server/api-models';
+
 import type { NewRuleEntity, RuleEntity, TransactionEntity } from './models';
 import { type ServerHandlers } from './server-handlers';
 
