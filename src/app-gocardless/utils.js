49c5adc9cf5705ff2658d195b0b2e1c70f7742e4
Koen van Staveren <koenvanstaveren@hotmail.com>
2024-10-23 10:53:12 +0200

    feat: sort transactions better (#470)
    * feat: sort transactions better
    
    * chore: release note
    
    * chore: pr feedback

--- src/app-gocardless/utils.js
+++ src/app-gocardless/utils.js
@@ -6,11 +6,40 @@ export const printIban = (account) => {
   }
 };
 
+const compareDates = (
+  /** @type {string | number | Date | undefined} */ a,
+  /** @type {string | number | Date | undefined} */ b,
+) => {
+  if (a == null && b == null) {
+    return 0;
+  } else if (a == null) {
+    return 1;
+  } else if (b == null) {
+    return -1;
+  }
+
+  return +new Date(a) - +new Date(b);
+};
+
+/**
+ * @type {(function(*, *): number)[]}
+ */
+const compareFunctions = [
+  (a, b) => compareDates(a.bookingDate, b.bookingDate),
+  (a, b) => compareDates(a.bookingDateTime, b.bookingDateTime),
+  (a, b) => compareDates(a.valueDate, b.valueDate),
+  (a, b) => compareDates(a.valueDateTime, b.valueDateTime),
+];
+
 export const sortByBookingDateOrValueDate = (transactions = []) =>
-  transactions.sort(
-    (a, b) =>
-      +new Date(b.bookingDate || b.valueDate) -
-      +new Date(a.bookingDate || a.valueDate),
-  );
+  transactions.sort((a, b) => {
+    for (const sortFunction of compareFunctions) {
+      const result = sortFunction(b, a);
+      if (result !== 0) {
+        return result;
+      }
+    }
+    return 0;
+  });
 
 export const amountToInteger = (n) => Math.round(n * 100);
