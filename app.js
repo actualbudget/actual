603f970f12480c4d871d1a40d277213511e429ac
Michael Clark <5285928+MikesGlitch@users.noreply.github.com>
2024-10-28 22:17:35 +0000

    🐛  Fix migrations not working on fresh clone (#487)

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
-import run from './src/app.js';
 import runMigrations from './src/migrations.js';
 
 runMigrations()
-  .then(run)
+  .then(() => {
+    //import the app here becasue initial migrations need to be run first - they are dependencies of the app.js
+    import('./src/app.js').then((app) => app.default()); // run the app
+  })
   .catch((err) => {
     console.log('Error starting app:', err);
     process.exit(1);
