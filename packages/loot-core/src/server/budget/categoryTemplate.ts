5ee72573fa93682554f778e335ec593f3b784e1f
youngcw <calebyoung94@gmail.com>
2024-10-29 17:38:13 -0700

    minor changes and TS class migration

--- packages/loot-core/src/server/budget/categoryTemplate.ts
+++ packages/loot-core/src/server/budget/categoryTemplate.ts
@@ -1,9 +1,9 @@
 // @ts-strict-ignore
 
 import * as monthUtils from '../../shared/months';
-import * as db from '../../db';
+import * as db from '../db';
 import { getSheetValue, isReflectBudget, setBudget, setGoal } from './actions';
-import { amountToInteger, integerToAmount } from '../../shared/util';
+import { amountToInteger } from '../../shared/util';
 import {getActiveSchedules} from './statements';
 
 class categoryTemplate {
@@ -26,220 +26,230 @@ class categoryTemplate {
 //-----------------------------------------------------------------------------
 // Class interface
   
-  // returns the categoryID of the category for this object
-  get categoryID(){ return this.#categoryID; }
-
   // returns the total remainder weight of remainder templates in this category
-  get remainderWeight(){ return this.#remainderWeight; }
+  get remainderWeight(){ return this.remainderWeight; }
 
   // return the current budgeted amount in the category
-  get startingBalance(){ return this.#startingBalance; }
+  get startingBalance(){ return this.startingBalance; }
 
   // returns a list of priority levels in this category
-  get priorities(){ return this.#priorities; }
+  get priorities(){ return this.priorities; }
   
   // what is the full requested amount this month
-  static runAll(){
-    //TODO
+  runAll(force: boolean, available: number){
+    let toBudget;
+    this.priorities.forEach(p => {
+      toBudget += this.runTemplatesForPriority(p, available, force);
+    });
+    return toBudget;
   } 
 
   // run all templates in a given priority level
   // return: amount budgeted in this priority level
-  static runTemplatesForPriority(priority: int, budgetAvail: int){
-    const t = this.#templates.filter(t => t.priority === priority);
+  async runTemplatesForPriority(priority: number, budgetAvail: number, force: boolean){
+    const t = this.templates.filter(t => t.priority === priority);
     let available = budgetAvail;
     let toBudget = 0;
     // switch on template type and calculate the amount for the line
     for(let i = 0; i < t.length; i++) {
       switch (t[i].type) {
         case 'simple': {
-          toBudget += #runSimple(t,this.#limitAmount);
+          toBudget += this.runSimple(t,this.limitAmount);
           break;
         }
         case 'copy' : {
-          toBudget += #runCopy(t);
+          toBudget += await this.runCopy(t);
           break;
         }
         case 'week': {
-          toBudget += #runWeek(t);
+          toBudget += this.runWeek(t);
           break;
         }
         case 'spend': {
-          toBudget +- #runSpend(t);
+          toBudget +-this.runSpend(t);
           break;
         }
         case 'percentage': {
-          toBudget += #runPercentage(t, budgetAvail);
+          toBudget +=  await this.runPercentage(t, budgetAvail);
           break;
         }
         case 'by': {
-          toBudget += #runBy(t);
+          toBudget += this.runBy(t);
           break;
         }
         case 'schedule': {
-          toBudget += #runSchedule(t);
+          toBudget += this.runSchedule(t);
           break;
         }
         case 'average': {
-          toBudget += #runAverage(t);
+          toBudget += await this.runAverage(t);
           break;
         }
       }
 
       // don't overbudget when using a priority
-      if(priority > 0 && toBudget>available){
+      if(priority > 0 && force === false &&toBudget>available){
         toBudget=available;
       }
       available = available - toBudget;
 
-      if(available <= 0) { break; }
+      if(available <= 0 && force === false) { break; }
     }
 
-    this.#toBudgetAmount += toBudget;
+    this.toBudgetAmount += toBudget;
     return toBudget;
   }
 
-  static applyLimit(){
-    if(this.#limitCheck === false){
-      this.#toBudgetAmount = this.#targetAmount;
+  applyLimit(){
+    if(this.limitCheck === false){
+      this.toBudgetAmount = this.targetAmount;
       return
     }
-    if(this.#limitHold && this.#fromLastMonth >= this.#limitAmount){
-      this.#toBudgetAmount = 0;
+    if(this.limitHold && this.fromLastMonth >= this.limitAmount){
+      this.toBudgetAmount = 0;
       return
     }
-    if(this.#targetAmount>this.#limitAmount){
-      this.#toBudget = this.#limitAmount - this.#fromLastMonth;
+    if(this.targetAmount>this.limitAmount){
+      this.toBudgetAmount = this.limitAmount - this.fromLastMonth;
     }
   }
 
   // run all of the 'remainder' type templates
-  static runRemainder(budgetAvail: int, perWeight: int){
-    const toBudget = Math.round(this.#remainderWeight*perWeight); 
+  runRemainder(budgetAvail: number, perWeight: number){
+    const toBudget = Math.round(this.remainderWeight*perWeight); 
     //check possible overbudget from rounding, 1cent leftover
     if(toBudget > budgetAvail){
-      this.#toBudgetAmount += budgetAvail;
+      this.toBudgetAmount += budgetAvail;
     } else if (budgetAvail - toBudget === 1){
-      this.#toBudgetAmount+=toBudget + 1;
+      this.toBudgetAmount+=toBudget + 1;
     } else {
-      this.#toBudgetAmount+=toBudget;
+      this.toBudgetAmount+=toBudget;
     }
   }
 
-  static runFinish(){
-    #runGoal()
-    #setBudget();
-    #setGoal();
+  runFinish(){
+    this.runGoal()
+    this.setBudget();
+    this.setGoal();
   }
 
 
 //-----------------------------------------------------------------------------
 // Implimentation
-  
-  #categoryID;
-  #month;
-  #templates = [];
-  #remainder = [];
-  #goals = [];
-  #priorities = [];
-  #remainderWeight = 0; // sum of all remainder weights in category
-  #startingBalance = 0; // budgeted at the start;
-  #toBudgetAmount = 0; // amount that will be budgeted by the templates
-  #isLongGoal = false; // is the goal type a long goal
-  #goalAmount = 0;
-  #spentThisMonth = 0; // amount already spend this month
-  #fromLastMonth = 0; // leftover from last month
-  #limitAmount = 0, 
-  #limitCheck = false, 
-  #limitHold = false;
+  readonly categoryID: string;
+  private month;
+  private templates = [];
+  private remainder = [];
+  private goals = [];
+  private toBudgetAmount = 0; // amount that will be budgeted by the templates
+  private targetAmount = 0;
+  private isLongGoal = false; // is the goal type a long goal
+  private goalAmount = 0;
+  private spentThisMonth = 0; // amount already spend this month
+  private fromLastMonth = 0; // leftover from last month
+  private limitAmount = 0;
+  private limitCheck = false;
+  private limitHold = false;
 
-  constructor(templates, categoryID, month) {
-    this.#categoryID = categoryID;
-    this.#month = month;
+  private set priorities(value){this.priorities=value}
+  private set remainderWeight(value){this.remainderWeight=value}
+  private set startingBalance(value){this.startingBalance=value}
+
+  constructor(templates, categoryID: string, month) {
+    this.categoryID = categoryID;
+    this.month = month;
     // sort the template lines into regular template, goals, and remainder templates
-    this.#templates = templates.filter(t => {t.directive === 'template' && t.type != 'remainder'});    
-    this.#remainder = templates.filter(t =>{t.directive === 'template' && t.type === 'remainder'});
-    this.#goals = templates.filter(t => {t.directive === 'goal'});
+    this.templates = templates.filter(t => {t.directive === 'template' && t.type != 'remainder'});    
+    this.remainder = templates.filter(t =>{t.directive === 'template' && t.type === 'remainder'});
+    this.goals = templates.filter(t => {t.directive === 'goal'});
     //check templates and throw exception if there is something wrong
-    #checkTemplates();
+    this.checkTemplates();
     //find priorities
-    #findPriorities();
+    this.findPriorities();
     //find remainder weight
-    #findRemainderWeightSum();
+    this.findRemainderWeightSum();
     //get the starting budget amount
-    #readBudgeted();
+    this.readBudgeted();
     //get the from last month amount
-    #readSpent();
-    #calcLeftover();
+    this.readSpent();
+    this.calcLeftover();
   }
-
-  static #findPriorities(){
+  
+  private findPriorities(){
     let p = [];
-    this.#templates.forEach(t => {
+    this.templates.forEach(t => {
       if(t.priority != null){
         p.push(t.priority)
       }
     });
     //sort and reduce to unique items
-    this.#priorities = p.sort(function (a,b) {
+    this.priorities = p.sort(function (a,b) {
       return a-b;
     })
-    .filter((item, idx, curr) => curr.indexOf(item) === index);
+    .filter((item, idx, curr) => curr.indexOf(item) === idx);
   }
 
-  static #findRemainderWeightSum(){
+  private findRemainderWeightSum(){
     let weight = 0;
-    this.#remainder.forEach(r => {weight+=r.weight});
+    this.remainder.forEach(r => {weight+=r.weight});
     this.remainderWeight = weight;
   }
   
-  static #checkTemplates(){
+  private checkTemplates(){
     //run all the individual checks
-    #checkByAndSchedule();
-    #checkPercentage();
-    #checkLimit();
-    #checkGoal();
+    this.checkByAndSchedule();
+    this.checkPercentage();
+    this.checkLimit();
+    this.checkGoal();
   }
 
-  static #runGoal(){
-    if(this.#goals.length>0) {
-      this.#isLongGoal = true; 
-      this.#goalAmount = amountToInteger(this.#goals[0].amount);
+  private runGoal(){
+    if(this.goals.length>0) {
+      this.isLongGoal = true; 
+      this.goalAmount = amountToInteger(this.goals[0].amount);
       return
     }
-    this.#goalAmount = this.#toBudgetAmount;
+    this.goalAmount = this.toBudgetAmount;
   }
 
-  static #setBudget(){
+  private setBudget(){
     setBudget({
-      category: this.#categoryID,
-      month: this.#month,
-      amount: this.#toBudgetAmount,
+      category: this.categoryID,
+      month: this.month,
+      amount: this.toBudgetAmount,
     });
   }
 
-  static #readBudgeted(){
+  private async readBudgeted(){
     return await getSheetValue(
-      monthUtils.sheetForMonth(month),
+      monthUtils.sheetForMonth(this.month),
       `budget-${this.categoryID}`,
     );
   }
 
-  static #setGoal(){
+  private setGoal(){
     setGoal({
       category: this.categoryID,
-      goal: this.#goalAmount;
-      month: this.#month,
-      long_goal: this.#isLongGoal ? 1 : 0,
+      goal: this.goalAmount,
+      month: this.month,
+      long_goal: this.isLongGoal ? 1 : 0,
     });
+  }
+
+  private readSpent(){
+    //TODO
+  }
+
+  private calcLeftover(){
+    //TODO
   }
 
 //-----------------------------------------------------------------------------
 //  Template Validation
-  static #checkByAndSchedule(){
+  private async checkByAndSchedule(){
     //check schedule names
     const scheduleNames = (await getActiveSchedules()).map(({name}) => name);
-    this.#templates
+    this.templates
     .filter(t => t.type === 'schedule')
     .forEach(t => {
       if (!scheduleNames.includes(t.name)) {
@@ -248,7 +258,7 @@ class categoryTemplate {
     });
     //find lowest priority
     let lowestPriority = null;
-    this.#templates
+    this.templates
     .filter(t=> t.type === 'schedule' || t.type === 'by' )
     .forEach(t => {
       if(lowestPriority === null || t.priority < lowestPriority){
@@ -256,27 +266,28 @@ class categoryTemplate {
       }
     });
     //set priority to needed value
-    this.#templates
+    this.templates
     .filter(t => t.type === 'schedule' || t.type === 'by')
     .forEach(t => t.priority = lowestPriority);
     //TODO add a message that the priority has been changed
   }
 
-  static #checkLimit(){
-    for(let i = 0; i < this.#templates.length; i++){
-      if(this.#limitCheck){
+  private checkLimit(){
+    for(let i = 0; i < this.templates.length; i++){
+      const t = this.templates[i];
+      if(this.limitCheck){
         throw new Error('Only one `up to` allowed per category');
         break;
       } else if (t.limit!=null){
         this.limitCheck = true;
-        this.#limitHold = t.limit.hold ? true : false;
-        this.#limitAmount = amountToInteger(t.limit.amount);
+        this.limitHold = t.limit.hold ? true : false;
+        this.limitAmount = amountToInteger(t.limit.amount);
       }
-    });
+    };
   }
   
-  static #checkPercentage(){
-    const pt = this.#templates.filter(t =>  t.type === 'percentage');
+  private async checkPercentage(){
+    const pt = this.templates.filter(t =>  t.type === 'percentage');
     let reqCategories = [];
     pt.forEach(t => reqCategories.push(t.category));
 
@@ -291,15 +302,14 @@ class categoryTemplate {
     reqCategories.forEach(n => {
       if(n === 'availble funds' || n === 'all income'){
         //skip the name check since these are special
-        continue;
       } else if (!availNames.includes(n)) {
-        throw new Error(`Category ${t.category} is not found in available income categories`);
+        throw new Error(`Category ${n} is not found in available income categories`);
       }
     });
   }
 
-  static #checkGoal(){
-    if(this.#goals.length>1){
+  private checkGoal(){
+    if(this.goals.length>1){
       throw new Error('Can only have one #goal per category');
     }
   }
@@ -307,7 +317,7 @@ class categoryTemplate {
 //-----------------------------------------------------------------------------
 //  Processor Functions
 
-  static #runSimple(template, limit){
+  private runSimple(template, limit){
     let toBudget = 0;
     if (template.monthly != null) {
       toBudget= amountToInteger(template.monthly);
@@ -317,22 +327,22 @@ class categoryTemplate {
     return toBudget;
   }
 
-  static #runCopy(template){
+  private async runCopy(template){
     const sheetName = monthUtils.sheetForMonth(
-      monthUtils.subMonts(this.#month,template.lookback),
+      monthUtils.subMonths(this.month,template.lookback),
     );
-    return await getSheetValue(sheetName, `budget-${this.#categoryID}`);
+    return await getSheetValue(sheetName, `budget-${this.categoryID}`);
   }
 
-  static #runWeek(template){
+  private runWeek(template){
     let toBudget = 0;
     const amount = amountToInteger(template.amount);
-    const weeks = template.weeks != null ? match.round(template.weeks) : 1;
+    const weeks = template.weeks != null ? Math.round(template.weeks) : 1;
     let w = template.starting;
-    const nextMonth = monthUtils.addMonths(this.#month,1);
+    const nextMonth = monthUtils.addMonths(this.month,1);
 
     while (w < nextMonth){
-      if( w >= this.#month) {
+      if( w >= this.month) {
         toBudget += amount;
       }
       w = monthUtils.addWeeks(w, weeks);
@@ -340,79 +350,82 @@ class categoryTemplate {
     return toBudget;
   }
 
-  static #runSpend(template){
+  private async runSpend(template){
     const fromMonth = `${template.from}`;
     const toMonth = `${template.month}`;
-    let alreadyBudgeted = this.#fromLastMonth;
+    let alreadyBudgeted = this.fromLastMonth;
     let firstMonth = true;
 
     for (
       let m = fromMonth;
-      monthUtils.differenceIncCalendarMonths(this.#month, m) > 0;
+      monthUtils.differenceInCalendarMonths(this.month, m) > 0;
       m = monthUtils.addMonths(m,1)
     ) {
       const sheetName = monthUtils.sheetForMonth(m);
       if(firstMonth) {
         //TODO figure out if I already  found these values and can pass them in
-        const spent = await getSheetValue(sheetName, `sum-amount-${this.#categoryID}`);
-        const balance = await getSheetValue(sheetName, `leftover-${this.#categoryID}`);
+        const spent = await getSheetValue(sheetName, `sum-amount-${this.categoryID}`);
+        const balance = await getSheetValue(sheetName, `leftover-${this.categoryID}`);
         alreadyBudgeted = balance - spent;
         firstMonth = false;
       } else {
-        alreadyBudgeted += await getSheetValue(sheetName, `budget-${this.#categoryID}`);
+        alreadyBudgeted += await getSheetValue(sheetName, `budget-${this.categoryID}`);
       }
     }
 
-    const numMonths = monthUtils.differenceIncCalendarMonths(toMonth, this.#month);
+    const numMonths = monthUtils.differenceInCalendarMonths(toMonth, this.month);
     const target = amountToInteger(template.amount);
     if(numMonths < 0){
       return  0;
     } else {
-      return Math.round((target-alreadyBudget) /(numMonths + 1));
+      return Math.round((target-alreadyBudgeted) /(numMonths + 1));
     }
   }
 
-  static #runPercentage(template, availableFunds)){
+  private async runPercentage(template, availableFunds){
     const percent = template.percent;
     const cat = template.category.toLowerCase();
     const prev = template.previous;
-    let montlyIncome = 0;
+    let sheetName;
+    let monthlyIncome = 0;
     
     //choose the sheet to find income for
     if(prev){
-      const sheetName = monthUtils.sheetForMonth(
-        monthUtils.subMonths(this.#month, 1)
+      sheetName = monthUtils.sheetForMonth(
+        monthUtils.subMonths(this.month, 1)
       );
     } else {
-      const sheetName = monthUtils.sheetForMonth(this.#month);
+      sheetName = monthUtils.sheetForMonth(this.month);
     }
     if(cat === 'all income'){
-      montlyIncome = await getSheetValue(sheetName,`total-income`);
+      monthlyIncome = await getSheetValue(sheetName,`total-income`);
     } else if (cat === 'available funds') {
-      montlyIncome = availableFunds;
+      monthlyIncome = availableFunds;
     } else {
       const incomeCat = (await db.getCategories()).find(c=> c.is_income && c.name.toLowerCase() === cat);
       monthlyIncome = await getSheetValue(sheetName, `sum_amount-${incomeCat.id}`);
     }
     
-    return Math.max(0, Math.round(montlyIncome * (percent /100)));
+    return Math.max(0, Math.round(monthlyIncome * (percent /100)));
   }
 
-  static #runBy(template_lines){
+  private runBy(template_lines){
     //TODO
+    return 0;
   }
 
-  static #runSchedule(){
+  private runSchedule(template_lines){
     //TODO
+    return 0;
   }
 
-  static #runAverage(template){
+  private async runAverage(template){
     let sum = 0;
     for (let i = 1; i <= template.numMonths; i++) {
       const sheetName = monthUtils.sheetForMonth(
-        monthUtils.subMonts(this.#month, i)
+        monthUtils.subMonths(this.month, i)
       );
-      sum += await getSheetValue(sheetName, `sum-amount-${this.#categoryID}`);
+      sum += await getSheetValue(sheetName, `sum-amount-${this.categoryID}`);
     }
     return -Math.round(sum / template.amount);
   }
