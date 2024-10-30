d7b4da96a70eeffcf47f87caa3d960e9bc80f9db
UnderKoen <koenvanstaveren@hotmail.com>
2024-10-30 22:14:49 +0100

    enhance: context menu on sidebar account

--- packages/desktop-client/src/components/sidebar/Account.tsx
+++ packages/desktop-client/src/components/sidebar/Account.tsx
@@ -1,8 +1,14 @@
 // @ts-strict-ignore
-import React, { type CSSProperties } from 'react';
+import React, { type CSSProperties, useEffect, useRef, useState } from 'react';
+import { useDispatch } from 'react-redux';
 
 import { css, cx } from '@emotion/css';
 
+import {
+  openAccountCloseModal,
+  reopenAccount,
+  updateAccount,
+} from 'loot-core/client/actions';
 import * as Platform from 'loot-core/client/platform';
 import { type AccountEntity } from 'loot-core/src/types/models';
 
@@ -10,6 +16,8 @@ import { useNotes } from '../../hooks/useNotes';
 import { styles, theme } from '../../style';
 import { AlignedText } from '../common/AlignedText';
 import { Link } from '../common/Link';
+import { Menu } from '../common/Menu';
+import { Popover } from '../common/Popover';
 import { Text } from '../common/Text';
 import { Tooltip } from '../common/Tooltip';
 import { View } from '../common/View';
@@ -23,6 +31,7 @@ import {
 } from '../sort';
 import { type SheetFields, type Binding } from '../spreadsheet';
 import { CellValue } from '../spreadsheet/CellValue';
+import { useFeatureFlag } from '../../hooks/useFeatureFlag';
 
 export const accountNameStyle: CSSProperties = {
   marginTop: -2,
@@ -74,6 +83,12 @@ export function Account<FieldName extends SheetFields<'account'>>({
         : 'account-onbudget'
     : 'title';
 
+  const triggerRef = useRef(null);
+  const [menuOpen, setMenuOpen] = useState(false);
+  const contextMenusEnabled = useFeatureFlag('contextMenus');
+  const [crossOffset, setCrossOffset] = useState(0);
+  const [offset, setOffset] = useState(0);
+
   const { dragRef } = useDraggable({
     type,
     onDragChange,
@@ -87,12 +102,52 @@ export function Account<FieldName extends SheetFields<'account'>>({
     onDrop,
   });
 
+  const dispatch = useDispatch();
+
+  const editingRef = useRef(null);
+  const [isEditing, setIsEditing] = useState(false);
+  useEffect(() => {
+    if (!editingRef.current) return;
+    if (isEditing) {
+      editingRef.current.focus();
+      window.getSelection().selectAllChildren(editingRef.current);
+    } else {
+      editingRef.current.textContent = name;
+    }
+  }, [name, isEditing]);
+
+  const updateName = () => {
+    if (account && isEditing) {
+      setIsEditing(false);
+      const newName = editingRef.current.textContent;
+      if (newName !== account.name && newName.trim()) {
+        dispatch(
+          updateAccount({
+            ...account,
+            name: newName,
+          }),
+        );
+      }
+    }
+  };
+
   const accountNote = useNotes(`account-${account?.id}`);
   const needsTooltip = !!account?.id;
 
   const accountRow = (
-    <View innerRef={dropRef} style={{ flexShrink: 0, ...outerStyle }}>
-      <View>
+    <View
+      innerRef={dropRef}
+      style={{ flexShrink: 0, ...outerStyle }}
+      onContextMenu={e => {
+        if (!needsTooltip || !contextMenusEnabled) return;
+        e.preventDefault();
+        const rect = e.currentTarget.getBoundingClientRect();
+        setCrossOffset(e.clientX - rect.left);
+        setOffset(e.clientY - rect.bottom);
+        setMenuOpen(true);
+      }}
+    >
+      <View innerRef={triggerRef}>
         <DropHighlight pos={dropPos} />
         <View innerRef={dragRef}>
           <Link
@@ -158,10 +213,65 @@ export function Account<FieldName extends SheetFields<'account'>>({
                   paddingBottom: '3px',
                 }
               }
-              left={name}
+              left={
+                <span
+                  contentEditable={isEditing}
+                  ref={editingRef}
+                  suppressContentEditableWarning={true}
+                  onBlur={updateName}
+                  onKeyDown={e => {
+                    if (e.key === 'Enter') {
+                      e.preventDefault();
+                      updateName();
+                    } else if (e.key === 'Escape') {
+                      setIsEditing(false);
+                    }
+                  }}
+                >
+                  {name}
+                </span>
+              }
               right={<CellValue binding={query} type="financial" />}
             />
           </Link>
+          {account && (
+            <Popover
+              triggerRef={triggerRef}
+              placement="bottom start"
+              isOpen={menuOpen}
+              onOpenChange={() => setMenuOpen(false)}
+              style={{ width: 200, margin: 1 }}
+              isNonModal
+              crossOffset={crossOffset}
+              offset={offset}
+            >
+              <Menu
+                onMenuSelect={type => {
+                  switch (type) {
+                    case 'close': {
+                      dispatch(openAccountCloseModal(account.id));
+                      break;
+                    }
+                    case 'reopen': {
+                      dispatch(reopenAccount(account.id));
+                      break;
+                    }
+                    case 'rename': {
+                      setIsEditing(true);
+                      break;
+                    }
+                  }
+                  setMenuOpen(false);
+                }}
+                items={[
+                  { name: 'rename', text: 'Rename' },
+                  account.closed
+                    ? { name: 'reopen', text: 'Reopen' }
+                    : { name: 'close', text: 'Close' },
+                ]}
+              />
+            </Popover>
+          )}
         </View>
       </View>
     </View>
