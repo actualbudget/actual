635ef27696a5fa6a31278d170485a6b2afa3dcce
Dāvis <dmednis@users.noreply.github.com>
2024-11-08 23:05:51 +0100

    Enrich pending transaction creditor info for SWEDBANK_HABALV22 (#497)

--- src/app-gocardless/banks/swedbank-habalv22.js
+++ src/app-gocardless/banks/swedbank-habalv22.js
@@ -14,17 +14,36 @@ export default {
    * The actual transaction date for card transactions is only available in the remittanceInformationUnstructured field when the transaction is booked.
    */
   normalizeTransaction(transaction, booked) {
-    const dateMatch = transaction.remittanceInformationUnstructured?.match(
-      /PIRKUMS [\d*]+ (\d{2}.\d{2}.\d{4})/,
-    );
+    const isCardTransaction =
+      transaction.remittanceInformationUnstructured?.startsWith('PIRKUMS');
 
-    if (dateMatch) {
-      const extractedDate = d.parse(dateMatch[1], 'dd.MM.yyyy', new Date());
+    if (isCardTransaction) {
+      if (!booked && !transaction.creditorName) {
+        const creditorNameMatch =
+          transaction.remittanceInformationUnstructured?.match(
+            /PIRKUMS [\d*]+ \d{2}\.\d{2}\.\d{2} \d{2}:\d{2} [\d.]+ \w{3} \(\d+\) (.+)/,
+          );
 
-      return Fallback.normalizeTransaction(
-        { ...transaction, bookingDate: d.format(extractedDate, 'yyyy-MM-dd') },
-        booked,
+        if (creditorNameMatch) {
+          transaction = {
+            ...transaction,
+            creditorName: creditorNameMatch[1],
+          };
+        }
+      }
+
+      const dateMatch = transaction.remittanceInformationUnstructured?.match(
+        /PIRKUMS [\d*]+ (\d{2}\.\d{2}\.\d{4})/,
       );
+
+      if (dateMatch) {
+        const extractedDate = d.parse(dateMatch[1], 'dd.MM.yyyy', new Date());
+
+        transaction = {
+          ...transaction,
+          bookingDate: d.format(extractedDate, 'yyyy-MM-dd'),
+        };
+      }
     }
 
     return Fallback.normalizeTransaction(transaction, booked);
