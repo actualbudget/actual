36839ff153a4ad6c26fab3ed686087f71ed5701a
Joel Jeremy Marquez <joeljeremy.marquez@gmail.com>
2024-11-21 15:23:35 -0800

    Convert ManagePayees components to Typescript (#3867)
    * Convert ManagePayees components to Typescript
    
    * Release notes
    
    * Fix lint
    
    * Fix lint
    
    * Fix lint

--- packages/desktop-client/src/components/payees/ManagePayeesPage.tsx
+++ packages/desktop-client/src/components/payees/ManagePayeesPage.tsx
@@ -2,6 +2,8 @@ import React from 'react';
 import { useTranslation } from 'react-i18next';
 import { useLocation } from 'react-router-dom';
 
+import { type PayeeEntity } from 'loot-core/types/models';
+
 import { Page } from '../Page';
 
 import { ManagePayeesWithData } from './ManagePayeesWithData';
@@ -9,15 +11,14 @@ import { ManagePayeesWithData } from './ManagePayeesWithData';
 export function ManagePayeesPage() {
   const { t } = useTranslation();
   const location = useLocation();
+  const locationState = location.state;
+  const initialSelectedIds =
+    locationState && 'selectedPayee' in locationState
+      ? [locationState.selectedPayee as PayeeEntity['id']]
+      : [];
   return (
     <Page header={t('Payees')}>
-      <ManagePayeesWithData
-        initialSelectedIds={
-          location.state && location.state.selectedPayee
-            ? [location.state.selectedPayee]
-            : null
-        }
-      />
+      <ManagePayeesWithData initialSelectedIds={initialSelectedIds} />
     </Page>
   );
 }
