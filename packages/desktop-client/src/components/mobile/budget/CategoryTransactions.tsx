9024f364c561824661e75f52b9f060db62292d22
Cindy Wu <ciindy.wu@gmail.com>
2024-11-22 18:35:45 -1000

    make onRefresh optional

--- packages/desktop-client/src/components/mobile/budget/CategoryTransactions.tsx
+++ packages/desktop-client/src/components/mobile/budget/CategoryTransactions.tsx
@@ -126,7 +126,6 @@ export function CategoryTransactions({
         onSearch={onSearch}
         onLoadMore={loadMoreTransactions}
         onOpenTransaction={onOpenTransaction}
-        onRefresh
       />
     </Page>
   );
