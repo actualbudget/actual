495ea07afe64c69ab3080441467c751a2cc8c53f
matt <matt@fiddaman.net>
2024-12-11 22:03:44 +0000

    UserAccessHeader

--- packages/desktop-client/src/components/admin/UserAccess/UserAccessHeader.tsx
+++ packages/desktop-client/src/components/admin/UserAccess/UserAccessHeader.tsx
@@ -1,13 +1,16 @@
 import React from 'react';
+import { useTranslation } from 'react-i18next';
 
 import { Cell, TableHeader } from '../../table';
 
 export function UserAccessHeader() {
+  const { t } = useTranslation();
+
   return (
     <TableHeader>
-      <Cell value="Access" width={100} style={{ paddingLeft: 15 }} />
-      <Cell value="User" width="flex" />
-      <Cell value="Owner" width={100} />
+      <Cell value={t('Access')} width={100} style={{ paddingLeft: 15 }} />
+      <Cell value={t('User')} width="flex" />
+      <Cell value={t('Owner')} width={100} />
     </TableHeader>
   );
 }
