5e805085b025d3bba5444cbfdedbc0f09065bafd
Joel Jeremy Marquez <joeljeremy.marquez@gmail.com>
2024-12-11 14:07:59 -0800

    Ignore unused useActions

--- packages/desktop-client/src/hooks/useActions.ts
+++ packages/desktop-client/src/hooks/useActions.ts
@@ -25,6 +25,7 @@ export type BoundActions = {
  * @deprecated please use actions directly with `useAppDispatch`
  * @see https://github.com/reduxjs/react-redux/issues/1252#issuecomment-488160930
  **/
+// eslint-disable-next-line import/no-unused-modules
 export function useActions() {
   const dispatch = useDispatch();
   return useMemo(() => {
