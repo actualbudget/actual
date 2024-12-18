7fc4685e7b8616329f5da31e9a1c418d5de551a6
Joel Jeremy Marquez <joeljeremy.marquez@gmail.com>
2024-12-18 11:42:41 -0800

    Fix typecheck error

--- packages/loot-core/src/client/store/mock.ts
+++ packages/loot-core/src/client/store/mock.ts
@@ -6,12 +6,12 @@ import { type store as realStore } from './index';
 
 const appReducer = combineReducers(reducers);
 
-export let mockStore: typeof realStore | null = null;
+export let mockStore: typeof realStore = configureStore({
+  reducer: appReducer,
+});
 
 export function resetMockStore() {
   mockStore = configureStore({
     reducer: appReducer,
   });
 }
-
-resetMockStore();
