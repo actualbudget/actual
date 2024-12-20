b83d3493d0b032e531e6069deb76a1e20622b839
Joel Jeremy Marquez <joeljeremy.marquez@gmail.com>
2024-12-20 08:54:34 -0800

    Rename state

--- packages/desktop-client/src/hooks/useFailedAccounts.ts
+++ packages/desktop-client/src/hooks/useFailedAccounts.ts
@@ -3,7 +3,7 @@ import { useMemo } from 'react';
 import { useSelector } from '../redux';
 
 export function useFailedAccounts() {
-  const failedAccounts = useSelector(state => state.account.failedAccounts);
+  const failedAccounts = useSelector(state => state.accounts.failedAccounts);
   return useMemo(
     () => new Map(Object.entries(failedAccounts)),
     [failedAccounts],
