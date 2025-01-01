b2f97aba1e1d426f2f131086efd41871b46f0cc9
Joel Jeremy Marquez <joeljeremy.marquez@gmail.com>
2025-01-01 12:49:12 -0800

    Remove useAppStore

--- packages/loot-core/src/client/store/index.ts
+++ packages/loot-core/src/client/store/index.ts
@@ -48,7 +48,6 @@ export const store = configureStore({
     }),
 });
 
-export type AppStore = typeof store;
 export type RootState = ReturnType<typeof store.getState>;
 export type AppDispatch = typeof store.dispatch;
 export type GetRootState = typeof store.getState;
