bdde7a5be4a501ec5ed508e0871f881ad91eb05e
Joel Jeremy Marquez <joeljeremy.marquez@gmail.com>
2025-01-08 13:40:39 -0800

    Fix typecheck error

--- packages/desktop-client/src/hooks/useFailedAccounts.ts
+++ packages/desktop-client/src/hooks/useFailedAccounts.ts
@@ -3,7 +3,7 @@ import { useMemo } from 'react';
 import { useSelector } from '../redux';
 
 export function useFailedAccounts() {
-  const failedAccounts = useSelector(state => state.accounts.failedAccounts);
+  const failedAccounts = useSelector(state => state.account.failedAccounts);
   return useMemo(
     () => new Map(Object.entries(failedAccounts)),
     [failedAccounts],
