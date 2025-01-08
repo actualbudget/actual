377e06abf77c47865af90e82398103a1d0492853
Joel Jeremy Marquez <joeljeremy.marquez@gmail.com>
2025-01-07 23:56:02 -0800

    Fix typecheck error

--- packages/loot-core/src/client/store/index.ts
+++ packages/loot-core/src/client/store/index.ts
@@ -59,6 +59,7 @@ export const store = configureStore({
     }),
 });
 
+export type AppStore = typeof store;
 export type RootState = ReturnType<typeof store.getState>;
 export type AppDispatch = typeof store.dispatch;
 export type GetRootState = typeof store.getState;
