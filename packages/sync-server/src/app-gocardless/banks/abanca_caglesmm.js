626c72ac266186cca0a9b7ab44e610afc362b122
Mike Clark <mclarkgb@gmail.com>
2025-01-10 19:18:23 +0000

    fallout

---
+++ packages/sync-server/src/app-gocardless/banks/abanca_caglesmm.js
@@ -0,0 +1,24 @@
+import Fallback from './integration-bank.js';
+
+import { formatPayeeName } from '../../util/payee-name.js';
+
+/** @type {import('./bank.interface.js').IBank} */
+export default {
+  ...Fallback,
+
+  institutionIds: ['ABANCA_CAGLESMM', 'ABANCA_CAGLPTPL'],
+
+  accessValidForDays: 180,
+
+  // Abanca transactions doesn't get the creditorName/debtorName properly
+  normalizeTransaction(transaction, _booked) {
+    transaction.creditorName = transaction.remittanceInformationStructured;
+    transaction.debtorName = transaction.remittanceInformationStructured;
+
+    return {
+      ...transaction,
+      payeeName: formatPayeeName(transaction),
+      date: transaction.bookingDate || transaction.valueDate,
+    };
+  },
+};
