940b0f10ec4e9c993224ffcf004ae1e19a51cfa4
Julian Dominguez-Schatz <julian.dominguezschatz@gmail.com>
2025-01-11 12:37:48 -0500

    Wrap missed strings in `t`

--- packages/desktop-client/src/components/settings/LanguageSettings.tsx
+++ packages/desktop-client/src/components/settings/LanguageSettings.tsx
@@ -1,5 +1,7 @@
 import React from 'react';
-import { Trans } from 'react-i18next';
+import { Trans, useTranslation } from 'react-i18next';
+
+import { type TFunction } from 'i18next';
 
 import { useGlobalPref } from '../../hooks/useGlobalPref';
 import { availableLanguages, setI18NextLanguage } from '../../i18n';
@@ -10,19 +12,21 @@ import { Text } from '../common/Text';
 
 import { Setting } from './UI';
 
-const languageOptions: SelectOption[] = [
-  ['', 'System default'] as [string, string],
-  Menu.line as typeof Menu.line,
-].concat(
-  availableLanguages.map(lang => [
-    lang,
-    new Intl.DisplayNames([lang], {
-      type: 'language',
-    }).of(lang) || lang,
-  ]),
-);
+const languageOptions = (t: TFunction): SelectOption[] =>
+  [
+    ['', t('System default')] as [string, string],
+    Menu.line as typeof Menu.line,
+  ].concat(
+    availableLanguages.map(lang => [
+      lang,
+      new Intl.DisplayNames([lang], {
+        type: 'language',
+      }).of(lang) || lang,
+    ]),
+  );
 
 export function LanguageSettings() {
+  const { t } = useTranslation();
   const [language, setLanguage] = useGlobalPref('language');
   const isEnabled = !!availableLanguages.length;
 
@@ -30,10 +34,11 @@ export function LanguageSettings() {
     <Setting
       primaryAction={
         <Select
-          options={languageOptions}
+          aria-label={t('Select language')}
+          options={languageOptions(t)}
           value={isEnabled ? (language ?? '') : 'not-available'}
           defaultLabel={
-            isEnabled ? 'Select language' : 'No languages available'
+            isEnabled ? t('Select language') : t('No languages available')
           }
           onChange={value => {
             setLanguage(value);
