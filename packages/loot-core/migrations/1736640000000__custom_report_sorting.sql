e54a9faee7b3fd0b689240a96b1ae8d1ae7309d4
matt <matt@fiddaman.net>
2025-01-13 16:02:26 +0000

    fix migration

--- packages/loot-core/migrations/1736640000000__custom_report_sorting.sql
+++ packages/loot-core/migrations/1736640000000__custom_report_sorting.sql
@@ -1,6 +1,6 @@
 BEGIN TRANSACTION;
 
-ALTER TABLE custom_reports ADD COLUMN sort_by TEXT DEFAULT 'desc';
-UPDATE custom_reports SET sort_by = 'desc';
+ALTER TABLE custom_reports ADD COLUMN sort_by TEXT DEFAULT 'Descending';
+UPDATE custom_reports SET sort_by = 'Descending';
 
 COMMIT;
