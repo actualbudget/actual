d8db9cde0e0851ba59effeb392f41b63bb0e609a
Joel Jeremy Marquez <joeljeremy.marquez@gmail.com>
2025-01-14 09:07:54 -0800

    Remove old prefs types

--- packages/loot-core/src/client/state-types/index.d.ts
+++ packages/loot-core/src/client/state-types/index.d.ts
@@ -1,15 +1,13 @@
 import type * as constants from '../constants';
 
-import type { PrefsActions, PrefsState } from './prefs';
 import type { UserActions, UserState } from './user';
 
 export type CloseBudgetAction = {
   type: typeof constants.CLOSE_BUDGET;
 };
 
-export type Action = PrefsActions | UserActions | CloseBudgetAction;
+export type Action = UserActions | CloseBudgetAction;
 
 export type State = {
-  prefs: PrefsState;
   user: UserState;
 };
