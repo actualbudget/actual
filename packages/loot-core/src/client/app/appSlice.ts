910127e5ac13eb557d849bf298c8f207a8530eb1
Joel Jeremy Marquez <joeljeremy.marquez@gmail.com>
2025-01-14 10:01:52 -0800

    Remove action imports

--- packages/loot-core/src/client/app/appSlice.ts
+++ packages/loot-core/src/client/app/appSlice.ts
@@ -8,6 +8,7 @@ import { syncAccounts } from '../accounts/accountsSlice';
 import { pushModal } from '../modals/modalsSlice';
 import { loadPrefs } from '../prefs/prefsSlice';
 import { createAppAsyncThunk } from '../redux';
+import { loadPrefs } from '../prefs/prefsSlice';
 
 const sliceName = 'app';
 
