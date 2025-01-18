17beda685ccaef7bff6409a4f62ec0973fd1f194
Matt Fiddaman <github@m.fiddaman.uk>
2025-01-18 23:10:56 +0000

    suggestions

--- packages/desktop-client/src/components/modals/EditRuleModal.jsx
+++ packages/desktop-client/src/components/modals/EditRuleModal.jsx
@@ -347,8 +347,9 @@ function ScheduleDescription({ id }) {
         </Text>
         <Text style={{ margin: '0 5px' }}> — </Text>
         <Text style={{ flexShrink: 0 }}>
-          <Trans>Next:</Trans>{' '}
-          {monthUtils.format(schedule.next_date, dateFormat)}
+          <Trans>
+            Next: {{month: monthUtils.format(schedule.next_date, dateFormat)}}
+          </Trans>
         </Text>
       </View>
       <StatusBadge status={status} />
