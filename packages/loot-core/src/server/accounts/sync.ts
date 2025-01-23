30ed94eb2ba5b97ba097e1b3b5a023eb9c63d1a8
Joel Jeremy Marquez <joeljeremy.marquez@gmail.com>
2025-01-23 08:50:11 -0800

    Fix lint and typecheck

--- packages/loot-core/src/server/accounts/sync.ts
+++ packages/loot-core/src/server/accounts/sync.ts
@@ -32,8 +32,6 @@ import {
   defaultMappings,
   mappingsFromString,
 } from '../util/custom-sync-mapping';
-import { runRules } from '../transactions/transaction-rules';
-import { batchUpdateTransactions } from '../transactions';
 
 import { getStartingBalancePayee } from './payees';
 import { title } from './title';
