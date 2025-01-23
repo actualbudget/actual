86c5682eaa71ff986297076a163fca668169b88d
Leandro Menezes <lelemm@gmail.com>
2025-01-22 23:53:39 -0300

    added some fallback cases

--- packages/loot-core/src/shared/locale.ts
+++ packages/loot-core/src/shared/locale.ts
@@ -1,7 +1,18 @@
 import * as locales from 'date-fns/locale';
 
 export function getLocale(language: string) {
-  const localeKey = language.replace('-', '') as keyof typeof locales;
+  if (!language || typeof language !== 'string') {
+    return locales.enUS;
+  }
+
+  let localeKey = language.replace('-', '') as keyof typeof locales;
+
+  if (localeKey in locales) {
+    return locales[localeKey];
+  }
+
+  //if language was not found with four letters, try with two
+  localeKey = language.replace('-', '').substring(0, 2) as keyof typeof locales;
 
   if (localeKey in locales) {
     return locales[localeKey];
