5475b4251fc02f035a7a221e7cf626800e90fc7b
Joel Jeremy Marquez <joeljeremy.marquez@gmail.com>
2025-01-29 09:43:27 -0800

    Fix lint and VRT

--- packages/desktop-client/src/components/mobile/budget/CategoryTransactions.tsx
+++ packages/desktop-client/src/components/mobile/budget/CategoryTransactions.tsx
@@ -1,4 +1,4 @@
-import React, { useCallback, useEffect, useMemo, useState } from 'react';
+import React, { useCallback, useEffect, useState } from 'react';
 
 import { SchedulesProvider } from 'loot-core/client/data-hooks/schedules';
 import {
