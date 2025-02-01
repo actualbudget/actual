266b656f3a023038a601cbc5cb5fac215edec7dd
Koen van Staveren <koenvanstaveren@hotmail.com>
2025-02-01 17:13:21 +0100

    Update packages/loot-core/migrations/1736640000000__custom_report_sorting.sql
    Co-authored-by: Matt Fiddaman <github@m.fiddaman.uk>

--- packages/loot-core/migrations/1736640000000__custom_report_sorting.sql
+++ packages/loot-core/migrations/1736640000000__custom_report_sorting.sql
@@ -2,6 +2,6 @@ BEGIN TRANSACTION;
 
 ALTER TABLE custom_reports ADD COLUMN sort_by TEXT DEFAULT 'desc';
 UPDATE custom_reports SET sort_by = 'desc';
-UPDATE custom_reports SET sort_by = 'Budget' where graph_type = 'TableGraph';
+UPDATE custom_reports SET sort_by = 'budget' where graph_type = 'TableGraph';
 
 COMMIT;
