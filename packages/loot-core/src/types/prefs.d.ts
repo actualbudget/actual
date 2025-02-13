2327eb2c22353f7ee8f5a64c59848e235436b0d9
Taylor Buist <tbuist22@gmail.com>
2025-02-12 19:53:13 -0600

    Add progress bars for categories and goals

--- packages/loot-core/src/types/prefs.d.ts
+++ packages/loot-core/src/types/prefs.d.ts
@@ -75,6 +75,7 @@ export type LocalPrefs = Partial<{
   reportsViewLabel: boolean;
   sidebarWidth: number;
   'mobile.showSpentColumn': boolean;
+  'budget.showProgressBars': boolean;
 }>;
 
 export type Theme = 'light' | 'dark' | 'auto' | 'midnight' | 'development';
