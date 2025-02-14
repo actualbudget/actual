ceced4f19d42d48474edd0942c46579d518c00b1
Alberto Cortina Eduarte <albertocortina96@gmail.com>
2025-02-14 19:43:34 +0100

    ♻️ (imports) Update all Toggle component imports to the component lib

--- packages/desktop-client/src/components/mobile/transactions/TransactionEdit.jsx
+++ packages/desktop-client/src/components/mobile/transactions/TransactionEdit.jsx
@@ -10,6 +10,7 @@ import React, {
 import { useTranslation } from 'react-i18next';
 import { useLocation, useParams } from 'react-router-dom';
 
+import { Toggle } from '@actual-app/components/toggle';
 import { View } from '@actual-app/components/view';
 import {
   format as formatDate,
@@ -61,7 +62,6 @@ import { useSelector, useDispatch } from '../../../redux';
 import { styles, theme } from '../../../style';
 import { Button } from '../../common/Button';
 import { Text } from '../../common/Text';
-import { Toggle } from '../../common/Toggle';
 import { MobilePageHeader, Page } from '../../Page';
 import { AmountInput } from '../../util/AmountInput';
 import { MobileBackButton } from '../MobileBackButton';
