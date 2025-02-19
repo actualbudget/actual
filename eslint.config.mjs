d902c38253af891654e6fff85ef3e65f6e3f4351
Joel Jeremy Marquez <joeljeremy.marquez@gmail.com>
2025-02-19 07:06:41 -0800

    Fix react-hooks/exhaustive-deps error on TransactionsTable.jsx (#4268)
    * Fix react-hooks/exhaustive-deps error on TransactionsTable.jsx
    
    * Release notes
    
    * Fix lint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -792,9 +792,6 @@ export default [
       'packages/desktop-client/src/components/spreadsheet/useSheetValue.ts',
       'packages/desktop-client/src/components/table.tsx',
       'packages/desktop-client/src/components/transactions/TransactionList.jsx',
-      'packages/desktop-client/src/components/transactions/TransactionsTable.jsx',
-      'packages/desktop-client/src/components/transactions/TransactionsTable.test.jsx',
-      // 'packages/desktop-client/src/hooks/useAccounts.ts',
       'packages/desktop-client/src/hooks/useCategories.ts',
     ],
 
