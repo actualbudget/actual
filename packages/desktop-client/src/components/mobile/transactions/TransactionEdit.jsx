67a4f03d8dd6077da1a4788a5db049fdf1504339
tempiz <39291148+tempiz@users.noreply.github.com>
2025-03-04 13:13:13 -0600

    Fix Lint Check Error

--- packages/desktop-client/src/components/mobile/transactions/TransactionEdit.jsx
+++ packages/desktop-client/src/components/mobile/transactions/TransactionEdit.jsx
@@ -904,35 +904,35 @@ const TransactionEditInner = memo(function TransactionEditInner({
         )}
         {childTransactions.length > 0 && (
           <View style={{ alignItems: 'center' }}>
-          <Button
-            disabled={editingField}
-            style={{
-              height: 40,
-              borderWidth: 0,
-              marginLeft: styles.mobileEditingPadding,
-              marginRight: styles.mobileEditingPadding,
-              marginTop: 10,
-              backgroundColor: 'transparent',
-            }}
-            onClick={() => onAddSplit(transaction.id)}
-            type="bare"
-          >
-            <SvgSplit
-              width={17}
-              height={17}
-              style={{ color: theme.formLabelText }}
-            />
-            <Text
+            <Button
+              disabled={editingField}
               style={{
-                marginLeft: 5,
-                userSelect: 'none',
-                color: theme.formLabelText,
+                height: 40,
+                borderWidth: 0,
+                marginLeft: styles.mobileEditingPadding,
+                marginRight: styles.mobileEditingPadding,
+                marginTop: 10,
+                backgroundColor: 'transparent',
               }}
+              onClick={() => onAddSplit(transaction.id)}
+              type="bare"
             >
-              {t('Add Split')}
-            </Text>
-          </Button>
-        </View>
+              <SvgSplit
+                width={17}
+                height={17}
+                style={{ color: theme.formLabelText }}
+              />
+              <Text
+                style={{
+                  marginLeft: 5,
+                  userSelect: 'none',
+                  color: theme.formLabelText,
+                }}
+              >
+                {t('Add Split')}
+              </Text>
+            </Button>
+          </View>
         )}
         <View>
           <FieldLabel title={t('Account')} />
