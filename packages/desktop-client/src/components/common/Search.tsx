fe0271fc1efb23fb4b331f9c31aa5c7057ba40a3
Matiss Janis Aboltins <matiss@mja.lv>
2025-03-10 16:35:23 +0000

    Patch linter

--- packages/desktop-client/src/components/common/Search.tsx
+++ packages/desktop-client/src/components/common/Search.tsx
@@ -2,12 +2,11 @@ import { useState, type Ref } from 'react';
 import { useTranslation } from 'react-i18next';
 
 import { Button } from '@actual-app/components/button';
+import { SvgRemove, SvgSearchAlternate } from '@actual-app/components/icons/v2';
 import { defaultInputStyle, Input } from '@actual-app/components/input';
 import { type CSSProperties } from '@actual-app/components/styles';
-import { View } from '@actual-app/components/view';
-
-import { SvgRemove, SvgSearchAlternate } from '@actual-app/components/icons/v2';
 import { theme } from '@actual-app/components/theme';
+import { View } from '@actual-app/components/view';
 
 type SearchProps = {
   inputRef?: Ref<HTMLInputElement>;
