e7db7030a85e56307bbbb760df7f39e3b2e6f0e3
Joel Jeremy Marquez <joeljeremy.marquez@gmail.com>
2025-03-20 11:42:43 -0700

    Blur on escape

--- packages/desktop-client/src/components/budget/BudgetCategoriesV2.tsx
+++ packages/desktop-client/src/components/budget/BudgetCategoriesV2.tsx
@@ -193,9 +193,12 @@ export function BudgetCategories({
   const [collapsedGroupIds = [], setCollapsedGroupIdsPref] =
     useLocalPref('budget.collapsed');
   const [showHiddenCategories] = useLocalPref('budget.showHiddenCategories');
-  const onCollapse = useCallback(value => {
-    setCollapsedGroupIdsPref(value);
-  }, []);
+  const onCollapse = useCallback(
+    (value: string[]) => {
+      setCollapsedGroupIdsPref(value);
+    },
+    [setCollapsedGroupIdsPref],
+  );
 
   const [isAddingGroup, setIsAddingGroup] = useState(false);
   const [groupOfNewCategory, setGroupOfNewCategory] = useState<
@@ -389,11 +392,18 @@ export function BudgetCategories({
         overflow: 'hidden',
         borderRadius: 4,
       }}
+      onKeyDown={e => {
+        if (e.key === 'Escape' && e.target instanceof HTMLElement) {
+          e.target.blur();
+        }
+      }}
     >
       <ResizableTableContainer
         style={{
           height: '100%',
-          overflow: 'auto',
+          width: '100%',
+          overflowY: 'auto',
+          overflowX: 'hidden',
         }}
       >
         <Table
@@ -1723,7 +1733,6 @@ function CategoryBudgetedCell({
               className={cx(
                 { 'hover-visible': !isMenuOpen && !isFocusVisible },
                 css({
-                  marginLeft: 5,
                   display:
                     shouldHideBudgetMenuButton && !isFocusVisible
                       ? 'none'
@@ -2230,7 +2239,7 @@ type BalanceColumnProps = ComponentPropsWithoutRef<typeof ResizableColumn> & {
   month: string;
 };
 
-function BalanceColumn({ month, ...props }: BalanceColumnProps) {
+function BalanceColumn({ month, style, ...props }: BalanceColumnProps) {
   const [budgetType = 'rollover'] = useSyncedPref('budgetType');
   const bindingBudgetType: SheetNames =
     budgetType === 'rollover' ? 'envelope-budget' : 'tracking-budget';
@@ -2241,7 +2250,13 @@ function BalanceColumn({ month, ...props }: BalanceColumnProps) {
       : trackingBudget.totalLeftover;
 
   return (
-    <Column {...props}>
+    <Column
+      style={{
+        flex: 1,
+        ...style,
+      }}
+      {...props}
+    >
       <SheetNameProvider name={monthUtils.sheetForMonth(month)}>
         <View
           style={{
