f343fce709905991db7caaffb42ea97d27a1baa6
Mike Clark <mclarkgb@gmail.com>
2025-03-21 23:21:09 +0000

    fix normal e2e scripts

--- packages/desktop-electron/e2e/onboarding.test.ts
+++ packages/desktop-electron/e2e/onboarding.test.ts
@@ -1,17 +1,19 @@
 import { test, expect, _electron } from '@playwright/test';
 
-test('Onboarding', async () => {
-  const electronApp = await _electron.launch({
-    args: ['.'],
-    env: {
-      ACTUAL_DOCUMENT_DIR: 'e2e/data',
-      ACTUAL_DATA_DIR: 'e2e/data',
-      EXECUTION_CONTEXT: 'playwright',
-    },
+test.describe('Onboarding', () => {
+  test('checks the page visuals', async () => {
+    const electronApp = await _electron.launch({
+      args: ['.'],
+      env: {
+        ACTUAL_DOCUMENT_DIR: 'e2e/data',
+        ACTUAL_DATA_DIR: 'e2e/data',
+        EXECUTION_CONTEXT: 'playwright',
+      },
+    });
+
+    const window = await electronApp.firstWindow();
+
+    await expect(window).toHaveScreenshot();
+    await electronApp.close();
   });
-
-  const window = await electronApp.firstWindow();
-
-  await expect(window).toHaveScreenshot('onboarding.png');
-  await electronApp.close();
 });
