6fdefd3ba5c0065046c7220f395a6c97733bd92a
Joel Jeremy Marquez <joeljeremy.marquez@gmail.com>
2025-03-27 23:10:24 -0700

    Update with indeces

--- packages/loot-core/drizzle/0002_category_groups.sql
+++ packages/loot-core/drizzle/0002_category_groups.sql
@@ -6,3 +6,8 @@ CREATE TABLE "category_groups" (
 	"hidden" boolean DEFAULT false,
 	"tombstone" boolean DEFAULT false
 );
+--> statement-breakpoint
+CREATE INDEX "category_groups_is_income_index" ON "category_groups" USING btree ("is_income");--> statement-breakpoint
+CREATE INDEX "category_groups_hidden_index" ON "category_groups" USING btree ("hidden");--> statement-breakpoint
+CREATE INDEX "category_groups_sort_order_index" ON "category_groups" USING btree ("sort_order");--> statement-breakpoint
+CREATE INDEX "category_groups_tombstone_index" ON "category_groups" USING btree ("tombstone");
--- packages/loot-core/drizzle/0007_messages_crdt.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE "messages_crdt" (
-	"id" varchar(36) PRIMARY KEY NOT NULL,
-	"timestamp" text,
-	"dataset" text,
-	"row" varchar(36),
-	"column" text,
-	"value" "bytea"
-);
