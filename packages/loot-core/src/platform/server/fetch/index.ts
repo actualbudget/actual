312cb8f2a25b19d74f86e691e9c41205ff3f1e5f
Roger Goldfinger <roger@remind101.com>
2025-03-29 14:10:26 -0700

    Reload on all redirects

--- packages/loot-core/src/platform/server/fetch/index.ts
+++ packages/loot-core/src/platform/server/fetch/index.ts
@@ -2,17 +2,19 @@ import * as connection from '../connection';
 
 export const fetch = async (
   input: RequestInfo | URL,
-  options?: RequestInit,
+  options: RequestInit = {},
 ): Promise<Response> => {
+
+  // Set redirect to manual so that we can detect and respond to redirects. 
+  if (!options.redirect) options.redirect = 'manual';
+
   const response = await globalThis.fetch(input, options);
 
-  // Detect if the API query has been redirected to a different origin. This may indicate that the
-  // request has been intercepted by an authentication proxy
-  const originalUrl = new URL(input instanceof Request ? input.url : input);
-  const responseUrl = new URL(response.url);
-  if (response.redirected && responseUrl.host !== originalUrl.host) {
+  // Authentication proxies redirect when authentication has expired. In this case, 
+  // we want to fully reload and yeild control from the service worker back to the server.
+  if (response.type === 'opaqueredirect') {
     connection.send('api-fetch-redirected');
-    throw new Error(`API request redirected to ${responseUrl.host}`);
+    throw new Error(`API request redirected to unknown destination`);
   }
 
   return response;
