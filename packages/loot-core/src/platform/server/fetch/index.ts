af1c92ba3338d35b94f61dc7446d07d83e21a4b8
Roger Goldfinger <rgoldfinger@gmail.com>
2025-03-29 14:34:25 -0700

    Update index.ts

--- packages/loot-core/src/platform/server/fetch/index.ts
+++ packages/loot-core/src/platform/server/fetch/index.ts
@@ -4,17 +4,16 @@ export const fetch = async (
   input: RequestInfo | URL,
   options: RequestInit = {},
 ): Promise<Response> => {
-
-  // Set redirect to manual so that we can detect and respond to redirects. 
+  // Set redirect to manual so that we can detect and respond to redirects.
   if (!options.redirect) options.redirect = 'manual';
 
   const response = await globalThis.fetch(input, options);
 
-  // Authentication proxies redirect when authentication has expired. In this case, 
+  // Authentication proxies redirect when authentication has expired. In this case,
   // we want to fully reload and yeild control from the service worker back to the server.
   if (response.type === 'opaqueredirect') {
     connection.send('api-fetch-redirected');
-    throw new Error(`API request redirected`);
+    throw new Error(`API request redirected to unknown destination`);
   }
 
   return response;
