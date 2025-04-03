f10c8044ea72d4bbb02a183fb58ca1db4b9810e2
Alec Bakholdin <abakho@icims.com>
2025-04-02 21:40:00 -0400

    Fixed pinning issue

--- packages/loot-core/src/server/preferences/app.ts
+++ packages/loot-core/src/server/preferences/app.ts
@@ -67,33 +67,25 @@ async function saveGlobalPrefs(prefs: GlobalPrefs) {
   }
 
   if (prefs.maxMonths) {
-    await asyncStorage.setItem('max-months', '' + prefs.maxMonths);
+    asyncStorage.setItem('max-months', '' + prefs.maxMonths);
   }
-  if (prefs.documentDir) {
-    if (prefs.documentDir && (await fs.exists(prefs.documentDir))) {
-      await asyncStorage.setItem('document-dir', prefs.documentDir);
-    }
+  if (prefs.documentDir && (await fs.exists(prefs.documentDir))) {
+    asyncStorage.setItem('document-dir', prefs.documentDir);
   }
-  if (prefs.floatingSidebar) {
-    await asyncStorage.setItem('floating-sidebar', '' + prefs.floatingSidebar);
+  if (prefs.floatingSidebar !== undefined) {
+    asyncStorage.setItem('floating-sidebar', '' + prefs.floatingSidebar);
   }
   if (prefs.language) {
-    await asyncStorage.setItem('language', prefs.language);
+    asyncStorage.setItem('language', prefs.language);
   }
   if (prefs.theme) {
-    await asyncStorage.setItem('theme', prefs.theme);
+    asyncStorage.setItem('theme', prefs.theme);
   }
   if (prefs.preferredDarkTheme) {
-    await asyncStorage.setItem(
-      'preferred-dark-theme',
-      prefs.preferredDarkTheme,
-    );
+    asyncStorage.setItem('preferred-dark-theme', prefs.preferredDarkTheme);
   }
   if (prefs.serverSelfSignedCert) {
-    await asyncStorage.setItem(
-      'server-self-signed-cert',
-      prefs.serverSelfSignedCert,
-    );
+    asyncStorage.setItem('server-self-signed-cert', prefs.serverSelfSignedCert);
   }
   return 'ok';
 }
