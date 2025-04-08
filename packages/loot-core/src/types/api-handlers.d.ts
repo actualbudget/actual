395e1ea321fbd5438455b7f3036fe87bdb4642ca
Alec Bakholdin <abakho@icims.com>
2025-04-08 16:36:17 -0400

    added missing types to api-handlers.d.ts

--- packages/loot-core/src/types/api-handlers.d.ts
+++ packages/loot-core/src/types/api-handlers.d.ts
@@ -1,5 +1,7 @@
 import { ImportTransactionsOpts } from '@actual-app/api';
 
+import { IntegerAmount } from '../shared/util';
+
 import type {
   APIAccountEntity,
   APICategoryEntity,
@@ -73,16 +75,16 @@ export interface ApiHandlers {
   'api/budget-reset-hold': (arg: { month: string }) => Promise<void>;
 
   'api/transactions-export': (arg: {
-    transactions;
-    categoryGroups;
-    payees;
-    accounts;
+    transactions: TransactionEntity[];
+    categoryGroups: APICategoryGroupEntity[];
+    payees: APIPayeeEntity[];
+    accounts: APIAccountEntity[];
   }) => Promise<unknown>;
 
   'api/transactions-import': (arg: {
-    accountId;
-    transactions;
-    isPreview?;
+    accountId: APIAccountEntity['id'];
+    transactions: TransactionEntity[];
+    isPreview?: boolean;
     opts?: ImportTransactionsOpts;
   }) => Promise<{
     errors?: { message: string }[];
@@ -91,21 +93,21 @@ export interface ApiHandlers {
   }>;
 
   'api/transactions-add': (arg: {
-    accountId;
-    transactions;
+    accountId: APIAccountEntity['id'];
+    transactions: TransactionEntity[];
     runTransfers?: boolean;
     learnCategories?: boolean;
   }) => Promise<'ok'>;
 
   'api/transactions-get': (arg: {
-    accountId?: string;
+    accountId?: AccountEntity['id'];
     startDate?: string;
     endDate?: string;
   }) => Promise<TransactionEntity[]>;
 
   'api/transaction-update': (arg: {
-    id;
-    fields;
+    id: TransactionEntity['id'];
+    fields: Omit<TransactionEntity, 'id'>;
   }) => Promise<Awaited<ReturnType<typeof batchUpdateTransactions>>['updated']>;
 
   'api/transaction-delete': (arg: {
@@ -118,58 +120,76 @@ export interface ApiHandlers {
 
   'api/accounts-get': () => Promise<APIAccountEntity[]>;
 
-  'api/account-create': (arg: { account; initialBalance? }) => Promise<string>;
+  'api/account-create': (arg: {
+    account: APIAccountEntity;
+    initialBalance?: IntegerAmount;
+  }) => Promise<string>;
 
-  'api/account-update': (arg: { id; fields }) => Promise<void>;
+  'api/account-update': (arg: {
+    id: APIAccountEntity['id'];
+    fields: Omit<APIAccountEntity, 'id'>;
+  }) => Promise<void>;
 
   'api/account-close': (arg: {
-    id;
-    transferAccountId;
-    transferCategoryId;
+    id: APIAccountEntity['id'];
+    transferAccountId?: APIAccountEntity['id'];
+    transferCategoryId?: APICategoryEntity['id'];
   }) => Promise<unknown>;
 
-  'api/account-reopen': (arg: { id }) => Promise<unknown>;
+  'api/account-reopen': (arg: {
+    id: APIAccountEntity['account_id'];
+  }) => Promise<unknown>;
 
-  'api/account-delete': (arg: { id }) => Promise<unknown>;
+  'api/account-delete': (arg: {
+    id: APIAccountEntity['account_id'];
+  }) => Promise<unknown>;
 
   'api/account-balance': (arg: {
-    id: string;
+    id: APIAccountEntity['id'];
     cutoff?: Date;
   }) => Promise<number>;
 
   'api/categories-get': (arg: {
-    grouped;
+    grouped: boolean;
   }) => Promise<Array<APICategoryGroupEntity | APICategoryEntity>>;
 
   'api/category-groups-get': () => Promise<APICategoryGroupEntity[]>;
 
-  'api/category-group-create': (arg: { group }) => Promise<string>;
+  'api/category-group-create': (arg: {
+    group: APICategoryGroupEntity['id'];
+  }) => Promise<string>;
 
   'api/category-group-update': (arg: { id; fields }) => Promise<unknown>;
 
   'api/category-group-delete': (arg: {
-    id;
-    transferCategoryId;
+    id: APICategoryEntity['id'];
+    transferCategoryId?: APICategoryEntity['id'];
   }) => Promise<unknown>;
 
-  'api/category-create': (arg: { category }) => Promise<string>;
+  'api/category-create': (arg: { category: CategoryEntity }) => Promise<string>;
 
-  'api/category-update': (arg: { id; fields }) => Promise<unknown>;
+  'api/category-update': (arg: {
+    id: APICategoryEntity['id'];
+    fields: Omit<CategoryEntity, 'id'>;
+  }) => Promise<unknown>;
 
   'api/category-delete': (arg: {
-    id;
-    transferCategoryId?;
+    id: APICategoryEntity['id'];
+    transferCategoryId?: APICategoryEntity['id'];
   }) => Promise<{ error?: string }>;
 
   'api/payees-get': () => Promise<APIPayeeEntity[]>;
 
   'api/common-payees-get': () => Promise<APIPayeeEntity[]>;
 
-  'api/payee-create': (arg: { payee }) => Promise<string>;
+  'api/payee-create': (arg: { payee: APIPayeeEntity }) => Promise<string>;
 
-  'api/payee-update': (arg: { id; fields }) => Promise<unknown>;
+  'api/payee-update': (arg: {
+    id: APIPayeeEntity['id'];
+    fields: Omit<APIPayeeEntity, 'id'>;
+  }) => Promise<unknown>;
 
-  'api/payee-delete': (arg: { id }) => Promise<unknown>;
+  'api/payee-delete': (arg: { id: APIPayeeEntity['id'] }) => Promise<unknown>;
 
   'api/payees-merge': (arg: {
     targetId: string;
