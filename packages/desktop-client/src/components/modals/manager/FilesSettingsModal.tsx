b032a069939da6c1b7040c3bfd15cb2dd4343555
Joel Jeremy Marquez <joeljeremy.marquez@gmail.com>
2025-04-15 15:52:11 -0700

    Fix lint error

--- packages/desktop-client/src/components/modals/manager/FilesSettingsModal.tsx
+++ packages/desktop-client/src/components/modals/manager/FilesSettingsModal.tsx
@@ -8,7 +8,6 @@ import { Text } from '@actual-app/components/text';
 import { theme } from '@actual-app/components/theme';
 import { View } from '@actual-app/components/view';
 
-
 import { loadAllFiles } from '../../../budgets/budgetsSlice';
 import { pushModal } from '../../../modals/modalsSlice';
 import { useDispatch } from '../../../redux';
