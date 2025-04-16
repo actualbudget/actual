e8df6d8c7c1ff8c10c172ff323a2f91a6da7b6cc
Julian Dominguez-Schatz <julian.dominguezschatz@gmail.com>
2025-04-15 22:47:06 -0400

    Move Account EmptyMessage to a separate file

--- packages/desktop-client/src/components/accounts/Account.tsx
+++ packages/desktop-client/src/components/accounts/Account.tsx
@@ -9,9 +9,7 @@ import React, {
 import { Trans } from 'react-i18next';
 import { Navigate, useParams, useLocation } from 'react-router-dom';
 
-import { Button } from '@actual-app/components/button';
 import { styles } from '@actual-app/components/styles';
-import { Text } from '@actual-app/components/text';
 import { theme } from '@actual-app/components/theme';
 import { View } from '@actual-app/components/view';
 import { debounce } from 'debounce';
@@ -91,6 +89,7 @@ import { type SavedFilter } from '../filters/SavedFilterMenuButton';
 import { TransactionList } from '../transactions/TransactionList';
 import { validateAccountName } from '../util/accountValidation';
 
+import { AccountEmptyMessage } from './AccountEmptyMessage';
 import { AccountHeader } from './Header';
 
 type ConditionEntity = Partial<RuleConditionEntity> | TransactionFilterEntity;
@@ -99,57 +98,6 @@ function isTransactionFilterEntity(
   filter: ConditionEntity,
 ): filter is TransactionFilterEntity {
   return 'id' in filter;
-}
-
-type EmptyMessageProps = {
-  onAdd: () => void;
-};
-
-function EmptyMessage({ onAdd }: EmptyMessageProps) {
-  return (
-    <View
-      style={{
-        color: theme.tableText,
-        backgroundColor: theme.tableBackground,
-        flex: 1,
-        alignItems: 'center',
-        borderTopWidth: 1,
-        borderColor: theme.tableBorder,
-      }}
-    >
-      <View
-        style={{
-          width: 550,
-          marginTop: 75,
-          fontSize: 15,
-          alignItems: 'center',
-        }}
-      >
-        <Text style={{ textAlign: 'center', lineHeight: '1.4em' }}>
-          <Trans>
-            For Actual to be useful, you need to <strong>add an account</strong>
-            . You can link an account to automatically download transactions, or
-            manage it locally yourself.
-          </Trans>
-        </Text>
-
-        <Button
-          variant="primary"
-          style={{ marginTop: 20 }}
-          autoFocus
-          onPress={onAdd}
-        >
-          <Trans>Add account</Trans>
-        </Button>
-
-        <View
-          style={{ marginTop: 20, fontSize: 13, color: theme.tableTextLight }}
-        >
-          <Trans>In the future, you can add accounts from the sidebar.</Trans>
-        </View>
-      </View>
-    </View>
-  );
 }
 
 type AllTransactionsProps = {
@@ -1868,7 +1816,7 @@ class AccountInternal extends PureComponent<
                   hideFraction={hideFraction}
                   renderEmpty={() =>
                     showEmptyMessage ? (
-                      <EmptyMessage
+                      <AccountEmptyMessage
                         onAdd={() =>
                           this.props.dispatch(
                             replaceModal({
