c8e63bce8e82f6666f921a24781d57101e9a1e23
youngcw <calebyoung94@gmail.com>
2025-04-26 07:11:11 -0700

    lint

--- packages/loot-core/src/server/budget/categoryTemplate.test.ts
+++ packages/loot-core/src/server/budget/categoryTemplate.test.ts
@@ -20,6 +20,7 @@ vi.mock('../db', () => ({
 
 // Test helper class to access constructor and methods
 class TestCategoryTemplate extends CategoryTemplate {
+  // eslint-disable-next-line @typescript-eslint/no-useless-constructor
   constructor(
     templates: Template[],
     category: CategoryEntity,
