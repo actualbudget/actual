0e3076052ca33719c28c04f5991c01b3cffaf4fb
misu-dev <misu1807@googlemail.com>
2025-05-03 22:29:41 +0200

    feat: update cash flow report

--- packages/desktop-client/src/components/reports/graphs/CashFlowGraph.tsx
+++ packages/desktop-client/src/components/reports/graphs/CashFlowGraph.tsx
@@ -18,25 +18,22 @@ import {
   type TooltipProps,
 } from 'recharts';
 
-import {
-  amountToCurrency,
-  amountToCurrencyNoDecimal,
-} from 'loot-core/shared/util';
-
 import { chartTheme } from '../chart-theme';
 import { Container } from '../Container';
 
 import { useLocale } from '@desktop-client/hooks/useLocale';
 import { usePrivacyMode } from '@desktop-client/hooks/usePrivacyMode';
+import { FormatType, useFormat } from '@desktop-client/components/spreadsheet/useFormat';
 
 const MAX_BAR_SIZE = 50;
 const ANIMATION_DURATION = 1000; // in ms
 
 type CustomTooltipProps = TooltipProps<number, 'date'> & {
   isConcise: boolean;
+  format: (value: unknown, type: FormatType) => string;
 };
 
-function CustomTooltip({ active, payload, isConcise }: CustomTooltipProps) {
+function CustomTooltip({ active, payload, isConcise, format }: CustomTooltipProps) {
   const locale = useLocale();
   const { t } = useTranslation();
 
@@ -68,27 +65,27 @@ function CustomTooltip({ active, payload, isConcise }: CustomTooltipProps) {
         <div style={{ lineHeight: 1.5 }}>
           <AlignedText
             left={t('Income:')}
-            right={amountToCurrency(data.income)}
+            right={format(data.income, 'financial')}
           />
           <AlignedText
             left={t('Expenses:')}
-            right={amountToCurrency(data.expenses)}
+            right={format(data.expenses, 'financial')}
           />
           <AlignedText
             left={t('Change:')}
             right={
-              <strong>{amountToCurrency(data.income + data.expenses)}</strong>
+              <strong>{format(data.income + data.expenses, 'financial')}</strong>
             }
           />
           {data.transfers !== 0 && (
             <AlignedText
               left={t('Transfers:')}
-              right={amountToCurrency(data.transfers)}
+              right={format(data.transfers, 'financial')}
             />
           )}
           <AlignedText
             left={t('Balance:')}
-            right={amountToCurrency(data.balance)}
+            right={format(data.balance, 'financial')}
           />
         </div>
       </div>
@@ -116,6 +113,7 @@ export function CashFlowGraph({
   const locale = useLocale();
   const privacyMode = usePrivacyMode();
   const [yAxisIsHovered, setYAxisIsHovered] = useState(false);
+  const format = useFormat();
 
   const data = graphData.expenses.map((row, idx) => ({
     date: row.x,
@@ -134,6 +132,11 @@ export function CashFlowGraph({
             height={height}
             stackOffset="sign"
             data={data}
+            margin={{ 
+              left: computePadding(
+                data.map(({ balance }) => ({ y: balance })),
+                (data) => format(data, 'financial')), // left padding for Y-axis
+            }}
           >
             <CartesianGrid strokeDasharray="3 3" vertical={false} />
             <XAxis
@@ -153,7 +156,7 @@ export function CashFlowGraph({
               tickFormatter={value =>
                 privacyMode && !yAxisIsHovered
                   ? '...'
-                  : amountToCurrencyNoDecimal(value)
+                  : format(value, 'financial')
               }
               onMouseEnter={() => setYAxisIsHovered(true)}
               onMouseLeave={() => setYAxisIsHovered(false)}
@@ -165,7 +168,7 @@ export function CashFlowGraph({
                   locale,
                 });
               }}
-              content={<CustomTooltip isConcise={isConcise} />}
+              content={<CustomTooltip isConcise={isConcise} format={format} />}
               isAnimationActive={false}
             />
 
@@ -198,4 +201,13 @@ export function CashFlowGraph({
       )}
     </Container>
   );
+}
+
+function computePadding(data: Array<{ y: number }>, formatter: (value: number) => string) {
+  const maxLength = Math.max(
+    ...data.map(({ y }) => {
+      return formatter(Math.round(y)).length;
+    }),
+  );
+  return Math.max(0, (maxLength - 5) * 5);
 }
