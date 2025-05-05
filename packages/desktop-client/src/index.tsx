d167fe218ca82124ea2f4a09b350fe06f91f5d86
Joel Jeremy Marquez <joeljeremy.marquez@gmail.com>
2025-05-05 10:23:27 -0700

    Fix lint error

--- packages/desktop-client/src/index.tsx
+++ packages/desktop-client/src/index.tsx
@@ -20,7 +20,6 @@ import * as accountsSlice from './accounts/accountsSlice';
 import * as appSlice from './app/appSlice';
 import { AuthProvider } from './auth/AuthProvider';
 import * as budgetsSlice from './budgets/budgetsSlice';
-import * as modalsSlice from './modals/modalsSlice';
 // See https://github.com/WICG/focus-visible. Only makes the blue
 // focus outline appear from keyboard events.
 import 'focus-visible';
