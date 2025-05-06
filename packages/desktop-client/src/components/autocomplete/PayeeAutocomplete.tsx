8518ab10ec7843e91b21ac64964f0b023fcb3890
Joel Jeremy Marquez <joeljeremy.marquez@gmail.com>
2025-05-05 22:55:45 -0700

    Fix lint error

--- packages/desktop-client/src/components/autocomplete/PayeeAutocomplete.tsx
+++ packages/desktop-client/src/components/autocomplete/PayeeAutocomplete.tsx
@@ -323,11 +323,7 @@ export function PayeeAutocomplete({
   const [rawPayee, setRawPayee] = useState('');
   const hasPayeeInput = !!rawPayee;
   const payeeSuggestions: PayeeAutocompleteItem[] = useMemo(() => {
-    const suggestions = getPayeeSuggestions(
-      nearbyPayees,
-      commonPayees,
-      payees,
-    );
+    const suggestions = getPayeeSuggestions(nearbyPayees, commonPayees, payees);
     const filteredSuggestions = filterActivePayees(
       suggestions,
       focusTransferPayees,
