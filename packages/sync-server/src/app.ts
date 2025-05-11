1b76d1fd7831fb08057e56e3b345e1ce304794ac
Mike Clark <mclarkgb@gmail.com>
2025-05-11 20:48:47 +0100

    release notes

--- packages/sync-server/src/app.ts
+++ packages/sync-server/src/app.ts
@@ -1,7 +1,5 @@
 import { createRequire } from 'module';
 import fs, { readFileSync } from 'node:fs';
-import { dirname, resolve } from 'node:path';
-import { fileURLToPath } from 'node:url';
 
 import bodyParser from 'body-parser';
 import cors from 'cors';
