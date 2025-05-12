553efebdf92bed54e7c10030e84da6e448f3dc1d
Joel Jeremy Marquez <joeljeremy.marquez@gmail.com>
2025-05-12 11:01:01 -0700

    Fix typecheck error

--- packages/desktop-client/src/queries/pagedQuery.test.ts
+++ packages/desktop-client/src/queries/pagedQuery.test.ts
@@ -8,6 +8,7 @@ import { subDays } from 'loot-core/shared/months';
 import { q } from 'loot-core/shared/query';
 import { resetTracer, tracer } from 'loot-core/shared/test-helpers';
 
+import { aqlQuery } from './aqlQuery';
 import { pagedQuery } from './pagedQuery';
 
 function wait(n) {
