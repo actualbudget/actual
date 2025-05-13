0431039eb6e22c38dce6144ce00baa1c71cb866e
Alec Bakholdin <43560338+alecbakholdin@users.noreply.github.com>
2025-05-13 10:33:05 -0400

    Inconsistent date update mobile new transaction (#4910)
    * converted onUpdate to onChange for better mobile ux
    
    * release notes
    
    ---------
    
    Co-authored-by: Alec Bakholdin <alecbakholdin@Alecs-Mac.local>

--- packages/desktop-client/src/components/mobile/transactions/TransactionEdit.jsx
+++ packages/desktop-client/src/components/mobile/transactions/TransactionEdit.jsx
@@ -992,11 +992,11 @@ const TransactionEditInner = memo(function TransactionEditInner({
               onFocus={() =>
                 onRequestActiveEdit(getFieldName(transaction.id, 'date'))
               }
-              onUpdate={value =>
+              onChange={event =>
                 onUpdateInner(
                   transaction,
                   'date',
-                  formatDate(parseISO(value), dateFormat),
+                  formatDate(parseISO(event.target.value), dateFormat),
                 )
               }
             />
@@ -1029,7 +1029,9 @@ const TransactionEditInner = memo(function TransactionEditInner({
             onFocus={() => {
               onRequestActiveEdit(getFieldName(transaction.id, 'notes'));
             }}
-            onUpdate={value => onUpdateInner(transaction, 'notes', value)}
+            onChange={event =>
+              onUpdateInner(transaction, 'notes', event.target.value)
+            }
           />
         </View>
 
