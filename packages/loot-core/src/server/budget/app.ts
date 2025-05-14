7652aac932ea6bab04ebc1aa1a6d2cb537c0328a
Joel Jeremy Marquez <joeljeremy.marquez@gmail.com>
2025-05-14 10:26:57 -0700

    Update imports

--- packages/loot-core/src/server/budget/app.ts
+++ packages/loot-core/src/server/budget/app.ts
@@ -15,7 +15,7 @@ import { undoable } from '../undo';
 import * as actions from './actions';
 import * as budget from './base';
 import * as cleanupActions from './cleanup-template';
-import * as goalActions from './goal-templates';
+import * as goalActions from './goal-template';
 
 export interface BudgetHandlers {
   'budget/budget-amount': typeof actions.setBudget;
