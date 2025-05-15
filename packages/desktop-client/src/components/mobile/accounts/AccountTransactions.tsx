fc2baa2b98473a68b3286884801f900bcd5505ba
youngcw <calebyoung94@gmail.com>
2025-05-15 11:09:41 -0700

    lint

--- packages/desktop-client/src/components/mobile/accounts/AccountTransactions.tsx
+++ packages/desktop-client/src/components/mobile/accounts/AccountTransactions.tsx
@@ -7,7 +7,6 @@ import React, {
 } from 'react';
 import { useTranslation } from 'react-i18next';
 
-import { getAccountBalance } from '@actual-app/api';
 import { Button } from '@actual-app/components/button';
 import { styles } from '@actual-app/components/styles';
 import { Text } from '@actual-app/components/text';
