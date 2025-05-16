d64737c4daea8219e4f6828c7f6d2f14792e844c
Joel Jeremy Marquez <joeljeremy.marquez@gmail.com>
2025-05-16 13:58:32 -0700

    Fix lint error

--- packages/desktop-client/src/components/budget/envelope/CoverMenu.tsx
+++ packages/desktop-client/src/components/budget/envelope/CoverMenu.tsx
@@ -1,4 +1,4 @@
-import React, { type Ref, useMemo, useState } from 'react';
+import React, { useMemo, useState } from 'react';
 import { Trans, useTranslation } from 'react-i18next';
 
 import { Button } from '@actual-app/components/button';
