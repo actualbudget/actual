6004a0d3737577d0151f246808113533efc8cc00
pogman-code <adrian.maurin@gmail.com>
2025-05-19 18:12:13 +0200

    add custom tag colors through settings

--- packages/desktop-client/src/components/settings/Tags.tsx
+++ packages/desktop-client/src/components/settings/Tags.tsx
@@ -1,12 +1,18 @@
-import React, { type ReactNode } from 'react';
+import React, { useState } from 'react';
 import { Trans } from 'react-i18next';
 
 import { Button } from '@actual-app/components/button';
+import { FormError } from '@actual-app/components/form-error';
+import { useResponsive } from '@actual-app/components/hooks/useResponsive';
+import { SvgAdd } from '@actual-app/components/icons/v0';
 import { SvgTrash } from '@actual-app/components/icons/v1';
+import { Input } from '@actual-app/components/input';
+import { styles } from '@actual-app/components/styles';
 import { Text } from '@actual-app/components/text';
 import { theme } from '@actual-app/components/theme';
 import { tokens } from '@actual-app/components/tokens';
 import { View } from '@actual-app/components/view';
+import { t } from 'i18next';
 
 import { useSidebar } from '../sidebar/SidebarProvider';
 
@@ -18,10 +24,26 @@ export function TagsSettings() {
   const sidebar = useSidebar();
   const [tags, setTagsPref] = useTags();
   const getTagCSS = useTagCSS();
+  const [newTag, setNewTag] = useState('');
+  const [newColor, setNewColor] = useState('');
+  const [errorMsg, setErrorMsg] = useState('');
+  const { isNarrowWidth } = useResponsive();
 
   const onTrashTag = (tag: string) => {
     const { [tag]: _, ...newTags } = tags;
     setTagsPref(newTags);
+  };
+
+  const onAddTagColor = () => {
+    if (!newColor.match(/^#(?:[0-9a-f]{3}){1,2}$/i)) {
+      setErrorMsg(t('Invalid HEX color'));
+      return;
+    }
+
+    setErrorMsg('');
+    setTagsPref({ ...tags, [newTag]: newColor });
+    setNewTag('');
+    setNewColor('');
   };
 
   return (
@@ -41,6 +63,47 @@ export function TagsSettings() {
             },
           }}
         >
+          {errorMsg && (
+            <FormError
+              style={{
+                paddingTop: 5,
+                marginLeft: styles.mobileEditingPadding,
+                marginRight: styles.mobileEditingPadding,
+              }}
+            >
+              * {errorMsg}
+            </FormError>
+          )}
+          <div
+            style={{
+              display: 'flex',
+              flexDirection: isNarrowWidth ? 'column' : 'row',
+              gap: '1em',
+            }}
+          >
+            <Input
+              id="tag-field"
+              placeholder="tag"
+              value={newTag}
+              onChangeValue={setNewTag}
+            />
+            <Input
+              id="colo-field"
+              placeholder="#hexcolor"
+              value={newColor}
+              onChangeValue={setNewColor}
+            />
+            <Button
+              variant="bare"
+              onPress={() => onAddTagColor()}
+              style={{
+                borderWidth: 0,
+                backgroundColor: 'transparent',
+              }}
+            >
+              <SvgAdd width={10} height={10} />
+            </Button>
+          </div>
           <span>
             <Button variant="bare" className={getTagCSS('')} onPress={() => {}}>
               #Default
