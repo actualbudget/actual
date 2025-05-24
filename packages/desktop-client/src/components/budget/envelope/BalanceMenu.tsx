9c811b7dec2c7865d269b25dd005e6f4705d05c6
youngcw <calebyoung94@gmail.com>
2025-05-23 17:38:39 -0700

    types

--- packages/desktop-client/src/components/budget/envelope/BalanceMenu.tsx
+++ packages/desktop-client/src/components/budget/envelope/BalanceMenu.tsx
@@ -12,9 +12,9 @@ type BalanceMenuProps = Omit<
   'onMenuSelect' | 'items'
 > & {
   categoryId: string;
-  onTransfer: () => void;
-  onCarryover: (carryOver: boolean) => void;
-  onCover: () => void;
+  onTransfer: () => void | undefined;
+  onCarryover: (carryOver: boolean) => void | undefined;
+  onCover: () => void | undefined;
 };
 
 export function BalanceMenu({
