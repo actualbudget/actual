02ef5c55c1d087eed0c08f78e92553319352eda8
Joel Jeremy Marquez <joeljeremy.marquez@gmail.com>
2025-06-17 14:58:30 -0700

    Fix lint

--- packages/desktop-client/src/hooks/useAccountPreviewTransactions.ts
+++ packages/desktop-client/src/hooks/useAccountPreviewTransactions.ts
@@ -68,7 +68,11 @@ export function useAccountPreviewTransactions({
   const accountBalanceValue = useSheetValue<
     'account',
     'balance' | 'accounts-balance'
-  >(accountId ? bindings.accountBalance(accountId) : bindings.allAccountBalance());
+  >(
+    accountId
+      ? bindings.accountBalance(accountId)
+      : bindings.allAccountBalance(),
+  );
 
   const {
     previewTransactions: allPreviewTransactions,
