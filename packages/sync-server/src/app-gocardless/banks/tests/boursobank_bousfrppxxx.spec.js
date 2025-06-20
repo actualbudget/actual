d73ead135e71bae0665331c3d4c1419d10b01f42
Guillaume Taquet Gasperini <9089317+Aerion@users.noreply.github.com>
2025-06-21 01:48:22 +0200

    Improve Boursobank Gocardless transaction parsing (#5202)
    * Fix Boursobank transfer parsing
    
    As per the comments on https://github.com/actualbudget/actual/pull/4958#issuecomment-2988814739,
    the Boursobank transfer parsing was not working correctly.
    
    Indeed, the array returned by GoCardless for transfers is randomly
    ordered. So we cannot rely on the first line to know its type.
    
    To work around this, the code gets a bit more complex by:
    - Checking the first line for known types (card, loan, atm withdrawal)
      and handling them accordingly.
    - If it's not one of these, we iterate through all the lines by checking
      if the array contains a line with the transfer type.
    
    * Add credit note type for BoursoBank
    
    An `Avoir` is a refund made to the credit card.
    Adds the proper payee / notes parsing for it.
    
    * Improve Boursobank card transaction parsing
    
    Some Boursobank transactions have an unknown number attached to the
    payee name. Remove it from the payee name to ensure consistency across
    transactions.
    
    For instance, `CARTE 19/03/25 Github 4 CB*0494` (notice the `4`).
    
    * Improve Boursobank payee name backslashes handling
    
    After more testing, I found that the backslashes present are for the
    pending transactions, and indicating the localization of the payment.
    
    To keep the payee name consistent, remove what follows the backslashes.
    
    For instance `PICARD SA 1234\\PARIS\\ FR` is for a pending transaction
    whereas `PICARD SA 1234` is what we receive for a completed one.
    
    * Set notes with date for Boursobank card transactions
    
    @mistyque requested to see the data of the card transaction in this
    comment: https://github.com/actualbudget/actual/pull/4958#issuecomment-2981459622
    
    * Add 5202 release note
    
    * [autofix.ci] apply automated fixes
    
    * refactor loops and match
    
    ---------
    
    Co-authored-by: autofix-ci[bot] <114827586+autofix-ci[bot]@users.noreply.github.com>
    Co-authored-by: Matt Fiddaman <github@m.fiddaman.uk>

--- packages/sync-server/src/app-gocardless/banks/tests/boursobank_bousfrppxxx.spec.js
+++ packages/sync-server/src/app-gocardless/banks/tests/boursobank_bousfrppxxx.spec.js
@@ -3,21 +3,26 @@ import BoursoBank from '../boursobank_bousfrppxxx.js';
 describe('BoursoBank', () => {
   describe('#normalizeTransaction', () => {
     it.each([
-      [['CARTE 01/03/25 PAYEE NAME CB*4567'], 'Payee Name', ''],
-      [['CARTE 01/03/25 PAYEE NAME'], 'Payee Name', ''],
-      [['CARTE 01/03/25 PAYEE NAME 7428347'], 'Payee Name', ''],
+      [['CARTE 01/03/25 PAYEE NAME CB*4567'], 'Payee Name', 'Carte 01/03/25'],
+      [
+        ['CARTE 01/03/25 PAYEE NAME 713621 CB*4567'],
+        'Payee Name',
+        'Carte 01/03/25',
+      ],
+      [['CARTE 01/03/25 PAYEE NAME'], 'Payee Name', 'Carte 01/03/25'],
+      [['CARTE 01/03/25 PAYEE NAME 7428347'], 'Payee Name', 'Carte 01/03/25'],
       [
         [
           'CARTE 03/02/25 PAYEE NAME CB*1234',
           '2,80 NZD / 1 euro = 1,818181818',
         ],
         'Payee Name',
-        '2,80 NZD / 1 euro = 1,818181818',
+        'Carte 03/02/25 2,80 NZD / 1 euro = 1,818181818',
       ],
       [
         ['RETRAIT DAB 01/03/25 My location CB*9876'],
         'Retrait DAB',
-        'My location',
+        'Retrait 01/03/25 My location',
       ],
       [
         [
@@ -25,10 +30,15 @@ describe('BoursoBank', () => {
           '2,80 NZD / 1 euro = 1,818181818',
         ],
         'Retrait DAB',
-        'My location 2,80 NZD / 1 euro = 1,818181818',
+        'Retrait 01/03/25 My location 2,80 NZD / 1 euro = 1,818181818',
       ],
       [
         ['VIR Text put by the sender', 'PAYEE NAME'],
+        'Payee Name',
+        'Text put by the sender',
+      ],
+      [
+        ['PAYEE NAME', 'VIR Text put by the sender'],
         'Payee Name',
         'Text put by the sender',
       ],
@@ -45,6 +55,15 @@ describe('BoursoBank', () => {
         [
           'VIR Text put by the sender',
           'Réf : SOME TEXT PUT BY THE BANK',
+          'PAYEE NAME',
+        ],
+        'Payee Name',
+        'Text put by the sender',
+      ],
+      [
+        [
+          'Réf : SOME TEXT PUT BY THE BANK',
+          'VIR Text put by the sender',
           'PAYEE NAME',
         ],
         'Payee Name',
@@ -56,6 +75,17 @@ describe('BoursoBank', () => {
         'Text put by the sender',
       ],
       [
+        ['Text put by the sender', 'VIR INST PAYEE NAME'],
+        'Payee Name',
+        'Text put by the sender',
+      ],
+      [['VIR INST PAYEE NAME'], 'Payee Name', ''],
+      [
+        ['PAYEE NAME', 'VIR Text put by the sender'],
+        'Payee Name',
+        'Text put by the sender',
+      ],
+      [
         [
           'VIR SEPA PAYEE NAME',
           'SOME TEXT',
@@ -63,7 +93,17 @@ describe('BoursoBank', () => {
           'YET ANOTHER TEXT',
         ],
         'Payee Name',
-        '',
+        'SOME TEXT ANOTHER TEXT YET ANOTHER TEXT',
+      ],
+      [
+        [
+          'SOME TEXT',
+          'ANOTHER TEXT',
+          'VIR SEPA PAYEE NAME',
+          'YET ANOTHER TEXT',
+        ],
+        'Payee Name',
+        'SOME TEXT ANOTHER TEXT YET ANOTHER TEXT',
       ],
       [
         [
@@ -74,15 +114,28 @@ describe('BoursoBank', () => {
           'PRELEVEMENT FOO BAR BAZ du',
         ],
         'Payee Name',
-        '',
+        'HERE IS SOMETHING SOME OTHER TEXT 30/04/2025 PRELEVEMENT FOO BAR BAZ du',
+      ],
+      [
+        [
+          '30/05/2025',
+          'SOME.TEXT.123.456',
+          'PRELEVEMENT FOO BAR BAZ du',
+          'PRLV SEPA Payee Name',
+          'ABC 1934821371',
+        ],
+        'Payee Name',
+        '30/05/2025 SOME.TEXT.123.456 PRELEVEMENT FOO BAR BAZ du ABC 1934821371',
       ],
       [
         ['ECH PRET:1823918329832913'],
         'Prêt bancaire',
         'ECH PRET:1823918329832913',
       ],
-      [['PAYEE NAME'], 'Payee Name', ''],
-      [['PAYEE\\NAME\\ BIS'], 'Payee Name Bis', ''],
+      [['PAYEE NAME 411'], 'Payee Name', ''],
+      [['PAYEE NAME\\PARIS\\ FR'], 'Payee Name', ''],
+      [['PAYEE NAME 1\\PARIS\\ FR'], 'Payee Name', ''],
+      [['AVOIR 17/06/25 PAYEE NAME CB*1234'], 'Payee Name', 'Avoir 17/06/25'],
     ])(
       'normalizes transaction with %s',
       (
