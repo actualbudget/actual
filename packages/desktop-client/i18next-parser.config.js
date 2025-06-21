7d3ffdac6ea776d19580e87db109389c8661b45b
Julian Dominguez-Schatz <julian.dominguezschatz@gmail.com>
2025-06-21 01:39:37 -0400

    Switch source string language from EN to 'untranslated'

--- packages/desktop-client/i18next-parser.config.js
+++ packages/desktop-client/i18next-parser.config.js
@@ -1,12 +1,12 @@
 module.exports = {
   input: ['src/**/*.{js,jsx,ts,tsx}', '../loot-core/src/**/*.{js,jsx,ts,tsx}'],
   output: 'locale/$LOCALE.json',
-  locales: ['en'],
+  locales: ['untranslated'],
   sort: true,
   keySeparator: false,
   namespaceSeparator: false,
   defaultValue: (locale, ns, key, value) => {
-    if (locale === 'en') {
+    if (locale === 'untranslated') {
       return value || key;
     }
     return '';
