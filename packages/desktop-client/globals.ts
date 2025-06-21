b7cf5d56115eaebaa80ec934a5ed3b42ef0bf38d
Julian Dominguez-Schatz <julian.dominguezschatz@gmail.com>
2025-06-21 00:28:50 -0400

    Fix remaining type errors

--- packages/desktop-client/globals.ts
+++ packages/desktop-client/globals.ts
@@ -1,17 +1,14 @@
-import { type CSSObject } from '@emotion/css/dist/declarations/src/create-instance';
+import '@playwright/test';
 
 // Allow images to be imported
 declare module '*.png';
 
-declare module 'react' {
-  // eslint-disable-next-line @typescript-eslint/no-empty-object-type
-  interface CSSProperties extends CSSObject {}
-}
-
 declare global {
   function __resetWorld(): void;
 
+  // eslint-disable-next-line @typescript-eslint/no-namespace
   namespace PlaywrightTest {
+    // eslint-disable-next-line @typescript-eslint/consistent-type-definitions
     interface Matchers<R> {
       toMatchThemeScreenshots(): Promise<R>;
     }
--- packages/loot-core/src/platform/server/asyncStorage/index-types.ts
+++ packages/loot-core/src/platform/server/asyncStorage/index-types.ts
@@ -1,35 +1,35 @@
-import { GlobalPrefs, GlobalPrefsJson } from '../../../types/prefs';
+import type { GlobalPrefsJson } from '../../../types/prefs';
 
-export function init(opts?: { persist?: boolean }): void;
+export declare function init(opts?: { persist?: boolean }): void;
 export type Init = typeof init;
 
-type InferType<K extends keyof GlobalPrefsJson> = GlobalPrefs[K];
-
-export function getItem<K extends keyof GlobalPrefsJson>(
+export declare function getItem<K extends keyof GlobalPrefsJson>(
   key: K,
 ): Promise<GlobalPrefsJson[K]>;
 export type GetItem = typeof getItem;
 
-export function setItem<K extends keyof GlobalPrefsJson>(
+export declare function setItem<K extends keyof GlobalPrefsJson>(
   key: K,
   value: GlobalPrefsJson[K],
 ): Promise<void>;
 export type SetItem = typeof setItem;
 
-export function removeItem(key: keyof GlobalPrefsJson): Promise<void>;
+export declare function removeItem(key: keyof GlobalPrefsJson): Promise<void>;
 export type RemoveItem = typeof removeItem;
 
-export function multiGet<K extends readonly (keyof GlobalPrefsJson)[]>(
+export declare function multiGet<K extends readonly (keyof GlobalPrefsJson)[]>(
   keys: K,
 ): Promise<{ [P in K[number]]: GlobalPrefsJson[P] }>;
 
 export type MultiGet = typeof multiGet;
 
-export function multiSet<K extends keyof GlobalPrefsJson>(
+export declare function multiSet<K extends keyof GlobalPrefsJson>(
   keyValues: Array<[K, GlobalPrefsJson[K]]>,
 ): Promise<void>;
 
 export type MultiSet = typeof multiSet;
 
-export function multiRemove(keys: (keyof GlobalPrefsJson)[]): Promise<void>;
+export declare function multiRemove(
+  keys: (keyof GlobalPrefsJson)[],
+): Promise<void>;
 export type MultiRemove = typeof multiRemove;
