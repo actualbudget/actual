1175021c4a039dd404f9f3cd1098b7954fa37b84
Mike Clark <mclarkgb@gmail.com>
2025-07-04 19:38:56 +0100

    adding our discord to the help menu so users can find community support

--- packages/desktop-client/src/components/HelpMenu.tsx
+++ packages/desktop-client/src/components/HelpMenu.tsx
@@ -38,7 +38,11 @@ function openDocsForCurrentPage() {
   window.Actual.openURLInBrowser(getPageDocs(window.location.pathname));
 }
 
-type HelpMenuItem = 'docs' | 'keyboard-shortcuts' | 'goal-templates';
+type HelpMenuItem =
+  | 'docs'
+  | 'discord'
+  | 'keyboard-shortcuts'
+  | 'goal-templates';
 
 type HelpButtonProps = {
   onPress?: () => void;
@@ -81,6 +85,10 @@ export const HelpMenu = () => {
       case 'docs':
         openDocsForCurrentPage();
         break;
+      case 'discord':
+        window.Actual.openURLInBrowser('https://discord.gg/pRYNYr4W5A');
+        // dispatch(pushModal({ modal: { name: '' } }));
+        break;
       case 'keyboard-shortcuts':
         dispatch(pushModal({ modal: { name: 'keyboard-shortcuts' } }));
         break;
@@ -112,6 +120,10 @@ export const HelpMenu = () => {
             {
               name: 'docs',
               text: t('Documentation'),
+            },
+            {
+              name: 'discord',
+              text: t('Discord community support'),
             },
             { name: 'keyboard-shortcuts', text: t('Keyboard shortcuts') },
             ...(showGoalTemplates && page === '/budget'
