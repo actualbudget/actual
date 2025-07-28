ed184fd6363ed1e2b5d8b06f6a063dba076683d1
Mike Clark <mclarkgb@gmail.com>
2025-07-28 22:58:31 +0100

    close but no cigar

--- packages/loot-core/src/server/polyfills.ts
+++ packages/loot-core/src/server/polyfills.ts
@@ -1,30 +1,12 @@
-// By default, no polyfills are installed
-
 // Polyfills for browser/web worker environment
-// These match what webpack's ProvidePlugin was providing
-
-import { Buffer } from 'buffer';
-import process from 'process';
-
 import * as jspb from 'google-protobuf';
-// Import process for browser environment using the alias
-// @ts-ignore - process doesn't have proper TypeScript exports
 
-// Make Buffer and process available globally
 if (typeof globalThis !== 'undefined') {
-  globalThis.Buffer = Buffer;
-  globalThis.process = process;
-
   // Add a basic require polyfill for CommonJS modules
   if (typeof globalThis.require === 'undefined') {
     // @ts-ignore - we're creating a minimal require implementation
     globalThis.require = (moduleId: string) => {
       switch (moduleId) {
-        case 'buffer':
-          return { Buffer };
-        case 'process':
-        case 'process/browser':
-          return process;
         case 'google-protobuf':
           return jspb;
         default:
@@ -38,9 +20,6 @@ if (typeof globalThis !== 'undefined') {
 
 // Also set on global for compatibility
 if (typeof global !== 'undefined') {
-  global.Buffer = Buffer;
-  global.process = process;
-
   if (typeof global.require === 'undefined') {
     // @ts-ignore - assigning minimal require implementation to global
     global.require = globalThis.require;
