e2177fed1f3ad8653aaed968d28266009f729627
pogman-code <adrian.maurin@gmail.com>
2025-08-01 17:59:29 +0200

    Set default tag color in first position

--- packages/component-library/src/ColorPicker.tsx
+++ packages/component-library/src/ColorPicker.tsx
@@ -34,10 +34,10 @@ function ColorSwatch(props: ColorSwatchProps) {
 
 // colors from https://materialui.co/colors
 const colorsets = [
-  ['#D32F2F', '#C2185B', '#7B1FA2', '#512DA8', '#303F9F'],
-  ['#1976D2', '#0288D1', '#0097A7', '#00796B', '#388E3C'],
-  ['#689F38', '#AFB42B', '#FBC02D', '#FFA000', '#F57C00'],
-  ['#E64A19', '#5D4037', '#616161', '#455A64', '#690CB0'],
+  ['#690CB0', '#D32F2F', '#C2185B', '#7B1FA2', '#512DA8'],
+  ['#303F9F', '#1976D2', '#0288D1', '#0097A7', '#00796B'],
+  ['#388E3C', '#689F38', '#AFB42B', '#FBC02D', '#FFA000'],
+  ['#F57C00', '#E64A19', '#5D4037', '#616161', '#455A64'],
 ];
 
 function ColorSwatchPicker() {
