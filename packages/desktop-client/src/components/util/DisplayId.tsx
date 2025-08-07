bacf3091b664e629e8e5db22a8b0fc370f946909
Emil George <emil.george7@gmail.com>
2025-08-07 18:16:05 +0100

    [4036] - Fix Payee Name Overflow (#5489)
    * fix: update payee name overflow styles
    
    * chore: add release note
    
    * fix: use text one line component

--- packages/desktop-client/src/components/util/DisplayId.tsx
+++ packages/desktop-client/src/components/util/DisplayId.tsx
@@ -3,6 +3,7 @@ import React from 'react';
 import { useTranslation } from 'react-i18next';
 
 import { Text } from '@actual-app/components/text';
+import { TextOneLine } from '@actual-app/components/text-one-line';
 import { theme } from '@actual-app/components/theme';
 
 import { useAccount } from '@desktop-client/hooks/useAccount';
@@ -43,11 +44,11 @@ function PayeeDisplayId({ id, noneColor }) {
   const { t } = useTranslation();
   const payee = usePayee(id);
   return (
-    <Text
+    <TextOneLine
       style={payee == null ? { color: noneColor } : null}
       title={payee ? payee.name : t('None')}
     >
       {payee ? payee.name : t('None')}
-    </Text>
+    </TextOneLine>
   );
 }
