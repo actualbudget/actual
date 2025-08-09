41867983e165ba5924ceaf34094eaec567619d62
Matiss Janis Aboltins <matiss@mja.lv>
2025-08-09 11:43:28 +0100

    Update RuleEditor.tsx

--- packages/desktop-client/src/components/rules/RuleEditor.tsx
+++ packages/desktop-client/src/components/rules/RuleEditor.tsx
@@ -406,7 +406,7 @@ function ScheduleDescription({ id }) {
           marginRight: 15,
         }}
       >
-        <SpaceBetween gap={5} style={{ flexWrap: 'no-wrap' }}>
+        <SpaceBetween gap={5} style={{ flexWrap: 'nowrap' }}>
           <Trans>Payee:</Trans>
           <DisplayId
             type="payees"
