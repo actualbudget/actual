c7cf48259e40ae51f371abb425c3ed44c4916cba
Matiss Janis Aboltins <matiss@mja.lv>
2025-08-13 19:09:49 +0100

    Refactor loading text in MobileRuleEditPage to use Trans component for improved translation handling

--- packages/desktop-client/src/components/mobile/rules/MobileRuleEditPage.tsx
+++ packages/desktop-client/src/components/mobile/rules/MobileRuleEditPage.tsx
@@ -1,7 +1,8 @@
 import React, { useEffect, useState } from 'react';
-import { useTranslation } from 'react-i18next';
+import { useTranslation, Trans } from 'react-i18next';
 import { useParams } from 'react-router';
 
+import { Text } from '@actual-app/components/text';
 import { theme } from '@actual-app/components/theme';
 import { View } from '@actual-app/components/view';
 
@@ -99,7 +100,9 @@ export function MobileRuleEditPage() {
             alignItems: 'center',
           }}
         >
-          <Text><Trans>Loading rule...<Trans></Text>
+          <Text>
+            <Trans>Loading rule...</Trans>
+          </Text>
         </View>
       </Page>
     );
