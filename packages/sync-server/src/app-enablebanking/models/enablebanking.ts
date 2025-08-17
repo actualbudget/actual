56f6c5295adff27dcb3f045bc1f14308da4ce4f8
Erik <realtwister@gmail.com>
2025-08-17 23:09:39 +0200

    forgot a file

---
+++ packages/sync-server/src/app-enablebanking/models/enablebanking.ts
@@ -0,0 +1,51 @@
+export function isErrorResponse(response: unknown): response is ErrorResponse {
+  return (
+    (response as ErrorResponse).error_type !== undefined &&
+    (response as ErrorResponse).error_code !== undefined
+  );
+}
+
+export type ErrorResponse = {
+  error_code: string;
+  error_type?: string;
+};
+
+export type EnableBankingBank = {
+  name: string;
+  logo: string;
+  BIC?: string;
+  country: string;
+};
+
+export type EnableBankingAuthenticationStartResponse = {
+  redirect_url: string;
+  state: string;
+};
+
+export type EnableBankingToken = {
+  bank_id: string;
+  session_id: string;
+  accounts: SyncServerEnableBankingAccount[];
+};
+
+export type SyncServerEnableBankingAccount = {
+  account_id: string;
+  name: string;
+  institution: string;
+  balance: number;
+};
+
+export type EnableBankingStatusResponse = {
+  configured:boolean;
+}
+
+export type EnableBankingTransaction = {
+  amount:number;
+  payee:string;
+  notes:string;
+  date:string;
+}
+
+export type EnableBankingTransactionsResponse = {
+  transactions:EnableBankingTransaction[];
+}
