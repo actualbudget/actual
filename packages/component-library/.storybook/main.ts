bf61084e87bdc1a29b3f9009f64529273d50f47a
Mike Clark <mclarkgb@gmail.com>
2025-08-23 19:55:31 +0100

    lint

--- packages/component-library/.storybook/main.ts
+++ packages/component-library/.storybook/main.ts
@@ -12,16 +12,18 @@ const require = createRequire(import.meta.url);
 function getAbsolutePath(value: string): any {
   return dirname(require.resolve(join(value, 'package.json')));
 }
+
 const config: StorybookConfig = {
   stories: ['../src/**/*.mdx', '../src/**/*.stories.@(js|jsx|mjs|ts|tsx)'],
   addons: [
     getAbsolutePath('@storybook/addon-docs'),
     getAbsolutePath('@storybook/addon-onboarding'),
-    getAbsolutePath("@storybook/addon-a11y")
+    getAbsolutePath('@storybook/addon-a11y'),
   ],
   framework: {
     name: getAbsolutePath('@storybook/react-vite'),
     options: {},
   },
 };
+
 export default config;
