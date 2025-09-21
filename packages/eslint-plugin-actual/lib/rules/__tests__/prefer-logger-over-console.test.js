a69ea6cf41cfe06c0cfa6470d7b279e0703e5bbf
Matt Fiddaman <github@m.fiddaman.uk>
2025-09-21 22:10:35 +0100

    lint rule

---
+++ packages/eslint-plugin-actual/lib/rules/__tests__/prefer-logger-over-console.test.js
@@ -0,0 +1,92 @@
+import { runClassic } from 'eslint-vitest-rule-tester';
+
+import * as rule from '../prefer-logger-over-console';
+
+runClassic(
+  'prefer-logger-over-console',
+  rule,
+  {
+    valid: [
+      {
+        code: 'console.log("test");',
+        filename: 'packages/some-other-package/src/file.js',
+      },
+      {
+        code: 'logger.log("test");',
+        filename: 'packages/loot-core/src/server/test.ts',
+      },
+      {
+        code: 'console.debug("test");',
+        filename: 'packages/loot-core/src/server/test.ts',
+      },
+    ],
+    invalid: [
+      {
+        code: 'console.log("test");',
+        filename: 'packages/loot-core/src/server/test.ts',
+        output: `import { logger } from '../platform/server/log';
+logger.log("test");`,
+        errors: [
+          {
+            messageId: 'preferLogger',
+            data: { method: 'log' },
+          },
+        ],
+      },
+      {
+        code: 'console.warn("warning");',
+        filename: 'packages/loot-core/src/server/test.ts',
+        output: `import { logger } from '../platform/server/log';
+logger.warn("warning");`,
+        errors: [
+          {
+            messageId: 'preferLogger',
+            data: { method: 'warn' },
+          },
+        ],
+      },
+      {
+        code: 'console.error("error");',
+        filename: 'packages/loot-core/src/server/test.ts',
+        output: `import { logger } from '../platform/server/log';
+logger.error("error");`,
+        errors: [
+          {
+            messageId: 'preferLogger',
+            data: { method: 'error' },
+          },
+        ],
+      },
+      {
+        code: 'console.info("info");',
+        filename: 'packages/loot-core/src/server/test.ts',
+        output: `import { logger } from '../platform/server/log';
+logger.info("info");`,
+        errors: [
+          {
+            messageId: 'preferLogger',
+            data: { method: 'info' },
+          },
+        ],
+      },
+      {
+        code: 'console.log("test");',
+        filename: 'packages/loot-core/src/server/sync/test.ts',
+        output: `import { logger } from '../../platform/server/log';
+logger.log("test");`,
+        errors: [
+          {
+            messageId: 'preferLogger',
+            data: { method: 'log' },
+          },
+        ],
+      },
+    ],
+  },
+  {
+    parserOptions: {
+      ecmaVersion: 2020,
+      sourceType: 'module',
+    },
+  },
+);
---
+++ packages/eslint-plugin-actual/lib/rules/prefer-logger-over-console.js
@@ -0,0 +1,109 @@
+/** @type {import('eslint').Rule.RuleModule} */
+module.exports = {
+  meta: {
+    type: 'suggestion',
+    docs: {
+      description:
+        'Replace console.log/warn/error calls with logger in loot-core files',
+    },
+    fixable: 'code',
+    schema: [],
+    messages: {
+      preferLogger: 'Use logger.{{method}} instead of console.{{method}}',
+    },
+  },
+
+  create(context) {
+    const filename = context.getFilename();
+
+    const isLootCoreFile = filename.includes('packages/loot-core/src/server');
+
+    if (!isLootCoreFile) {
+      return {};
+    }
+
+    const methodMap = {
+      log: 'log',
+      warn: 'warn',
+      error: 'error',
+      info: 'info',
+    };
+
+    function hasLoggerImport() {
+      const sourceCode = context.getSourceCode();
+      const ast = sourceCode.ast;
+
+      return ast.body.some(
+        node =>
+          node.type === 'ImportDeclaration' &&
+          node.source.value.includes('platform/server/log'),
+      );
+    }
+
+    function getLoggerImportPath() {
+      const pathParts = filename.split('/');
+      const serverIndex = pathParts.indexOf('server');
+
+      if (serverIndex > -1) {
+        const depth = pathParts.length - serverIndex - 1;
+        return '../'.repeat(depth) + 'platform/server/log';
+      }
+
+      return '../platform/server/log';
+    }
+
+    return {
+      CallExpression(node) {
+        if (
+          node.callee.type === 'MemberExpression' &&
+          node.callee.object.type === 'Identifier' &&
+          node.callee.object.name === 'console' &&
+          node.callee.property.type === 'Identifier' &&
+          methodMap.hasOwnProperty(node.callee.property.name)
+        ) {
+          const method = node.callee.property.name;
+
+          context.report({
+            node,
+            messageId: 'preferLogger',
+            data: { method },
+            fix(fixer) {
+              const sourceCode = context.getSourceCode();
+              const program = sourceCode.ast;
+
+              const fixes = [
+                fixer.replaceText(node.callee, `logger.${methodMap[method]}`),
+              ];
+
+              if (!hasLoggerImport()) {
+                const importPath = getLoggerImportPath();
+                const firstImport = program.body.find(
+                  n => n.type === 'ImportDeclaration',
+                );
+                if (firstImport) {
+                  fixes.unshift(
+                    fixer.insertTextAfter(
+                      firstImport,
+                      // eslint-disable-next-line actual/typography
+                      `\nimport { logger } from '${importPath}';`,
+                    ),
+                  );
+                } else {
+                  fixes.unshift(
+                    fixer.insertTextBefore(
+                      program.body[0],
+                      // eslint-disable-next-line actual/typography
+                      `import { logger } from '${importPath}';\n`,
+                    ),
+                  );
+                }
+              }
+
+              return fixes;
+            },
+          });
+        }
+      },
+    };
+  },
+};
