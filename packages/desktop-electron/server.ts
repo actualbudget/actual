0b16e3ec36e2003f16ece055376be7935248afc9
MikesGlitch <5285928+MikesGlitch@users.noreply.github.com>
2025-10-02 21:32:26 +0100

    add retries to electron server import

--- packages/desktop-electron/server.ts
+++ packages/desktop-electron/server.ts
@@ -1,3 +1,7 @@
+import promiseRetry from 'promise-retry';
+
+const BACKEND_IMPORT_MAX_RETRIES = 30;
+
 const lazyLoadBackend = async (isDev: boolean) => {
   if (process.env.lootCoreScript === undefined) {
     throw new Error(
@@ -6,11 +10,33 @@ const lazyLoadBackend = async (isDev: boolean) => {
   }
 
   try {
-    const bundle = await import(process.env.lootCoreScript);
+    // These retries are primarily for dev mode, where we watch for changes in loot-core
+    // In a packaged build this should always work the first time.
+    const bundle = await promiseRetry(
+      async (retry, number) => {
+        try {
+          return await import(process.env.lootCoreScript!);
+        } catch (error) {
+          console.info(
+            `Loading server bundle: Attempt ${number} of ${BACKEND_IMPORT_MAX_RETRIES}`,
+          );
+
+          retry(error);
+        }
+      },
+      {
+        retries: BACKEND_IMPORT_MAX_RETRIES,
+        minTimeout: 1000,
+        maxTimeout: 1000,
+        factor: 1, // No exponential backoff
+      },
+    );
     bundle.initApp(isDev);
   } catch (error) {
-    console.error('Failed to init the server bundle:', error);
-    throw new Error(`Failed to init the server bundle: ${error}`);
+    console.error('Failed to init the server bundle after all retries:', error);
+    throw new Error(
+      `Failed to init the server bundle after all retries: ${error}`,
+    );
   }
 };
 
