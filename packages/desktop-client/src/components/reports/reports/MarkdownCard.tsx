9caeb760de694157eb8a652505899c3bd2030580
gdufay <gaetandufay@hotmail.com>
2025-10-04 13:57:16 +0200

    Add remark plugin to MarkdownCard

--- packages/desktop-client/src/components/reports/reports/MarkdownCard.tsx
+++ packages/desktop-client/src/components/reports/reports/MarkdownCard.tsx
@@ -10,14 +10,85 @@ import { theme } from '@actual-app/components/theme';
 import { View } from '@actual-app/components/view';
 import { css } from '@emotion/css';
 import rehypeExternalLinks from 'rehype-external-links';
+import remarkGfm from 'remark-gfm';
 
 import { type MarkdownWidget } from 'loot-core/types/models';
 
 import { NON_DRAGGABLE_AREA_CLASS_NAME } from '@desktop-client/components/reports/constants';
 import { ReportCard } from '@desktop-client/components/reports/ReportCard';
 
+import {
+  remarkBreaks,
+  sequentialNewlinesPlugin,
+} from '@desktop-client/util/markdown';
+
+const remarkPlugins = [sequentialNewlinesPlugin, remarkGfm, remarkBreaks];
+
 const markdownStyles = css({
   paddingRight: 20,
+  overflowWrap: 'break-word',
+  '& p': {
+    margin: 0,
+    ':not(:first-child)': {
+      marginTop: '0.25rem',
+    },
+  },
+  '& ul, & ol': {
+    listStylePosition: 'inside',
+    margin: 0,
+    paddingLeft: 0,
+  },
+  '&>* ul, &>* ol': {
+    marginLeft: '1.5rem',
+  },
+  '& li>p': {
+    display: 'contents',
+  },
+  '& blockquote': {
+    paddingLeft: '0.75rem',
+    borderLeft: '3px solid ' + theme.markdownDark,
+    margin: 0,
+  },
+  '& hr': {
+    borderTop: 'none',
+    borderLeft: 'none',
+    borderRight: 'none',
+    borderBottom: '1px solid ' + theme.markdownNormal,
+  },
+  '& code': {
+    backgroundColor: theme.markdownLight,
+    padding: '0.1rem 0.5rem',
+    borderRadius: '0.25rem',
+  },
+  '& pre': {
+    padding: '0.5rem',
+    backgroundColor: theme.markdownLight,
+    textAlign: 'left',
+    borderRadius: '0.5rem',
+    margin: 0,
+    ':not(:first-child)': {
+      marginTop: '0.25rem',
+    },
+    '& code': {
+      background: 'inherit',
+      padding: 0,
+      borderRadius: 0,
+    },
+  },
+  '& table, & th, & td': {
+    border: '1px solid ' + theme.markdownNormal,
+  },
+  '& table': {
+    borderCollapse: 'collapse',
+    wordBreak: 'break-word',
+    display: 'inline-table',
+    ':not(:last-child)': {
+      marginBottom: '0.75rem',
+    },
+  },
+  '& td': {
+    padding: '0.25rem 0.75rem',
+  },
   '& h3': styles.mediumText,
 });
 
@@ -136,6 +207,7 @@ export function MarkdownCard({
         ) : (
           <Text className={markdownStyles}>
             <ReactMarkdown
+              remarkPlugins={remarkPlugins}
               rehypePlugins={[
                 [
                   rehypeExternalLinks,
