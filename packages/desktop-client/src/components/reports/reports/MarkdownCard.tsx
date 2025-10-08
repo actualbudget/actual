f6e033c1e2ade0ba7dc46b5f732e3402694802e5
gdufay <gaetandufay@hotmail.com>
2025-10-08 10:25:13 +0200

    Avoid duplicate css code

--- packages/desktop-client/src/components/reports/reports/MarkdownCard.tsx
+++ packages/desktop-client/src/components/reports/reports/MarkdownCard.tsx
@@ -4,7 +4,6 @@ import { useTranslation } from 'react-i18next';
 import ReactMarkdown from 'react-markdown';
 
 import { Menu } from '@actual-app/components/menu';
-import { styles } from '@actual-app/components/styles';
 import { Text } from '@actual-app/components/text';
 import { theme } from '@actual-app/components/theme';
 import { View } from '@actual-app/components/view';
@@ -19,76 +18,19 @@ import { ReportCard } from '@desktop-client/components/reports/ReportCard';
 import {
   remarkBreaks,
   sequentialNewlinesPlugin,
+  markdownBaseStyles,
 } from '@desktop-client/util/markdown';
 
 const remarkPlugins = [sequentialNewlinesPlugin, remarkGfm, remarkBreaks];
 
-const markdownStyles = css({
+const markdownStyles = css(markdownBaseStyles, {
   paddingRight: 20,
-  overflowWrap: 'break-word',
-  '& p': {
-    margin: 0,
-    ':not(:first-child)': {
-      marginTop: '0.25rem',
-    },
-  },
-  '& ul, & ol': {
-    listStylePosition: 'inside',
-    margin: 0,
-    paddingLeft: 0,
-  },
-  '&>* ul, &>* ol': {
-    marginLeft: '1.5rem',
-  },
-  '& li>p': {
-    display: 'contents',
-  },
-  '& blockquote': {
-    paddingLeft: '0.75rem',
-    borderLeft: '3px solid ' + theme.markdownDark,
-    margin: 0,
-  },
-  '& hr': {
-    borderTop: 'none',
-    borderLeft: 'none',
-    borderRight: 'none',
-    borderBottom: '1px solid ' + theme.markdownNormal,
-  },
-  '& code': {
-    backgroundColor: theme.markdownLight,
-    padding: '0.1rem 0.5rem',
-    borderRadius: '0.25rem',
-  },
-  '& pre': {
-    padding: '0.5rem',
-    backgroundColor: theme.markdownLight,
-    textAlign: 'left',
-    borderRadius: '0.5rem',
-    margin: 0,
-    ':not(:first-child)': {
-      marginTop: '0.25rem',
-    },
-    '& code': {
-      background: 'inherit',
-      padding: 0,
-      borderRadius: 0,
-    },
-  },
-  '& table, & th, & td': {
-    border: '1px solid ' + theme.markdownNormal,
-  },
   '& table': {
-    borderCollapse: 'collapse',
-    wordBreak: 'break-word',
     display: 'inline-table',
     ':not(:last-child)': {
       marginBottom: '0.75rem',
     },
   },
-  '& td': {
-    padding: '0.25rem 0.75rem',
-  },
-  '& h3': styles.mediumText,
 });
 
 type MarkdownCardProps = {
--- packages/desktop-client/src/util/markdown.ts
+++ packages/desktop-client/src/util/markdown.ts
@@ -1,6 +1,10 @@
 // @ts-strict-ignore
 import { newlineToBreak } from 'mdast-util-newline-to-break';
 
+import { theme } from '@actual-app/components/theme';
+import { styles } from '@actual-app/components/styles';
+import { css } from '@emotion/css';
+
 export function sequentialNewlinesPlugin() {
   // Adapted from https://codesandbox.io/s/create-react-app-forked-h3rmcy?file=/src/sequentialNewlinePlugin.js:0-774
   const data = this.data();
@@ -37,3 +41,66 @@ export function sequentialNewlinesPlugin() {
 export function remarkBreaks() {
   return newlineToBreak;
 }
+
+export const markdownBaseStyles = css({
+  overflowWrap: 'break-word',
+  '& p': {
+    margin: 0,
+    ':not(:first-child)': {
+      marginTop: '0.25rem',
+    },
+  },
+  '& ul, & ol': {
+    listStylePosition: 'inside',
+    margin: 0,
+    paddingLeft: 0,
+  },
+  '&>* ul, &>* ol': {
+    marginLeft: '1.5rem',
+  },
+  '& li>p': {
+    display: 'contents',
+  },
+  '& blockquote': {
+    paddingLeft: '0.75rem',
+    borderLeft: '3px solid ' + theme.markdownDark,
+    margin: 0,
+  },
+  '& hr': {
+    borderTop: 'none',
+    borderLeft: 'none',
+    borderRight: 'none',
+    borderBottom: '1px solid ' + theme.markdownNormal,
+  },
+  '& code': {
+    backgroundColor: theme.markdownLight,
+    padding: '0.1rem 0.5rem',
+    borderRadius: '0.25rem',
+  },
+  '& pre': {
+    padding: '0.5rem',
+    backgroundColor: theme.markdownLight,
+    textAlign: 'left',
+    borderRadius: '0.5rem',
+    margin: 0,
+    ':not(:first-child)': {
+      marginTop: '0.25rem',
+    },
+    '& code': {
+      background: 'inherit',
+      padding: 0,
+      borderRadius: 0,
+    },
+  },
+  '& table, & th, & td': {
+    border: '1px solid ' + theme.markdownNormal,
+  },
+  '& table': {
+    borderCollapse: 'collapse',
+    wordBreak: 'break-word',
+  },
+  '& td': {
+    padding: '0.25rem 0.75rem',
+  },
+  '& h3': styles.mediumText,
+});
