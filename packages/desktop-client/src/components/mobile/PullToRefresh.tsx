2f844919d8a3a47a39920bb4e4d52b0034d8f299
Brandon Butler <brandon@brandonbutler.me>
2025-10-12 20:47:52 -0400

    fix background color of pull to refresh

--- packages/desktop-client/src/components/mobile/PullToRefresh.tsx
+++ packages/desktop-client/src/components/mobile/PullToRefresh.tsx
@@ -7,7 +7,7 @@ type PullToRefreshProps = ComponentProps<typeof BasePullToRefresh>;
 
 export function PullToRefresh(props: PullToRefreshProps) {
   return (
-    <div style={{ overflow: 'auto', flex: 1 }}>
+    <div style={{ overflow: 'auto', flex: 1, backgroundColor: 'var(--color-pageBackground)', }}>
       <BasePullToRefresh
         pullDownThreshold={80}
         resistance={2}
