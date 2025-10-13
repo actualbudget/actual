8110d030956fddec1cb2c9c5e2209837667d20c7
code-with-jov <52590043+code-with-jov@users.noreply.github.com>
2025-10-13 03:06:10 +0000

    Remove logger

--- packages/loot-core/src/server/budget/base.ts
+++ packages/loot-core/src/server/budget/base.ts
@@ -1,5 +1,4 @@
 // @ts-strict-ignore
-import { logger } from '../../platform/server/log';
 import * as monthUtils from '../../shared/months';
 import { q } from '../../shared/query';
 import { getChangedValues } from '../../shared/util';
@@ -260,11 +259,6 @@ export async function createBudget(months) {
       const { start, end } = monthUtils.bounds(month);
       const sheetName = monthUtils.sheetForMonth(month);
       const prevSheetName = monthUtils.sheetForMonth(prevMonth);
-
-      logger.log(
-        `[createBudget] Creating sheet for month=${month}, sheetName=${sheetName}, isPayPeriod=${monthUtils.isPayPeriod(month)}`,
-      );
-
       categories.forEach(cat => {
         createCategory(cat, sheetName, prevSheetName, start, end);
       });
