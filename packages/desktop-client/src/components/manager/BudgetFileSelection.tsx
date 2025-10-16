1736f5f65c293d630d9eb8b3e3b0168c262ef42f
Matiss Janis Aboltins <matiss@mja.lv>
2025-10-16 21:49:48 +0100

    Align onboarding form element sizes

--- packages/desktop-client/src/components/manager/BudgetFileSelection.tsx
+++ packages/desktop-client/src/components/manager/BudgetFileSelection.tsx
@@ -623,10 +623,12 @@ export function BudgetFileSelection({
         maxHeight: '100%',
         flex: 1,
         justifyContent: 'center',
-        ...(!quickSwitchMode && {
-          marginTop: 20,
-          width: '100vw',
-        }),
+        ...(quickSwitchMode
+          ? {
+              marginTop: 20,
+              width: '100vw',
+            }
+          : { marginBottom: 20 }),
         [`@media (min-width: ${tokens.breakpoint_small})`]: {
           maxWidth: tokens.breakpoint_small,
           width: '100%',
@@ -673,10 +675,12 @@ export function BudgetFileSelection({
       {!quickSwitchMode && (
         <View
           style={{
+            display: 'flex',
             flexDirection: 'row',
             justifyContent: 'flex-end',
-            alignItems: 'center',
-            padding: 25,
+            alignItems: 'stretch',
+            margin: 10,
+            minHeight: 39,
           }}
         >
           <Button
