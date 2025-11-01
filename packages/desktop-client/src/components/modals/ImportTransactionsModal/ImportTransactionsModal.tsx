fd26d0cc97e3918e00c4988b0f98ec8b971904e7
Kim Biesbjerg <kim@medware.dk>
2025-11-01 15:23:55 +0100

    wip

--- packages/desktop-client/src/components/modals/ImportTransactionsModal/ImportTransactionsModal.tsx
+++ packages/desktop-client/src/components/modals/ImportTransactionsModal/ImportTransactionsModal.tsx
@@ -982,16 +982,19 @@ export function ImportTransactionsModal({
                       <Trans>Encoding:</Trans>
                       <Select
                         options={[
-                          ['utf-8', 'UTF-8'],
-                          ['windows-1252', 'ISO-8859-1 / Windows-1252'],
-                          ['utf-16le', 'UTF-16 LE'],
-                          ['shift-jis', 'Shift_JIS (Japanese)'],
-                          ['euc-jp', 'EUC-JP (Japanese)'],
-                          ['iso-2022-jp', 'ISO-2022-JP (Japanese)'],
-                          ['gbk', 'GBK (Chinese Simplified)'],
-                          ['gb18030', 'GB18030 (Chinese Simplified)'],
-                          ['big5', 'Big5 (Chinese Traditional)'],
-                          ['euc-kr', 'EUC-KR (Korean)'],
+                          ['utf-8', 'Unicode (UTF-8)'],
+                          [
+                            'windows-1252',
+                            'Western Europe (Windows-1252 / ISO-8859-1)',
+                          ],
+                          ['utf-16le', 'Unicode (UTF-16 LE)'],
+                          ['shift-jis', 'Japanese (Shift_JIS)'],
+                          ['euc-jp', 'Japanese (EUC-JP)'],
+                          ['iso-2022-jp', 'Japanese (ISO-2022-JP)'],
+                          ['gbk', 'Chinese Simplified (GBK)'],
+                          ['gb18030', 'Chinese Simplified (GB18030)'],
+                          ['big5', 'Chinese Traditional (Big5)'],
+                          ['euc-kr', 'Korean (EUC-KR)'],
                         ]}
                         value={encoding}
                         onChange={value => {
