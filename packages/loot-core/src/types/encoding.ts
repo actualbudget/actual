314e1dd56590198dea366ed370a4c8156a5e1786
Kim Biesbjerg <kim@medware.dk>
2025-11-02 18:54:23 +0100

    feature-import-csv-encoding

--- packages/loot-core/src/types/encoding.ts
+++ packages/loot-core/src/types/encoding.ts
@@ -1,14 +1,14 @@
 const SUPPORTED_ENCODINGS = [
-  'utf-8',
-  'windows-1252', // latin1
-  'utf-16le',
-  'shift-jis',
-  'euc-jp',
-  'iso-2022-jp',
-  'gbk',
-  'gb18030',
-  'big5',
-  'euc-kr',
+  'utf-8', // Unicode (UTF-8)
+  'windows-1252', // Western Europe (Windows-1252 / ISO-8859-1)
+  'utf-16le', // Unicode (UTF-16 LE)
+  'shift-jis', // Japanese (Shift_JIS)
+  'euc-jp', // Japanese (EUC-JP)
+  'iso-2022-jp', // Japanese (ISO-2022-JP)
+  'gbk', // Chinese Simplified (GBK)
+  'gb18030', // Chinese Simplified (GB18030)
+  'big5', // Chinese Traditional (Big5)
+  'euc-kr', // Korean (EUC-KR)
 ] as const;
 
 export type Encoding = (typeof SUPPORTED_ENCODINGS)[number];
