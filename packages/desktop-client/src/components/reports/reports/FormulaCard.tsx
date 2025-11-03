d1d18c5bf997c2c246ef5666194c2464053562a2
Matt Fiddaman <github@m.fiddaman.uk>
2025-11-03 01:13:11 +0000

    remove React import

--- packages/desktop-client/src/components/reports/reports/FormulaCard.tsx
+++ packages/desktop-client/src/components/reports/reports/FormulaCard.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useMemo } from 'react';
+import { useState, useMemo } from 'react';
 import { useTranslation } from 'react-i18next';
 
 import { View } from '@actual-app/components/view';
