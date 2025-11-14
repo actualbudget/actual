63de281d13086293777b7ac676b4fc72a84dc941
Joel Jeremy Marquez <joeljeremy.marquez@gmail.com>
2025-11-14 08:44:54 -0800

    TestProviders

---
+++ packages/desktop-client/src/mocks.tsx
@@ -0,0 +1,25 @@
+import React, { type ReactNode } from 'react';
+import { Provider } from 'react-redux';
+
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+
+import { configureAppStore, type AppStore } from './redux/store';
+
+let mockQueryClient = new QueryClient();
+
+export let mockStore: AppStore = configureAppStore({
+  queryClient: mockQueryClient,
+});
+
+export function resetTestProviders() {
+  mockQueryClient = new QueryClient();
+  mockStore = configureAppStore({ queryClient: mockQueryClient });
+}
+
+export function TestProviders({ children }: { children: ReactNode }) {
+  return (
+    <QueryClientProvider client={mockQueryClient}>
+      <Provider store={mockStore}>{children}</Provider>
+    </QueryClientProvider>
+  );
+}
