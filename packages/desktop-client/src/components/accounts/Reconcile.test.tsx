74bb4b60717b3d3522a459806e77f4752b8d9c68
Juulz <julesmcn@gmail.com>
2025-11-20 16:34:50 -0500

    Rename button text from 'Done reconciling' to 'Complete Reconciling'

--- packages/desktop-client/src/components/accounts/Reconcile.test.tsx
+++ packages/desktop-client/src/components/accounts/Reconcile.test.tsx
@@ -57,7 +57,7 @@ describe('ReconcilingMessage math & UI', () => {
     ).not.toBeInTheDocument();
 
     // Done button triggers callback
-    await userEvent.click(screen.getByText('Done reconciling'));
+    await userEvent.click(screen.getByText('Complete Reconciling'));
     expect(onDone).toHaveBeenCalledTimes(1);
   });
 
