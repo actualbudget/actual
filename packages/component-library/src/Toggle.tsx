3f7bb522df97798b9c0df899b59ec9679bc55000
Matthew Mao <matty8100@gmail.com>
2025-12-01 21:03:19 -0800

    typo

--- packages/component-library/src/Toggle.tsx
+++ packages/component-library/src/Toggle.tsx
@@ -32,7 +32,7 @@ export const Toggle = ({
         className={css({
           height: 0,
           width: 0,
-          visiblity: 'hidden',
+          visibility: 'hidden',
         })}
         type="checkbox"
       />
