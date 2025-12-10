2b755ff0e3aaf8cee4d04ad030c3a8be6b2d9473
Bradley Nelson <bradley@nel.family>
2025-12-10 04:38:02 +0000

    lint

--- packages/sync-server/src/services/api-token-service.js
+++ packages/sync-server/src/services/api-token-service.js
@@ -1,5 +1,6 @@
-import * as bcrypt from 'bcrypt';
 import crypto from 'crypto';
+
+import * as bcrypt from 'bcrypt';
 import { v4 as uuidv4 } from 'uuid';
 
 import { getAccountDb } from '../account-db';
