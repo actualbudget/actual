579e72dc7ff62f5937184e60b7f8798cef852167
Bradley Nelson <bradley@nel.family>
2025-12-11 21:08:56 -0700

    fix: use async bcrypt in api-token-service

--- packages/sync-server/src/app-account.js
+++ packages/sync-server/src/app-account.js
@@ -117,8 +117,8 @@ app.post('/login', async (req, res) => {
   res.send({ status: 'ok', data: { token } });
 });
 
-app.post('/change-password', (req, res) => {
-  const session = validateSession(req, res);
+app.post('/change-password', async (req, res) => {
+  const session = await validateSession(req, res);
   if (!session) return;
 
   const { error } = changePassword(req.body.password);
@@ -131,8 +131,8 @@ app.post('/change-password', (req, res) => {
   res.send({ status: 'ok', data: {} });
 });
 
-app.post('/server-prefs', (req, res) => {
-  const session = validateSession(req, res);
+app.post('/server-prefs', async (req, res) => {
+  const session = await validateSession(req, res);
   if (!session) return;
 
   if (!isAdmin(session.user_id)) {
@@ -156,8 +156,8 @@ app.post('/server-prefs', (req, res) => {
   res.send({ status: 'ok', data: {} });
 });
 
-app.get('/validate', (req, res) => {
-  const session = validateSession(req, res);
+app.get('/validate', async (req, res) => {
+  const session = await validateSession(req, res);
   if (session) {
     const user = getUserInfo(session.user_id);
     if (!user) {
