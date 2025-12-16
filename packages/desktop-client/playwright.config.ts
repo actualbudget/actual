9962a9743f42e847252ba40f62d49e7fab3f2762
MikesGlitch <5285928+MikesGlitch@users.noreply.github.com>
2025-12-16 20:25:23 +0000

    dot reporter for line by line

--- packages/desktop-client/playwright.config.ts
+++ packages/desktop-client/playwright.config.ts
@@ -8,7 +8,7 @@ export default defineConfig({
   fullyParallel: true,
   workers: process.env.CI ? 1 : undefined,
   testDir: 'e2e/',
-  reporter: process.env.CI ? 'blob' : [['html', { open: 'never' }]],
+  reporter: process.env.CI ? [['blob'],['dot']] : [['html', { open: 'never' }]],
   use: {
     userAgent: 'playwright',
     screenshot: 'on',
