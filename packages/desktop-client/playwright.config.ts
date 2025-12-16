ad6a0df7f235102c453bd0ef8150dd434741d209
MikesGlitch <5285928+MikesGlitch@users.noreply.github.com>
2025-12-16 20:56:16 +0000

    list reporter

--- packages/desktop-client/playwright.config.ts
+++ packages/desktop-client/playwright.config.ts
@@ -9,7 +9,7 @@ export default defineConfig({
   workers: process.env.CI ? 1 : undefined,
   testDir: 'e2e/',
   reporter: process.env.CI
-    ? [['blob'], ['dot']]
+    ? [['blob'], ['list']]
     : [['html', { open: 'never' }]],
   use: {
     userAgent: 'playwright',
