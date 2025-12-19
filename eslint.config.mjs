c28163ae958c44497e5233ab688c02b68920827a
Matiss Janis Aboltins <matiss@mja.lv>
2025-12-19 20:52:52 +0000

    Update linting configuration

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,4 +1,3 @@
-import pluginJSXA11y from 'eslint-plugin-jsx-a11y';
 import oxlint from 'eslint-plugin-oxlint';
 import pluginPerfectionist from 'eslint-plugin-perfectionist';
 import pluginTypescriptPaths from 'eslint-plugin-typescript-paths';
@@ -86,9 +85,6 @@ export default defineConfig(
   },
   {
     files: ['**/*.{js,ts,jsx,tsx,mjs,mts}'],
-    plugins: {
-      'jsx-a11y': pluginJSXA11y,
-    },
     rules: {
       // http://eslint.org/docs/rules/
       'no-dupe-args': 'warn',
@@ -144,35 +140,6 @@ export default defineConfig(
           newlinesBetween: 'always',
         },
       ],
-
-      // https://github.com/evcohen/eslint-plugin-jsx-a11y/tree/master/docs/rules
-      'jsx-a11y/alt-text': 'warn',
-      'jsx-a11y/anchor-has-content': 'warn',
-      'jsx-a11y/anchor-is-valid': [
-        'warn',
-        {
-          aspects: ['noHref', 'invalidHref'],
-        },
-      ],
-      'jsx-a11y/aria-activedescendant-has-tabindex': 'warn',
-      'jsx-a11y/aria-props': 'warn',
-      'jsx-a11y/aria-proptypes': 'warn',
-      'jsx-a11y/aria-role': [
-        'warn',
-        {
-          ignoreNonDOM: true,
-        },
-      ],
-      'jsx-a11y/aria-unsupported-elements': 'warn',
-      'jsx-a11y/heading-has-content': 'warn',
-      'jsx-a11y/iframe-has-title': 'warn',
-      'jsx-a11y/img-redundant-alt': 'warn',
-      'jsx-a11y/no-access-key': 'warn',
-      'jsx-a11y/no-distracting-elements': 'warn',
-      'jsx-a11y/no-redundant-roles': 'warn',
-      'jsx-a11y/role-has-required-aria-props': 'warn',
-      'jsx-a11y/role-supports-aria-props': 'warn',
-      'jsx-a11y/scope': 'warn',
 
       'actual/typography': 'warn',
       'actual/prefer-if-statement': 'warn',
