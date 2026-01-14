a5173989dead84ed54a99c4d8f0beca268ad8641
Matt Fiddaman <github@m.fiddaman.uk>
2026-01-14 22:49:21 +0000

    goals

--- packages/desktop-client/src/components/budget/goals/editor/WeekAutomationReadOnly.tsx
+++ packages/desktop-client/src/components/budget/goals/editor/WeekAutomationReadOnly.tsx
@@ -1,7 +1,9 @@
 import { Trans } from 'react-i18next';
 
 import type { PeriodicTemplate } from 'loot-core/types/models/templates';
+import { type TransObjectLiteral } from 'loot-core/types/util';
 
+import { TNum } from '@desktop-client/components/TNum';
 import { useFormat } from '@desktop-client/hooks/useFormat';
 
 type WeekAutomationReadOnlyProps = {
@@ -16,9 +18,13 @@ export const WeekAutomationReadOnly = ({
   return (
     <Trans>
       Budget{' '}
-      {{
-        amount: format(template.amount, 'financial'),
-      }}{' '}
+      <TNum>
+        {
+          {
+            amount: format(template.amount, 'financial'),
+          } as TransObjectLiteral
+        }
+      </TNum>{' '}
       each week
     </Trans>
   );
