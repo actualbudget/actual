a09946bac936f9f5619e9d554ef096700e50b6ae
Juulz <julesmcn@gmail.com>
2026-01-15 10:06:56 -0500

    Update light.ts

--- packages/desktop-client/src/style/themes/light.ts
+++ packages/desktop-client/src/style/themes/light.ts
@@ -47,7 +47,7 @@ export const sidebarItemBackgroundHover = colorPalette.navy800;
 export const sidebarItemAccentSelected = colorPalette.purple200;
 export const sidebarItemText = colorPalette.navy150;
 export const sidebarItemTextSelected = colorPalette.purple200;
-export const sidebarBudgetName = buttonNormalBorder;
+export const sidebarBudgetName = colorPalette.navy150;
 
 export const menuBackground = colorPalette.white;
 export const menuItemBackground = colorPalette.navy50;
