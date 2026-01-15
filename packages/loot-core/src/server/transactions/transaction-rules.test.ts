bb70074f355d5c778467ca3615b99e6604a73d85
lif <1835304752@qq.com>
2026-01-15 08:56:22 +0800

    fix: disable oneOf and notOneOf operations for Notes filter (#6567)
    * fix: disable oneOf and notOneOf operations for Notes filter
    
    The Notes field is a free-text field and should not support "one of" /
    "not one of" filter operations, as these require predefined values.
    Attempting to use these operations with the Notes filter caused the app
    to crash with a TypeError.
    
    This change:
    - Adds 'oneOf' and 'notOneOf' to disallowedOps for the notes field
    - Updates the TypeScript type definitions to reflect this constraint
    - Updates existing tests to use imported_payee field for oneOf tests
    - Adds unit tests for the isValidOp and getValidOps functions
    
    Fixes #6325
    
    Signed-off-by: majiayu000 <1835304752@qq.com>
    
    * move note
    
    ---------
    
    Signed-off-by: majiayu000 <1835304752@qq.com>
    Co-authored-by: Matt Fiddaman <github@m.fiddaman.uk>

--- packages/loot-core/src/server/transactions/transaction-rules.test.ts
+++ packages/loot-core/src/server/transactions/transaction-rules.test.ts
@@ -451,9 +451,9 @@ describe('Transaction rules', () => {
     expect(transactions.map(t => t.id)).toEqual(['2', '3']);
 
     transactions = await getMatchingTransactions([
-      { field: 'notes', op: 'oneOf', value: ['fooo', 'barr'] },
+      { field: 'notes', op: 'is', value: 'barr' },
     ]);
-    expect(transactions.map(t => t.id)).toEqual(['2', '3', '1']);
+    expect(transactions.map(t => t.id)).toEqual(['1']);
 
     transactions = await getMatchingTransactions([
       { field: 'notes', op: 'is', value: '' },
