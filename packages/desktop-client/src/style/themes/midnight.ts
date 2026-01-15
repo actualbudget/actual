618000a6c6269eb7c4d583133cca9e137aa64c9d
Juulz <julesmcn@gmail.com>
2026-01-15 09:57:11 -0500

    Update midnight.ts to add sidebarBudgetName variable

--- packages/desktop-client/src/style/themes/midnight.ts
+++ packages/desktop-client/src/style/themes/midnight.ts
@@ -47,6 +47,7 @@ export const sidebarItemAccentSelected = colorPalette.purple200;
 export const sidebarItemBackgroundHover = colorPalette.gray700;
 export const sidebarItemText = colorPalette.gray100;
 export const sidebarItemTextSelected = colorPalette.purple200;
+export const sidebarBudgetName = buttonNormalBorder;
 
 export const menuBackground = colorPalette.gray700;
 export const menuItemBackground = colorPalette.gray200;
