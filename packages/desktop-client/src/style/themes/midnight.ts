4d9753f697ae0574f76981719769fac0f3433920
Juulz <julesmcn@gmail.com>
2026-01-22 10:32:59 -0500

    Change toBudgetZero color to numberPositive

--- packages/desktop-client/src/style/themes/midnight.ts
+++ packages/desktop-client/src/style/themes/midnight.ts
@@ -49,7 +49,7 @@ export const budgetNumberPositive = budgetNumberNeutral;
 export const templateNumberFunded = numberPositive;
 export const templateNumberUnderFunded = colorPalette.orange300;
 export const toBudgetPositive = numberPositive;
-export const toBudgetZero = pageTextPositive;
+export const toBudgetZero = numberPositive;
 
 export const sidebarBackground = colorPalette.gray900;
 export const sidebarItemBackgroundPending = colorPalette.orange200;
