782bdfb017d810b20608721211e41eef171dbd0a
Matt Fiddaman <github@m.fiddaman.uk>
2026-02-05 14:32:51 +0000

    reset dashboard

--- packages/desktop-client/src/components/settings/Reset.tsx
+++ packages/desktop-client/src/components/settings/Reset.tsx
@@ -31,31 +31,40 @@ export function ResetCache() {
 
   return (
     <>
-    <Setting
-      primaryAction={
-        <ButtonWithLoading isLoading={resetting} onPress={onResetCache}>
-          <Trans>Reset budget cache</Trans>
-        </ButtonWithLoading>
-      }
-    >
-      <Text>
-        <Trans>
-          <strong>Reset budget cache</strong> will clear all cached values for
-          the budget and recalculate the entire budget. All values in the budget
-          are cached for performance reasons, and if there is a bug in the cache
-          you won't see correct values. There is no danger in resetting the
-          cache. Hopefully you never have to do this.
-        </Trans>
-      </Text>
-    </Setting>
-    <Setting
-      primaryAction={
-        <ButtonWithLoading isLoading={resettingDashboard} onPress={onResetDashboard}>
-          <Trans>Reset Dashboard</Trans>
-        </ButtonWithLoading>
-      }
-    >
-    </Setting>
+      <Setting
+        primaryAction={
+          <ButtonWithLoading isLoading={resetting} onPress={onResetCache}>
+            <Trans>Reset budget cache</Trans>
+          </ButtonWithLoading>
+        }
+      >
+        <Text>
+          <Trans>
+            <strong>Reset budget cache</strong> will clear all cached values for
+            the budget and recalculate the entire budget. All values in the
+            budget are cached for performance reasons, and if there is a bug in
+            the cache you won't see correct values. There is no danger in
+            resetting the cache. Hopefully you never have to do this.
+          </Trans>
+        </Text>
+      </Setting>
+      <Setting
+        primaryAction={
+          <ButtonWithLoading
+            isLoading={resettingDashboard}
+            onPress={onResetDashboard}
+          >
+            <Trans>Reset Dashboard</Trans>
+          </ButtonWithLoading>
+        }
+      >
+        <Text>
+          <Trans>
+            <strong>Reset Dashboard</strong> will reset all dashboard
+            customizations and restore the default layout and widgets.
+          </Trans>
+        </Text>
+      </Setting>
     </>
   );
 }
