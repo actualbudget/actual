6be6d1066e19231dadd9431fbba13459d5ab27a5
Joel Jeremy Marquez <joeljeremy.marquez@gmail.com>
2026-02-06 19:02:33 +0000

    Fix onbudget and offbudget displaying closed accounts

--- packages/desktop-client/src/hooks/useAccounts.ts
+++ packages/desktop-client/src/hooks/useAccounts.ts
@@ -1,7 +1,9 @@
-import { useAccountsQuery } from './useAccountsQuery';
+import { useQuery } from '@tanstack/react-query';
+
+import { accountQueries } from '@desktop-client/accounts';
 
 export function useAccounts() {
-  const query = useAccountsQuery();
+  const query = useQuery(accountQueries.list());
   // TODO: Update to return query states (e.g. isFetching, isError, etc)
   // so clients can handle loading and error states appropriately.
   return query.data ?? [];
