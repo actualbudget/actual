43eed93fabac2e82cb1ebb8df463c76812236c25
MikesGlitch <5285928+MikesGlitch@users.noreply.github.com>
2026-04-12 19:08:42 +0100

    testing

--- packages/desktop-electron/e2e/onboarding.test.ts
+++ packages/desktop-electron/e2e/onboarding.test.ts
@@ -20,6 +20,38 @@ test.describe('Onboarding', () => {
   test('starts the sync server and navigates to bootstrap page', async ({
     electronPage,
   }) => {
+    const consoleMessages: string[] = [];
+
+    electronPage.on('console', msg => {
+      consoleMessages.push(msg.text());
+      console.info('Console message:', msg.text());
+    });
+
+    await electronPage.waitForTimeout(20_000); // wait for potential console messages to arrive
+    console.error(
+      'Collected console messages:',
+      JSON.stringify(consoleMessages),
+    );
+    expect(consoleMessages.length).toBeGreaterThan(0);
+
+    expect(consoleMessages).toContain(
+      'Sync-Server: Listening on localhost:5007...',
+    );
+
+    // await expect
+    //   .poll(() => consoleMessages, { timeout: 15_000 })
+    //   .toEqual(
+    //     expect.arrayContaining([
+    //       expect.stringContaining('Sync-Server: Migrations: DONE'),
+    //       expect.stringContaining(
+    //         'Sync-Server: Actual Sync Server has started!',
+    //       ),
+    //       expect.stringContaining(
+    //         'Sync-Server: Listening on localhost:5007...',
+    //       ),
+    //     ]),
+    //   );
+
     const bootstrapPage = await configurationPage.clickOnStartSyncServer();
     await expect(bootstrapPage.getHeading()).toHaveText('Welcome to Actual!');
     await expect(electronPage).toHaveScreenshot();
