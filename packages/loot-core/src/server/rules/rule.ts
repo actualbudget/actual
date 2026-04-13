825744ec772b4eba725fcadf92db69df32299e90
Stephen Brown II <Stephen.Brown2@gmail.com>
2026-04-13 13:08:59 -0400

    Remove protective if statement when adding _balanceOfPrefetched

--- packages/loot-core/src/server/rules/rule.ts
+++ packages/loot-core/src/server/rules/rule.ts
@@ -45,10 +45,8 @@ function execSplitActions(actions: Action[], transaction) {
     }
     newTransactions[splitTransactionIndex].parent_amount = transaction.amount;
     newTransactions[splitTransactionIndex].balance = transaction.balance;
-    if (transaction._balanceOfPrefetched) {
-      newTransactions[splitTransactionIndex]._balanceOfPrefetched =
-        transaction._balanceOfPrefetched;
-    }
+    newTransactions[splitTransactionIndex]._balanceOfPrefetched =
+      transaction._balanceOfPrefetched;
     action.exec(newTransactions[splitTransactionIndex]);
   });
 
