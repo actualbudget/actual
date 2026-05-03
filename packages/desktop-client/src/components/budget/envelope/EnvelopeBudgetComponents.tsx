30ae9bb591fe84a8732b543274e3aaa8a10b0836
Emil Tveden Bjerglund <emilbp@gmail.com>
2026-05-03 16:48:33 +0200

    Set minWidth instead

--- packages/desktop-client/src/components/budget/envelope/EnvelopeBudgetComponents.tsx
+++ packages/desktop-client/src/components/budget/envelope/EnvelopeBudgetComponents.tsx
@@ -514,7 +514,7 @@ export const ExpenseCategoryMonth = memo(function ExpenseCategoryMonth({
           onOpenChange={() => setBalanceMenuOpen(false)}
           style={{
             margin: 1,
-            width: 'min(220px, calc(100vw - 16px))',
+            minWidth: 190,
           }}
           isNonModal
           {...balancePosition}
