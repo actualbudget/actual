3ae37b7ea6cfcbe5e613eb563e54f531b6441361
Julian Dominguez-Schatz <julian.dominguezschatz@gmail.com>
2026-05-12 23:56:34 -0400

    Revert "Temp: trigger docs CI"
    This reverts commit 1c2ca1125ccc4342203832ae5c78eee5984716c0.

--- packages/docs/docs-sidebar.js
+++ packages/docs/docs-sidebar.js
@@ -1,6 +1,6 @@
 /**
  * Creating a sidebar enables you to:
- - create an ordered group of docs,
+ - create an ordered group of docs
  - render a sidebar for each doc of that group
  - provide next/previous navigation
 
