cf21deb9592feaf47fe97a8f8eb5443b990900f0
Leandro Menezes <lelemm@gmail.com>
2026-06-09 09:21:07 -0300

    removed useless reexports

--- packages/loot-core/src/server/rules/customFunctions.ts
+++
@@ -1,9 +0,0 @@
-export {
-  createBudgetQueryPrefetchKey,
-  CustomFunctionsPlugin,
-  customFunctionsTranslations,
-} from '#shared/formulas/customFunctions';
-export type {
-  BudgetQueryRequest,
-  FormulaQueryContext,
-} from '#shared/formulas/customFunctions';
