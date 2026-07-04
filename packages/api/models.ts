40b90cffb6a7a4c2d086d4e2b692376e73f09a8e
Matiss Janis Aboltins <matiss@mja.lv>
2026-07-04 23:09:02 +0100

    Update models.ts

--- packages/api/models.ts
+++ packages/api/models.ts
@@ -1,2 +1,2 @@
 export type * from '@actual-app/core/server/api-models';
-export type { SyncedPrefs } from '@actual-app/core/types/prefs';
+export type { SyncedPrefs as Preferences } from '@actual-app/core/types/prefs';
