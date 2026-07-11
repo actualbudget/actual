8e170470f3bcc58e83a7719936473b9d7d5b59ce
Ehtasham-Yasin <ehtasham.yasin.dev@gmail.com>
2026-07-11 21:59:52 +0200

    chore(api): migrate node version validation to TypeScript

---
+++ packages/api/validateNodeVersion.ts
@@ -0,0 +1,14 @@
+import { satisfies } from 'compare-versions';
+
+import * as packageJson from './package.json';
+
+export function validateNodeVersion(): void {
+  const nodeVersion = process.versions.node;
+  const minimumNodeVersion = packageJson.engines.node;
+
+  if (!satisfies(nodeVersion, minimumNodeVersion)) {
+    throw new Error(
+      `@actual-app/api requires a node version ${minimumNodeVersion}. Found that you are using: ${nodeVersion}. Please upgrade to a higher version`,
+    );
+  }
+}
