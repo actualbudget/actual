ee94412f8b8fd7724b17d02dcfe3070e185847e8
Matt Fiddaman <github@m.fiddaman.uk>
2026-08-20 18:42:50 +0100

    add experimental flag and account group plumbing

---
+++ packages/desktop-client/src/account-groups/queries.ts
@@ -0,0 +1,16 @@
+import { send } from '@actual-app/core/platform/client/connection';
+import type { AccountGroupEntity } from '@actual-app/core/types/models';
+import { queryOptions } from '@tanstack/react-query';
+
+export const accountGroupQueries = {
+  all: () => ['account-groups'],
+  lists: () => [...accountGroupQueries.all(), 'lists'],
+  list: () =>
+    queryOptions<AccountGroupEntity[]>({
+      queryKey: [...accountGroupQueries.lists()],
+      queryFn: () => send('account-groups-get'),
+      placeholderData: [],
+      // Manually invalidated when account groups change
+      staleTime: Infinity,
+    }),
+};
